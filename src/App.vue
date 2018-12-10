670ca9414b174c98feff20b7a9c501765a8bcec6
Evan Savage <esavage@toronto.ca>
2018-12-10 11:39:32 -0500

    Use PostCSS instead of LESS
    for a better upgrade path to new CSS features as they're made available,
    browserslist support, and fewer dependencies.

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@ export default {
 };
 </script>
 
-<style lang="less">
+<style lang="postcss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
