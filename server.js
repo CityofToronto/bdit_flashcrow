539b6b3da02931be2391e00b2f447a2bc06e7832
Evan Savage <esavage@toronto.ca>
2018-12-20 13:35:48 -0500

    Use `express-promise-router` to provide async routes
    in preparation for adding PostgreSQL bindings.

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+// const csrf = require('csurf');
 const express = require('express');
+const Router = require('express-promise-router');
 const session = require('express-session');
 const helmet = require('helmet');
 
@@ -11,18 +13,26 @@ serverConfig = serverConfig[ENV];
 
 app.use(helmet());
 app.use(session(serverConfig.session));
-
+/*
+app.use(csrf({
+  cookie: false,
+}));
+*/
 
 let counter = 0;
 
-app.get('/counter', (req, res) => {
+const router = new Router();
+
+router.get('/counter', async (req, res) => {
   res.send({ counter });
 });
 
-app.put('/counter', (req, res) => {
+router.put('/counter', async (req, res) => {
   counter += 1;
   res.send({ counter });
 });
+
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`[${ENV}] app listening on port ${PORT}!`);
