2ccf8b0255e8b572adc3c1270cb273fbd856ddbe
EC2 Default User <ec2-user@ip-10-160-7-249.ec2.internal>
2019-02-01 19:24:29 +0000

    Update EC2 DB / deployment scripts to work properly
    The EC2 deployment script isn't complete, but we're getting there.

--- scripts/deployment/ec2.sh
+++ scripts/deployment/ec2.sh
@@ -4,11 +4,17 @@ set -e
 set -o nounset
 
 GIT_ROOT=$(git rev-parse --show-toplevel)
-
+# update to latest code
 cd $GIT_ROOT
-nvm use
-npm install
-pip install -r requirements.txt
 git fetch
 git merge origin/master
-$GIT_ROOT/scripts/db/db-update.sh
+
+# update dependencies
+. ~/.nvm/nvm.sh
+nvm use
+npm install -g forever
+npm install
+pip install -r requirements.txt
+
+# update database
+$GIT_ROOT/scripts/db/db-update-ec2.sh
