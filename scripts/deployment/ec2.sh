731ec9445a39dead3dfba007a7efbbd7918d8f64
EC2 Default User <ec2-user@ip-10-160-7-249.ec2.internal>
2019-02-01 21:14:28 +0000

    Finish up EC2 deployment script
    so that we can one-click deploy this.

--- scripts/deployment/ec2.sh
+++ scripts/deployment/ec2.sh
@@ -4,6 +4,14 @@ set -e
 set -o nounset
 
 GIT_ROOT=$(git rev-parse --show-toplevel)
+WEB_ROOT=/usr/share/nginx/html/flashcrow
+FOREVER_ID=flashcrow-api
+
+# stop application
+if forever list | grep $FOREVER_ID; then
+  forever stop $FOREVER_ID
+fi
+
 # update to latest code
 cd $GIT_ROOT
 git fetch
@@ -16,5 +24,15 @@ npm install -g forever
 npm install
 pip install -r requirements.txt
 
+# build static files into dist
+npm run build
+
+# copy to web root
+sudo rm -r $WEB_ROOT
+sudo cp -r dist $WEB_ROOT
+
 # update database
 $GIT_ROOT/scripts/db/db-update-ec2.sh
+
+# start application again
+NODE_ENV=production forever start $GIT_ROOT/scripts/deployment/forever.json
