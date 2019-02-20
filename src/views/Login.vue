f3ea7ed8d5173105e96cddb8bdd7e7010dc270d9
Evan Savage <esavage@toronto.ca>
2019-02-20 18:00:12 -0500

    OpenID Connect working!
    This breaks RestApi.spec.js, but I don't have the time to debug it -
    I'll need to figure out how to handle that.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,19 +1,7 @@
 <template>
   <div class="login">
     <h1>Sign In</h1>
-    <form action="/flashcrow/api/login" method="POST">
-      <div class="form-group">
-        <label for="username">Username</label>
-        <input type="text" id="username" name="username" required placeholder="Username">
-      </div>
-      <div class="form-group">
-        <label for="password">Password</label>
-        <input type="password" id="password" name="password" required placeholder="Password">
-      </div>
-      <div class="form-group">
-        <input type="submit" value="Sign In">
-      </div>
-    </form>
+    <a href="/flashcrow/api/auth/openid-connect">with OpenID Connect</a>
   </div>
 </template>
 
