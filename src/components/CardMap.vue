0f3c63220d4045402a16b9a42e3412069d5f4218
Evan Savage <esavage@toronto.ca>
2019-03-07 11:26:25 -0500

    Bound and center map on Toronto
    and set some zoom level limits as well.

--- src/components/CardMap.vue
+++ src/components/CardMap.vue
@@ -12,13 +12,25 @@ import GeoStyle from '@/lib/geo/GeoStyle';
 export default {
   name: 'CardMap',
   mounted() {
+    const bounds = new mapboxgl.LngLatBounds(
+      new mapboxgl.LngLat(-79.939957, 43.507725),
+      new mapboxgl.LngLat(-78.852997, 43.899377),
+    );
+    const center = new mapboxgl.LngLat(-79.396477, 43.703871);
     const style = GeoStyle.get();
     Vue.nextTick(() => {
       this.map = new mapboxgl.Map({
+        bounds,
+        center,
         container: this.$el,
         dragRotate: false,
+        maxBounds: bounds,
+        maxZoom: 15,
+        minZoom: 11,
         pitchWithRotate: false,
+        renderWorldCopies: false,
         style,
+        zoom: 11,
       });
     });
   },
