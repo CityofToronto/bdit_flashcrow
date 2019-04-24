3ce03285fa254bcb2699ae291e07c8d2b4469581
Evan Savage <Evan.Savage@toronto.ca>
2019-04-24 19:54:15 +0000

    Urgent state for Confirm step and confirmation modal
    Just some quick-and-dirty copy here.

--- src/components/ModalRequestsNewConfirmation.vue
+++ src/components/ModalRequestsNewConfirmation.vue
@@ -4,8 +4,13 @@
       <h2>Confirmation: Request #1234567890</h2>
     </template>
     <template v-slot:content>
-      <p>
-        Thank you for your request!  You should receive your data by
+      <p>Thank you for your request!</p>
+      <p v-if="data.priority === 'URGENT'">
+        You've marked this request urgent.  The Traffic Safety Unit will
+        contact you to make adjustments to the schedule.
+      </p>
+      <p v-else>
+        You should receive your data by
         <strong>{{data.estimatedDeliveryDate | date}}</strong>.
         The Traffic Safety Unit will contact you if there are unforeseen
         scheduling changes.
