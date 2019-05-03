cb98bb3d73e51e1a58126d66376e19369a316bbb
EC2 Default User <ec2-user@ip-10-160-7-249.ec2.internal>
2019-05-03 19:01:09 +0000

    Use `.pgpass` file plus job param to pass PostgreSQL credentials
    so that we can actually run `psql` from the Docker container here.

--- scripts/concourse/fetch_centreline.sh
+++ scripts/concourse/fetch_centreline.sh
@@ -6,6 +6,9 @@ amazon-linux-extras enable postgresql9.6
 yum clean metadata
 yum install -y postgresql
 
+echo "$PGPASS" > "$HOME/.pgpass"
+chmod 0600 "$HOME/.pgpass"
+
 psql -U candu -h 10.160.12.47 -p 5432 bigdata \
   -c "COPY gis.centreline TO STDOUT (FORMAT text, ENCODING 'UTF8')" \
   > centreline/centreline.txt
