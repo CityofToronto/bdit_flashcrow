8827278349161d6f8500f702666aeec1b7a5a9fd
Evan Savage <Evan.Savage@toronto.ca>
2019-05-15 21:01:22 +0000

    TdsTopBar: lightweight top bar component
    with "left" and "right" areas, separated by a `div.flex-fill` to help push the "right" to, well, the right.  This is useful for showing profile dropdowns at top-right.

---
+++ src/components/tds/TdsTopBar.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="tds-top-bar">
+  <slot name="left"></slot>
+  <div class="flex-fill"></div>
+  <slot name="right"></slot>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'TdsTopBar',
+};
+</script>
+
+<style lang="postcss">
+.tds-top-bar {
+  align-items: center;
+  background-color: var(--base-lighter);
+  display: flex;
+  flex-direction: row;
+  padding: var(--space-m) var(--space-xl);
+  & > * {
+    margin-right: var(--space-m);
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+}
+</style>
