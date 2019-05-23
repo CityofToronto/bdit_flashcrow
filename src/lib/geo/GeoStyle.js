68c586bb10cf0d110639e49c0cbe7720b7e25954
Evan Savage <Evan.Savage@toronto.ca>
2019-05-23 20:48:36 +0000

    Fix duplicate layer ID bug in GeoStyle
    By monkey-patching `GeoStyle.get()` in `PaneMap`, we introduced a bug where hiding then re-showing the map would fail with duplicate layer ID errors.  This changes `GeoStyle.get()` to return a deep clone of `style`.

--- src/lib/geo/GeoStyle.js
+++ src/lib/geo/GeoStyle.js
@@ -1,17 +1,14 @@
 import style from '@/lib/geo/root.json';
 import metadata from '@/lib/geo/metadata.json';
 
-let STYLE = null;
+let ranBuildStyle = null;
 
 /**
  * See http://bl.ocks.org/jgravois/51e2b30e3d6cf6c00f06b263a29108a2 for
  * further details.
  */
 function buildStyle() {
-  STYLE = {};
-  Object.assign(STYLE, style);
-
-  STYLE.sources.esri = {
+  style.sources.esri = {
     type: 'vector',
     scheme: 'xyz',
     tilejson: metadata.tilejson || '2.0.0',
@@ -23,16 +20,15 @@ function buildStyle() {
     description: metadata.description || '',
     name: metadata.name,
   };
-
-  return STYLE;
+  ranBuildStyle = true;
 }
 
 class GeoStyle {
   static get() {
-    if (STYLE === null) {
+    if (!ranBuildStyle) {
       buildStyle();
     }
-    return STYLE;
+    return JSON.parse(JSON.stringify(style));
   }
 }
 
