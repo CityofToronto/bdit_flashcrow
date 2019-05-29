24434d1986ab50118b59e3ac2b7f14933c6ebd2c
Evan Savage <Evan.Savage@toronto.ca>
2019-05-29 14:01:19 +0000

    More form widgets: radio groups, single / range-mode DatePicker, etc.
    Also added a quick-and-dirty columnar layout for our per-study `<fieldset>`s, using `.flex-container-row > .flex-1` :)
    
    Still need to mock up the automatic count, move these out into separate components, and get the form flow together, but we're getting much closer.

--- src/views/FcRequestStudySchedule.vue
+++ src/views/FcRequestStudySchedule.vue
@@ -122,6 +122,106 @@
             <span class="number-icon">1</span>
             Turning Movement Count
           </legend>
+          <div class="flex-container-row">
+            <div class="form-group flex-1">
+              <strong>When do you want your study to be conducted?</strong>
+              <DatePicker
+                v-model="dateRange0"
+                mode="range"
+                name="dateRange0"
+                size="l"
+                v-bind="attrsDueDate">
+              </DatePicker>
+            </div>
+            <div class="form-group flex-1">
+              <strong>What days of the week should the study fall on?</strong>
+              <TdsButtonGroup
+                v-model="daysOfWeek0"
+                name="daysOfWeek0"
+                :options="[
+                  { label: 'Su', value: 0 },
+                  { label: 'M', value: 1 },
+                  { label: 'Tu', value: 2 },
+                  { label: 'W', value: 3 },
+                  { label: 'Th', value: 4 },
+                  { label: 'F', value: 5 },
+                  { label: 'Sa', value: 6 },
+                ]"
+                type="checkbox" />
+            </div>
+          </div>
+          <div class="flex-container-row">
+            <div class="form-group flex-1">
+              <strong>What type of hours should we use?</strong>
+              <TdsRadioGroup
+                v-model="hours0"
+                name="hours0"
+                :options="[
+                  { label: 'School', value: 'SCHOOL' },
+                  { label: 'Routine', value: 'ROUTINE' },
+                  { label: 'Other', value: 'OTHER' },
+                ]" />
+            </div>
+            <div class="form-group flex-1">
+              <strong>&nbsp;</strong>
+              <div
+                v-if="hours0 === 'SCHOOL'"
+                class="tds-panel tds-panel-info">
+                <i class="fa fa-clock"></i>
+                <p>
+                  School hours:
+                </p>
+                <p>
+                  <small>
+                  07:30 &ndash; 09:30<br />
+                  10:00 &ndash; 11:00<br />
+                  12:00 &ndash; 13:30<br />
+                  14:15 &ndash; 15:45<br />
+                  16:00 &ndash; 18:00
+                  </small>
+                </p>
+              </div>
+              <div
+                v-else-if="hours0 === 'ROUTINE'"
+                class="tds-panel tds-panel-info">
+                <i class="fa fa-clock"></i>
+                <p>
+                  Routine hours:
+                </p>
+                <p>
+                  <small>
+                  07:30 &ndash; 09:30<br />
+                  10:00 &ndash; 12:00<br />
+                  13:00 &ndash; 15:00<br />
+                  16:00 &ndash; 18:00
+                  </small>
+                </p>
+              </div>
+              <div
+                v-else-if="hours0 === 'OTHER'"
+                class="tds-panel tds-panel-warning">
+                <i class="fa fa-clock"></i>
+                <p>
+                  Please specify your desired schedule in the notes below.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="flex-container-row">
+            <div class="form-group flex-fill">
+              <strong>Any additional notes you'd like to share?</strong>
+              <textarea
+                v-model="notes0"
+                name="notes0"
+                rows="5"></textarea>
+            </div>
+          </div>
+        </fieldset>
+        <fieldset>
+          <legend>
+            <span class="number-icon">2</span>
+            Speed / Volume ATR
+          </legend>
         </fieldset>
       </div>
     </section>
@@ -139,6 +239,7 @@ import { mapState } from 'vuex';
 import DatePicker from '@/components/DatePicker.vue';
 import TdsButtonGroup from '@/components/tds/TdsButtonGroup.vue';
 import TdsChecklistDropdown from '@/components/tds/TdsChecklistDropdown.vue';
+import TdsRadioGroup from '@/components/tds/TdsRadioGroup.vue';
 import Constants from '@/lib/Constants';
 // import validations from '@/lib/validation/ValidationsStudyRequest';
 
@@ -148,6 +249,7 @@ export default {
     DatePicker,
     TdsButtonGroup,
     TdsChecklistDropdown,
+    TdsRadioGroup,
   },
   data() {
     const { now } = this.$store.state;
@@ -157,12 +259,19 @@ export default {
       now.getDate() + 1,
     );
     return {
+      // top-level meta
       hasServiceRequestId: null,
       serviceRequestId: null,
       priority: null,
       dueDate,
       reasons: [],
       REASONS: Constants.REASONS,
+      // manual studies
+      dateRange0: null,
+      daysOfWeek0: [2, 3, 4],
+      hours0: 'ROUTINE',
+      notes0: '',
+      // automatic counts
     };
   },
   computed: {
@@ -210,8 +319,16 @@ export default {
 
 <style lang="postcss">
 .fc-request-study-schedule {
-  fieldset > legend {
-    padding-left: 0;
+  fieldset {
+    & > legend {
+      padding-left: 0;
+    }
+    & > .flex-container-row {
+      padding: var(--space-m) var(--space-l);
+      & > .form-group {
+        padding: 0 var(--space-l);
+      }
+    }
   }
   .number-icon {
     background-color: var(--white);
