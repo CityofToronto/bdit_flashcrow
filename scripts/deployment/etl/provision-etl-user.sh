02dbc6ef3eb2cae9e7c224d4a365c967473958c6
Evan Savage <Evan.Savage@toronto.ca>
2019-06-07 22:03:42 +0000

    Lots of ETL scripting work
    and a bit of light work on the Show Reports modal.

---
+++ scripts/deployment/etl/provision-etl-user.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# provision-web-user.sh
+#
+# Sets up Airflow-specific user-level packages and directories.
+#
+# This script should *not* contain any calls to sudo.  If you have commands
+# that must be run as root, add them to provision-web-admin.sh.
+
+set -e
+set -o nounset
+
+cd "$(dirname "$0")"
+
+mkdir -p "$HOME/airflow"
+# shellcheck disable=SC2016
+echo 'export AIRFLOW_HOME="$HOME/airflow"' >> "$HOME/.bashrc"
+# shellcheck disable=SC1090
+. "$HOME/.bashrc"
+
+pip install --upgrade pip
+pip install apache-airflow psycopg2 mbutil flask-bcrypt
+
+# create log directories
+echo "creating log directories..."
+mkdir -p "$HOME/log/airflow"
+
+# initdb, first time (to generate directory)
+airflow initdb
+
+# copy airflow.cfg over
+# run airflow_admin_user.py
+
+ln -s /home/ec2-user/flashcrow/scripts/airflow/dags /home/ec2-user/airflow/dags
+
+# copy over systemd files
+sudo cp /home/ec2-user/flashcrow/scripts/airflow/systemd/airflow /etc/sysconfig/airflow
+sudo cp /home/ec2-user/flashcrow/scripts/airflow/systemd/airflow-scheduler.service /usr/lib/systemd/system
+sudo cp /home/ec2-user/flashcrow/scripts/airflow/systemd/airflow-webserver.service /usr/lib/systemd/system
+sudo cp /home/ec2-user/flashcrow/scripts/airflow/systemd/airflow.conf /usr/lib/tmpfiles.d
+sudo systemctl start airflow-webserver
+sudo systemctl start airflow-scheduler
