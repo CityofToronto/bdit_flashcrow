61785ee41198bf548e1503f450f16c5b5db05628
Chelsea Rosic <chelsea.rosic@gmail.com>
2019-07-16 13:47:02 +0000

    created rough queries for crash geocoding
    Started crash geocoding, I have rough queries but am worried that I created too many tables in the process or that I wrote the queries in an inefficient way

---
+++ scripts/airflow/dags/crash_geocoding.py
@@ -0,0 +1,34 @@
+'''
+Crash geocoding DAG
+Match collisions to a centreline street segment or intersection
+'''
+
+from datetime import datetime
+import os
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+AIRFLOW_DAGS = os.path.dirname(os.path.realpath(__file__))
+AIRFLOW_ROOT = os.path.dirname(AIRFLOW_DAGS)
+AIRFLOW_TASKS = os.path.join(AIRFLOW_ROOT, 'tasks')
+
+DEFAULT_ARGS = {
+    'email': ['Chelsea.Rosic@toronto.ca'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'owner': 'ec2-user',
+    'start_date': datetime(2019, 5, 6),
+    'task_concurrency': 1
+}
+
+CRASH_GEOCODING_DAG = DAG(
+    'crash_geocoding',
+    default_args=DEFAULT_ARGS,
+    max_active_runs=1,
+    schedule_interval='30 4 * * *')
+
+CRASH_GEOCODING_SH = os.path.join(AIRFLOW_TASKS, 'crash_geocoding.sh')
+CRASH_GEOCODING = BashOperator(
+    task_id='crash_geocoding',
+    bash_command='{0} '.format(CRASH_GEOCODING_SH),
+    dag=CRASH_GEOCODING_DAG)
---
+++ scripts/airflow/tasks/crash_geocoding/crash_geocoding.sql
@@ -0,0 +1,80 @@
+INSERT INTO spatial_ref_sys (srid, auth_name, auth_srid, proj4text, srtext) values ( 98012, 'sr-org', 8012, '+proj=tmerc +lat_0=0 +lon_0=-79.5 +k=0.9999 +x_0=304800 +y_0=0 +ellps=clrk66 +units=m +no_defs ', 'PROJCS["MTM_3Degree",GEOGCS["GCS_North_American_1927",DATUM["D_North_American_1927",SPHEROID["Clarke_1866",6378206.4,294.9786982]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Transverse_Mercator"],PARAMETER["False_Easting",304800.0],PARAMETER["False_Northing",0.0],PARAMETER["Central_Meridian",-79.5],PARAMETER["Scale_Factor",0.9999],PARAMETER["Latitude_Of_Origin",0.0],UNIT["Meter",1.0]]')
+ON CONFLICT DO NOTHING;
+
+CREATE OR REPLACE FUNCTION "TRAFFIC".get_intersection(collision_lat NUMERIC, collision_long NUMERIC, tbl_name TEXT)
+RETURNS NUMERIC AS $gid$
+DECLARE 
+gid NUMERIC;
+BEGIN
+EXECUTE 
+(CASE WHEN tbl_name LIKE '%intersection%'
+THEN 
+format('SELECT gid
+	FROM %s 
+	WHERE ST_DWithin(
+			st_transform(geom, 98012), 
+			ST_Transform(ST_SetSRID(ST_MakePoint(%s, %s), 4326), 98012), 
+	   		50
+			)
+	AND 
+	(intersec5 NOT LIKE ''%% Trl /%% Trl%%'' OR intersec5 LIKE ''%%/%%/%%'')
+	AND intersec5 NOT LIKE ''%% Trl /%% Trl /%% Trl%%''
+	ORDER BY st_transform(geom, 98012) <-> ST_Transform(ST_SetSRID(ST_MakePoint(%s, %s), 4326), 98012)
+	LIMIT 1'
+	, 
+	tbl_name, collision_long::TEXT, collision_lat::TEXT, collision_long::TEXT, collision_lat::TEXT)
+ELSE 
+
+format(
+	'
+	SELECT gid
+	FROM %s 
+	WHERE ST_DWithin(
+			st_transform(geom, 98012), 
+			ST_Transform(ST_SetSRID(ST_MakePoint(%s, %s), 4326), 98012), 
+			200
+			)
+	AND 
+	fcode_desc::text = ANY(ARRAY[''Collector''::character varying, ''Collector Ramp''::character varying, ''Local''::character varying, ''Major Arterial''::character varying, ''Major Arterial Ramp''::character varying, ''Minor Arterial''::character varying, ''Minor Arterial Ramp''::character varying, ''Pending''::character varying]::text[])
+	ORDER BY st_transform(geom, 98012) <-> ST_Transform(ST_SetSRID(ST_MakePoint(%s, %s), 4326), 98012)
+	LIMIT 1'
+	, 
+	tbl_name, collision_long::TEXT, collision_lat::TEXT, collision_long::TEXT, collision_lat::TEXT)
+END
+)
+INTO gid;
+
+RETURN gid;
+END; 
+$gid$ LANGUAGE plpgsql;
+
+
+CREATE TABLE "TRAFFIC".collision_events_geocoded AS (
+SELECT collision_accnb, collision_date, collision_time, day_no, px, street_1, street_type_1, direction_1, street_2, street_type_2, direction_2,
+    street_3, street_type_3, direction_3, location_class, location_desc, collision_type, impact_type, road_class, visibility, light, 
+    road_surface_cond, longitude, latitude, output_gid_arr[1] AS gid, output_gid_arr[2] AS tbl_type, 	
+	(CASE WHEN output_gid_arr[1] IS NOT NULL AND output_gid_arr[2] = 1
+		THEN (SELECT geom FROM gis.centreline_intersection WHERE gid = output_gid_arr[1])
+		WHEN output_gid_arr[1] IS NOT NULL AND output_gid_arr[2] = 2
+		THEN (SELECT geom FROM gis.centreline WHERE gid = output_gid_arr[1])
+		ELSE NULL
+		END
+		) AS geom 
+	FROM 
+	(
+	SELECT *, 
+			( CASE WHEN geom_int IS NOT NULL 
+					THEN ARRAY[geom_int, 1]::NUMERIC[]
+					ELSE ARRAY["TRAFFIC".get_intersection(latitude::NUMERIC, longitude::NUMERIC, 'gis.centreline'::TEXT), 2]::NUMERIC[]
+					END
+			) AS output_gid_arr
+			FROM 
+			(
+			SELECT DISTINCT ON (collision_accnb, collision_date) 
+				*,
+				"TRAFFIC".get_intersection(latitude::NUMERIC, longitude::NUMERIC, 'gis.centreline_intersection'::TEXT) AS geom_int
+			FROM "TRAFFIC".collision_events
+			 WHERE longitude IS NOT NULL AND latitude IS NOT NULL and collision_accnb IS NOT NULL AND latitude > 43 AND longitude < -79
+			) x
+	) AS output_gid_arr_tbl
+);
