4e05f3145368db011de7ce992153620b6382ca9d
Chelsea Rosic <chelsea.rosic@gmail.com>
2019-07-16 20:22:42 +0000

    Restructured `crash_geocoding.sql` so it is easier to read and understand. Restructured the output table (`collisions.events_geocoded`) to be formatted differently ... I am open to changing the format again though, not sure if this is the best format.

--- scripts/airflow/tasks/crash_geocoding/crash_geocoding.sql
+++ scripts/airflow/tasks/crash_geocoding/crash_geocoding.sql
@@ -1,15 +1,16 @@
--- INSERT INTO spatial_ref_sys (srid, auth_name, auth_srid, proj4text, srtext) values ( 98012, 'sr-org', 8012, '+proj=tmerc +lat_0=0 +lon_0=-79.5 +k=0.9999 +x_0=304800 +y_0=0 +ellps=clrk66 +units=m +no_defs ', 'PROJCS["MTM_3Degree",GEOGCS["GCS_North_American_1927",DATUM["D_North_American_1927",SPHEROID["Clarke_1866",6378206.4,294.9786982]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Transverse_Mercator"],PARAMETER["False_Easting",304800.0],PARAMETER["False_Northing",0.0],PARAMETER["Central_Meridian",-79.5],PARAMETER["Scale_Factor",0.9999],PARAMETER["Latitude_Of_Origin",0.0],UNIT["Meter",1.0]]')
--- ON CONFLICT DO NOTHING;
+DROP TYPE IF EXISTS gid_type CASCADE;
+CREATE TYPE gid_type AS (
+	intersection_gid NUMERIC, 
+	centreline_gid NUMERIC
+);
 
-CREATE OR REPLACE FUNCTION collisions.get_intersection(collision_lat NUMERIC, collision_long NUMERIC, tbl_name TEXT)
-RETURNS NUMERIC AS $gid$
+CREATE OR REPLACE FUNCTION collisions.get_intersection(collision_lat NUMERIC, collision_long NUMERIC, intersection_tbl_name TEXT, centreline_tbl_name TEXT)
+RETURNS gid_type AS $gids$
 DECLARE 
-gid NUMERIC;
+gids gid_type; 
 BEGIN
-EXECUTE 
-(CASE WHEN tbl_name LIKE '%intersection%'
-THEN 
-format('SELECT gid
+
+EXECUTE format('SELECT gid AS intersection_gid, NULL AS centreline_gid
 	FROM %s 
 	WHERE ST_DWithin(
 			st_transform(geom, 26917), 
@@ -20,59 +21,46 @@ format('SELECT gid
 	(intersec5 NOT LIKE ''%% Trl /%% Trl%%'' OR intersec5 LIKE ''%%/%%/%%'')
 	AND intersec5 NOT LIKE ''%% Trl /%% Trl /%% Trl%%''
 	ORDER BY st_transform(geom, 26917) <-> ST_Transform(ST_SetSRID(ST_MakePoint(%s, %s), 4326), 26917)
-	LIMIT 1'
-	, 
-	tbl_name, collision_long::TEXT, collision_lat::TEXT, collision_long::TEXT, collision_lat::TEXT)
-ELSE 
+	LIMIT 1'::TEXT, intersection_tbl_name, collision_long::TEXT, collision_lat::TEXT, collision_long::TEXT, collision_lat::TEXT)
+INTO gids.intersection_gid;
 
-format(
-	'
-	SELECT gid
-	FROM %s 
-	WHERE ST_DWithin(
-			st_transform(geom, 26917), 
-			ST_Transform(ST_SetSRID(ST_MakePoint(%s, %s), 4326), 26917), 
-			200
-			)
-	AND 
-	fcode_desc::text = ANY(ARRAY[''Collector''::character varying, ''Collector Ramp''::character varying, ''Local''::character varying, ''Major Arterial''::character varying, ''Major Arterial Ramp''::character varying, ''Minor Arterial''::character varying, ''Minor Arterial Ramp''::character varying, ''Pending''::character varying]::text[])
-	ORDER BY st_transform(geom, 26917) <-> ST_Transform(ST_SetSRID(ST_MakePoint(%s, %s), 4326), 26917)
-	LIMIT 1'
-	, 
-	tbl_name, collision_long::TEXT, collision_lat::TEXT, collision_long::TEXT, collision_lat::TEXT)
-END
-)
-INTO gid;
+EXECUTE 
+(
+	CASE WHEN (SELECT gids.intersection_gid) IS NOT NULL 
+	THEN 
+	-- since the collision is already assigned to an intersection, dont assign it to a centreline segment
+	'SELECT 0'
+	ELSE 
+	format('
+			SELECT gid
+			FROM %s 
+			WHERE ST_DWithin(
+					st_transform(geom, 26917), 
+					ST_Transform(ST_SetSRID(ST_MakePoint(%s, %s), 4326), 26917), 
+					200
+					)
+			AND 
+			fcode_desc::text = ANY(ARRAY[''Collector''::character varying, ''Collector Ramp''::character varying, ''Local''::character varying, ''Major Arterial''::character varying, ''Major Arterial Ramp''::character varying, ''Minor Arterial''::character varying, ''Minor Arterial Ramp''::character varying, ''Pending''::character varying]::text[])
+			ORDER BY st_transform(geom, 26917) <-> ST_Transform(ST_SetSRID(ST_MakePoint(%s, %s), 4326), 26917)
+			LIMIT 1'::TEXT, centreline_tbl_name, collision_long::TEXT, collision_lat::TEXT, collision_long::TEXT, collision_lat::TEXT)
+	END) 
+INTO gids.centreline_gid;
 
-RETURN gid;
+RETURN gids;
 END; 
-$gid$ LANGUAGE plpgsql;
+$gids$ LANGUAGE plpgsql;
 
 
+DROP TABLE IF EXISTS collisions.events_geocoded; 
 CREATE TABLE collisions.events_geocoded AS (
-SELECT collision_accnb, collision_date, collision_time, day_no, px, longitude, latitude, output_gid_arr[1] AS gid, output_gid_arr[2] AS tbl_type, 	
-	(CASE WHEN output_gid_arr[1] IS NOT NULL AND output_gid_arr[2] = 1
-		THEN (SELECT geom FROM gis.centreline_intersection WHERE gid = output_gid_arr[1])
-		WHEN output_gid_arr[1] IS NOT NULL AND output_gid_arr[2] = 2
-		THEN (SELECT geom FROM gis.centreline WHERE gid = output_gid_arr[1])
-		ELSE NULL
-		END
-		) AS geom 
+SELECT *
 	FROM 
 	(
-	SELECT *, 
-			( CASE WHEN geom_int IS NOT NULL 
-					THEN ARRAY[geom_int, 1]::NUMERIC[]
-					ELSE ARRAY["TRAFFIC".get_intersection(latitude::NUMERIC, longitude::NUMERIC, 'gis.centreline'::TEXT), 2]::NUMERIC[]
-					END
-			) AS output_gid_arr
-			FROM 
-			(
-			SELECT DISTINCT ON (collision_id, collision_accnb, collision_date, collision_time) 
-				*,
-				collisions.get_intersection(latitude::NUMERIC, longitude::NUMERIC, 'gis.centreline_intersection'::TEXT) AS geom_int
-			FROM collisions.events
-			 WHERE longitude IS NOT NULL AND latitude IS NOT NULL and collision_accnb IS NOT NULL AND latitude > 43 AND longitude < -79
-			) x
-	) AS output_gid_arr_tbl
+		SELECT DISTINCT ON (collision_id, collision_accnb, day_no, collision_time) *
+		FROM 
+			collisions.events
+			-- where I go inpsiration for the composite type: https://stackoverflow.com/questions/4284762/postgresql-select-from-function-that-returns-composite-type
+			JOIN LATERAL collisions.get_intersection(latitude::NUMERIC, longitude::NUMERIC, 'gis.centreline_intersection'::TEXT, 'gis.centreline'::TEXT) AS gids ON TRUE
+		WHERE longitude IS NOT NULL AND latitude IS NOT NULL and collision_accnb IS NOT NULL AND latitude > 43 AND longitude < -79
+		) x
 );
