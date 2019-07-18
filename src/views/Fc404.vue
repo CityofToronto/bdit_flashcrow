6a2099f42065187f7355c72df1e15c3583945778
Evan Savage <Evan.Savage@toronto.ca>
2019-07-18 18:54:30 +0000

    History mode for Vue Router
    so that we can get rid of that `#/` hash part.

---
+++ src/views/Fc404.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="fc-404">
+    <h2>HTTP 404 Not Found</h2>
+    <p>
+      Click
+      <router-link :to="{ name: 'home' }">
+        here
+      </router-link>
+      to return home, or use the sidebar links to return
+      to other parts of MOVE.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Fc404',
+};
+</script>
+
+<style lang="postcss">
+.fc-404 {
+
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,7 @@ const cert = fs.readFileSync(path.join(__dirname, 'ssl', 'localhost.crt'));
 module.exports = {
   publicPath: '/',
   devServer: {
+    historyApiFallback: true,
     host: '0.0.0.0',
     https: { key, cert },
     proxy: {
