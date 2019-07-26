7234e819477cfa86c73370233e72b58494818790
Evan Savage <Evan.Savage@toronto.ca>
2019-07-26 17:55:17 +0000

    Fix bug in UserDAO.create()
    The optional third parameter to `db.query()` is a `QueryResultMask`, not a function - hence the error message.

--- tests/jest/db/UserDAO.spec.js
+++ tests/jest/db/UserDAO.spec.js
@@ -12,10 +12,14 @@ const NEW_EMAIL = 'foo2@toronto.ca';
 const NEW_TOKEN = 'tokenFoo2';
 
 test('UserDAO works properly', async () => {
+  // TODO: remove this once we have standalone DB testing harness
+  await UserDAO.delete(USER);
+
   await expect(UserDAO.bySubject(USER.subject)).resolves.toBeNull();
   await expect(UserDAO.delete(USER)).resolves.toEqual(false);
   await expect(UserDAO.update(USER)).resolves.toEqual(false);
-  await expect(UserDAO.create(USER)).resolves.toEqual(USER.subject);
+  const userCreated = await UserDAO.create(USER);
+  expect(userCreated.subject).toEqual(USER.subject);
   await expect(UserDAO.bySubject(USER.subject)).resolves.toEqual(USER);
   Object.assign(USER, {
     email: NEW_EMAIL,
