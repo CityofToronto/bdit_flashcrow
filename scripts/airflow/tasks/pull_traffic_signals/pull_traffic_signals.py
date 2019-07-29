5753c97c5db4e921fa144005699b4ba6af3e991d
Chelsea Rosic <chelsea.rosic@gmail.com>
2019-07-29 17:33:01 +0000

    airflow pipeline to pull traffic signal and ped crossing data is now functional #135

--- scripts/airflow/tasks/pull_traffic_signals/pull_traffic_signals.py
+++ scripts/airflow/tasks/pull_traffic_signals/pull_traffic_signals.py
@@ -1,7 +1,6 @@
 '''
 Pull traffic signals (including pedestrian crossings) from Open Data API
 '''
-from time import sleep
 import psycopg2
 from psycopg2.extras import execute_values
 import requests
@@ -19,32 +18,34 @@ def create_tables():
             DROP TABLE IF EXISTS %s;
             CREATE TABLE %s
             (
-                px TEXT, 
+                px TEXT,
                 main TEXT,
                 midblock TEXT,
                 side1 TEXT,
                 side2 TEXT,
                 private_access TEXT,
                 additional_info TEXT,
-                geo_id TEXT, 
+                geo_id TEXT,
                 nodeid TEXT,
                 x NUMERIC,
                 y NUMERIC,
                 lat NUMERIC,
                 long NUMERIC,
-                activation_date TEXT, 
-                signal_system TEXT, 
-                non_system TEXT, 
+                activation_date TEXT,
+                signal_system TEXT,
+                non_system TEXT,
                 mode_of_control TEXT,
                 ped_walk_speed TEXT,
-                aps NUMERIC,
+                aps_signal TEXT,
+                aps_operation TEXT,
+                aps_activation_date TEXT,
                 transit_preempt NUMERIC,
                 fire_preempt NUMERIC,
                 rail_preempt NUMERIC,
-                no_of_signalized_approaches NUMERIC, 
-                ups NUMERIC, 
+                no_of_signalized_approaches NUMERIC,
+                ups NUMERIC,
                 led_blackout_sign NUMERIC,
-                leading_pedestrian_intervals NUMERIC, 
+                leading_pedestrian_intervals NUMERIC,
                 bicycle_signal NUMERIC
             );
             """ % (signal_tablename, signal_tablename)
@@ -56,14 +57,14 @@ def create_tables():
             CREATE TABLE %s
             (
                 id NUMERIC,
-                px TEXT, 
+                px TEXT,
                 main TEXT,
                 midblock TEXT,
                 side1 TEXT,
                 side2 TEXT,
                 private_access TEXT,
                 additional_info TEXT,
-                geo_id TEXT, 
+                geo_id TEXT,
                 nodeid TEXT,
                 x NUMERIC,
                 y NUMERIC,
@@ -77,36 +78,20 @@ def create_tables():
 
   CONN.commit()
 
-def insert_into_table(output_table, params):
+def insert_into_table(output_table, file_id, name):
   '''
   Pull from Open Data API and insert the JSON into DB
   '''
 
-  result = requests.get(
-    "https://ckan0.cf.opendata.inter.sandbox-toronto.ca/api/3/action/package_show",
-    params)
+  url = "https://ckanadmin0.intra.prod-toronto.ca/dataset/1a106e88-f734-4179-b3fe-d690a6187a71" + \
+        "/resource/" + file_id + "/download/" + name
+  return_json = requests.get(url).json()
 
-  while True:
-    try:
-      result = requests.get(
-        "http://ckan0.cf.opendata.inter.sandbox-toronto.ca/api/3/action/package_show",
-        params)
-    except requests.exceptions.ConnectionError:
-      sleep(10)
-      continue
-    else:
-      return_json = result.json()
-      break
-
-  return_json = result.json()
   rows = []
-  features = return_json['features']
-  fields = return_json['fields']
-  trials = [[field['name'], field['type']] for field in fields]
-  for feature in features:
-    row = [feature['attributes'][trial[0]] for trial in trials]
+  for feature in return_json:
+    row = [x for x in feature.values()]
     rows.append(row)
-  insert = 'INSERT INTO _{} VALUES %s'.format(output_table)
+  insert = 'INSERT INTO {} VALUES %s'.format(output_table)
   with CONN:
     with CONN.cursor() as cur:
       execute_values(cur, insert, rows)
@@ -122,7 +107,12 @@ if __name__ == "__main__":
     raise Exception('Could not connect to Flashcrow DB')
 
   create_tables()
-  insert_into_table("gis.traffic_signals", {"id": "0d9b9ab2-0f81-42b1-9274-59875f3feda0"})
-  insert_into_table('gis.pedestrian_crossings', {'id': '81a6a164-3824-4e6d-80ca-b67502fea264'})
-
+  insert_into_table("gis.traffic_signals",
+                    "656fdd0a-f5a2-4936-a02c-62c5a250d38e",
+                    "traffic-signals-all-version-2-json.json"
+                    )
+  insert_into_table("gis.traffic_signals",
+                    "17d0fd03-c1b5-410f-9dd0-7837d28ac0a7",
+                    "pedestrian-crossovers-version-2-json.json"
+                    )
   CONN.close()
