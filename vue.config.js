78da86ba6f4569bf0df6fe3ffef4e794275eb8cf
Evan Savage <Evan.Savage@toronto.ca>
2019-08-15 14:33:25 +0000

    Switch to ES6 modules across backend libs
    This completes the switch to ES6, which turned out to be relatively painless.

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,10 @@ const path = require('path');
 const key = fs.readFileSync(path.join(__dirname, 'ssl', 'localhost.key'));
 const cert = fs.readFileSync(path.join(__dirname, 'ssl', 'localhost.crt'));
 
+/**
+ * Vue configuration.  Note that this is the only file in CommonJS module style;
+ * `vue-cli-service` balks at running ES6 modules.
+ */
 module.exports = {
   publicPath: '/',
   devServer: {
