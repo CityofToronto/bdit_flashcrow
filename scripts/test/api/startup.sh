c93f68778a8415cc9b68d200c9f6c5df1be53877
Evan Savage <Evan.Savage@toronto.ca>
2019-08-29 19:28:58 +0000

    Auto-startup / auto-shutdown for REST API tests
    so that devs don't have to have the server running separately.

--- scripts/test/api/startup.sh
+++ scripts/test/api/startup.sh
@@ -4,12 +4,19 @@ set -euo pipefail
 
 GIT_ROOT=$(realpath "$(dirname "$0")"/../../..)
 DB_STARTUP_SCRIPT="${GIT_ROOT}/scripts/test/db/startup.sh"
+PID_FILE="${GIT_ROOT}/scripts/test/api/.pid"
 
 "${DB_STARTUP_SCRIPT}" --withDevData
 
-# TODO: actually start servers if not running?
+rm -f "${PID_FILE}"
 
 if ! lsof -t -i :8080 -s TCP:LISTEN; then
-  echo "backend not running!"
-  exit 1
+  echo "backend not running, starting new instance..."
+  npm run backend:test-api > /dev/null 2>&1 &
+  while ! lsof -t -i :8080 -s TCP:LISTEN; do
+    echo "waiting for backend..."
+    sleep 1
+  done
+  lsof -t -i :8080 -s TCP:LISTEN > "${PID_FILE}"
+  echo "backend started."
 fi
