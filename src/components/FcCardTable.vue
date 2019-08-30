08a1304219fe5a1d1031d496ed3405b400c9c352
Evan Savage <Evan.Savage@toronto.ca>
2019-08-30 19:45:10 +0000

    Visual highlighting and notification for FcCardTableCounts
    so that it's a bit more obvious that parts of this do things.

--- src/components/FcCardTable.vue
+++ src/components/FcCardTable.vue
@@ -249,8 +249,16 @@ export default {
         border-bottom: var(--border-default);
       }
     }
-    &.expanded > tr:first-child {
-      background-color: var(--base-lighter);
+    &.expanded {
+      &:hover {
+        background-color: var(--white);
+        & > tr:first-child {
+          background-color: var(--primary-lighter);
+        }
+      }
+      & > tr:first-child {
+        background-color: var(--base-lighter);
+      }
     }
 
     &:hover {
