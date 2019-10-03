b8f24cf5e9806547ba3f52398ade48e4c268ec63
Evan Savage <Evan.Savage@toronto.ca>
2019-10-03 20:52:11 +0000

    Test Intersection Warrant Summary report against legacy values
    so that we can double-check that the logic does the right thing on actual data.
    
    Rather than integrate this with the database layer, we dump JSON input (from our `/count/data` endpoint in the MOVE web backend) and create JSON output (from looking at legacy reports), and test the logic in isolation from there.
    
    We can extend this same strategy to the other report types, to ensure that we have at least some coverage here.

--- tests/jest/unit/reports/ReportIntersectionWarrantSummary.spec.js
+++ tests/jest/unit/reports/ReportIntersectionWarrantSummary.spec.js
@@ -1,8 +1,14 @@
+/* eslint-disable camelcase */
+import { CardinalDirection } from '@/lib/Constants';
 import ReportIntersectionWarrantSummary from '@/lib/reports/ReportIntersectionWarrantSummary';
 import {
   generateMajorAndMinorDirections,
   generateTmc,
 } from '@/lib/test/random/CountDataGenerator';
+
+import countData_5_38661 from './data/countData_5_38661.json';
+import transformedData_INTERSECTION_WARRANT_SUMMARY_5_38661 from
+  './data/transformedData_INTERSECTION_WARRANT_SUMMARY_5_38661.json';
 
 test('ReportIntersectionWarrantSummary#transformData', () => {
   const reportInstance = new ReportIntersectionWarrantSummary();
@@ -19,4 +25,34 @@ test('ReportIntersectionWarrantSummary#transformData', () => {
       });
     }).not.toThrow();
   }
+});
+
+test('ReportIntersectionWarrantSummary#transformData [Overlea and Thorncliffe: 5/38661]', () => {
+  const reportInstance = new ReportIntersectionWarrantSummary();
+
+  const countData = countData_5_38661.map(({
+    id,
+    countId,
+    t,
+    data,
+  }) => ({
+    id,
+    countId,
+    t: new Date(t.slice(0, -1)),
+    data,
+  }));
+  const majorDirections = [CardinalDirection.EAST, CardinalDirection.WEST];
+  const minorDirections = [CardinalDirection.NORTH, CardinalDirection.SOUTH];
+
+  const { hourlyTotals, totals } = reportInstance.transformData({
+    countData,
+    majorDirections,
+    minorDirections,
+  });
+  expect(hourlyTotals).toEqual(
+    transformedData_INTERSECTION_WARRANT_SUMMARY_5_38661.data.hourlyTotals,
+  );
+  expect(totals).toEqual(
+    transformedData_INTERSECTION_WARRANT_SUMMARY_5_38661.data.totals,
+  );
 });
