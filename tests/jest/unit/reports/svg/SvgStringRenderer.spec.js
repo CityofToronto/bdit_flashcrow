02a8b97fd15f9044501ac9eb3cebdf7e7e87092b
Evan Savage <Evan.Savage@toronto.ca>
2019-11-05 19:07:01 +0000

    SvgStringRenderer test
    Easy enough: we set up a couple of `SvgGenerator` subclasses.

---
+++ tests/jest/unit/reports/svg/SvgStringRenderer.spec.js
@@ -0,0 +1,53 @@
+import SvgGenerator from '@/lib/reports/svg/SvgGenerator';
+import SvgStringRenderer from '@/lib/reports/svg/SvgStringRenderer';
+import 'd3-selection-multi';
+
+/* eslint-disable class-methods-use-this */
+class NoopSvgGenerator extends SvgGenerator {
+  init() {}
+
+  update() {}
+}
+
+class SimpleSvgGenerator extends SvgGenerator {
+  constructor(container, options) {
+    super(container, options);
+    this.i = 0;
+  }
+
+  init() {
+    this.group = this.svg.append('g');
+  }
+
+  update() {
+    this.data[this.i] += 1;
+    this.i = (this.i + 1) % this.data.length;
+
+    const text = this.group.selectAll('text.foo')
+      .data(this.data);
+
+    text.exit().remove();
+
+    text.enter().append('text')
+      .attr('class', 'foo')
+      .merge(text)
+      .text(d => d);
+  }
+}
+
+test('SvgStringRenderer.render', () => {
+  expect(SvgStringRenderer.render(NoopSvgGenerator, {
+    aspectRatio: 2,
+    width: 640,
+    height: 320,
+  })).toEqual(
+    '<svg width="640" height="320" viewBox="0 0 960 480" xmlns="http://www.w3.org/2000/svg"></svg>',
+  );
+  expect(SvgStringRenderer.render(SimpleSvgGenerator, {
+    data: [0, 0, 0],
+    width: 800,
+    height: 450,
+  })).toEqual(
+    '<svg width="800" height="450" viewBox="0 0 960 540" xmlns="http://www.w3.org/2000/svg"><g><text class="foo">1</text><text class="foo">0</text><text class="foo">0</text></g></svg>',
+  );
+});
