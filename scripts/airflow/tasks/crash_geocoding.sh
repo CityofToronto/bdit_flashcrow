20aadec8be09003af16f802c372562fa9cbdb23f
EC2 Default User <ec2-user@ip-10-162-43-198.ca-central-1.compute.internal>
2019-11-08 20:10:13 +0000

    Get rid of PostgreSQL hostname in other Airflow scripts
    except `traffic_signals.py`, where we can't just pull the `env $(xargs)` trick.

--- scripts/airflow/tasks/crash_geocoding.sh
+++ scripts/airflow/tasks/crash_geocoding.sh
@@ -5,4 +5,4 @@ set -eu
 # shellcheck disable=SC2086
 cd $(dirname $0)
 
-psql -v ON_ERROR_STOP=1 -U flashcrow -h fr194ibxx9jxbj3.ccca5v4b7zsj.us-east-1.rds.amazonaws.com -p 5432 flashcrow < crash_geocoding/crash_geocoding.sql
+env $(xargs < "/home/ec2-user/cot-env.config") psql -v ON_ERROR_STOP=1 < crash_geocoding/crash_geocoding.sql
