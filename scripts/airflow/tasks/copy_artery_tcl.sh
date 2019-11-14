8ebee848e930d696083ce12b8f56666e44a4ac65
EC2 Default User <ec2-user@ip-10-162-43-198.ca-central-1.compute.internal>
2019-11-14 19:56:07 +0000

    Port @aharpalaniTO's arterycode-intersection matching query over to Airflow
    Made one small tweak to trim down what we're fetching from `traffic_signals`, and renamed a few things to work with our database setup, but otherwise this was basically a copy-paste operation :)

--- scripts/airflow/tasks/copy_artery_tcl.sh
+++ scripts/airflow/tasks/copy_artery_tcl.sh
@@ -5,8 +5,7 @@ GIT_ROOT=/home/ec2-user/flashcrow
 TASKS_ROOT="${GIT_ROOT}/scripts/airflow/tasks"
 
 # TODO: re-enable when RDS access is unblocked
-# env $(xargs < "/home/ec2-user/cot-env.config") psql -v ON_ERROR_STOP=1 < "${TASKS_ROOT}/copy_artery_tcl/copy_artery_tcl.sql"
+env $(xargs < "/home/ec2-user/cot-env.config") psql -v ON_ERROR_STOP=1 < "${TASKS_ROOT}/copy_artery_tcl/copy_artery_tcl.sql"
 
 # TODO: re-enable when RDS access is unblocked
 # pg_dump -U candu -h 10.160.12.47 -p 5432 -t "prj_volume.artery_tcl" -x --no-owner --clean --if-exists bigdata | env $(xargs < "/home/ec2-user/cot-env.config") psql -v ON_ERROR_STOP=1
-# psql -U candu -h 10.160.12.47 -p 5432 bigdata -c "COPY (SELECT * FROM prj_volume.artery_intersections) TO STDOUT (FORMAT text, ENCODING 'UTF-8')" | env $(xargs < "/home/ec2-user/cot-env.config") psql -v ON_ERROR_STOP=1 -c "COPY prj_volume.artery_intersections FROM STDIN (FORMAT text, ENCODING 'UTF-8')"
--- scripts/airflow/tasks/copy_artery_tcl/copy_artery_tcl.sql
+++ scripts/airflow/tasks/copy_artery_tcl/copy_artery_tcl.sql
@@ -1,10 +1,84 @@
-create schema if not exists prj_volume;
-create table if not exists prj_volume.artery_intersections (
-  arterycode bigint,
-  int_id double precision,
-  location varchar,
-  px integer,
-  latitude double precision,
-  longitude double precision
+DROP TABLE IF EXISTS prj_volume.artery_intersections;
+CREATE TABLE prj_volume.artery_intersections AS (
+  WITH initial AS (
+    WITH ind AS (
+      SELECT
+        x.arterycode,
+        x.int_id,
+        sum(x.num_points) AS total_points
+      FROM (
+        SELECT
+          a_2.arterycode,
+          c_1.fnode AS int_id,
+          count(1) AS num_points
+        FROM prj_volume.artery_tcl a_2
+        JOIN "TRAFFIC"."ARTERYDATA" b_2 ON a_2.arterycode = b_2."ARTERYCODE"
+        JOIN gis.centreline c_1 ON a_2.centreline_id = c_1.geo_id
+        WHERE a_2.artery_type = 2
+        GROUP BY a_2.arterycode, c_1.fnode
+      UNION ALL
+        SELECT
+          a_2.arterycode,
+          c_1.tnode AS int_id,
+          count(1) AS num_points
+        FROM prj_volume.artery_tcl a_2
+        JOIN "TRAFFIC"."ARTERYDATA" b_2 ON a_2.arterycode = b_2."ARTERYCODE"
+        JOIN gis.centreline c_1 ON a_2.centreline_id = c_1.geo_id
+        WHERE a_2.artery_type = 2
+        GROUP BY a_2.arterycode, c_1.tnode
+      ) x
+      GROUP BY x.arterycode, x.int_id
+      ORDER BY x.arterycode
+    )
+    SELECT
+      a_1.arterycode,
+      a_1.int_id,
+      d.location,
+      c.px,
+      f.px AS px_secondary,
+      e.latitude,
+      e.longitude,
+      CASE
+        WHEN f.px IS NOT NULL THEN 1::double precision
+        ELSE a_1.int_id
+      END AS ranking
+    FROM ind a_1
+    JOIN (
+      SELECT
+        ind.arterycode,
+        max(ind.total_points) AS max_points
+      FROM ind
+      GROUP BY ind.arterycode
+    ) b_1 ON a_1.arterycode = b_1.arterycode AND b_1.max_points = a_1.total_points
+    LEFT JOIN (
+      SELECT
+        "ARTERYCODE" AS arterycode,
+        (regexp_matches("LOCATION"::text, 'PX (\d+)'::text))[1] AS px
+      FROM "TRAFFIC"."ARTERYDATA"
+    ) c ON a_1.arterycode = c.arterycode
+    LEFT JOIN (
+      SELECT
+        "ARTERYCODE" AS arterycode,
+        "LOCATION" AS location
+      FROM "TRAFFIC"."ARTERYDATA"
+    ) d ON a_1.arterycode = d.arterycode
+    LEFT JOIN gis.centreline_intersection e USING (int_id)
+    LEFT JOIN gis.traffic_signal f ON e.int_id = f.node_id AND f.node_id <> 0 AND c.px = f.px
+    ORDER BY a_1.arterycode
+  )
+  SELECT
+    a.arterycode,
+    a.int_id,
+    a.location,
+    a.px,
+    a.latitude,
+    a.longitude
+  FROM initial a
+  JOIN (
+    SELECT
+      initial.arterycode,
+      min(initial.ranking) AS ranking
+    FROM initial
+    GROUP BY initial.arterycode
+  ) b USING (arterycode, ranking)
 );
-truncate table prj_volume.artery_intersections restart identity;
