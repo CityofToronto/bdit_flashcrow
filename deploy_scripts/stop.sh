10c1a2ac9d93069d13cc2964de2a73e1970c15e6
Evan Savage <Evan.Savage@toronto.ca>
2019-11-28 22:02:43 +0000

    Another small change
    to trigger a new deployment.

--- deploy_scripts/stop.sh
+++ deploy_scripts/stop.sh
@@ -2,7 +2,10 @@
 
 set -euo pipefail
 
-nvm use lts/*
+set +u
+source /home/ec2-user/.bash_profile
+set -u
+
 if NODE_ENV=production forever list | grep 'No forever'; then
     echo "forever is not running."
 else
