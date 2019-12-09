5989d6a32932fa196d2dbb50df1866e07f2e925a
EC2 Default User <ec2-user@ip-10-162-43-198.ca-central-1.compute.internal>
2019-12-09 20:51:31 +0000

    Fix path to collisions.json
    to refer to `/data/tiles` like the rest of this script.

--- scripts/airflow/tasks/build_collisions_tiles.sh
+++ scripts/airflow/tasks/build_collisions_tiles.sh
@@ -7,4 +7,4 @@ TASKS_ROOT="${GIT_ROOT}/scripts/airflow/tasks"
 mkdir -p /data/tiles
 env $(xargs < "/home/ec2-user/cot-env.config") psql -v ON_ERROR_STOP=1 -f "${TASKS_ROOT}/build_collisions_tiles/download_collisions.sql" > /data/tiles/collisions.json
 
-tippecanoe --force -o /data/tiles/collisions.mbtiles --accumulate-attribute=heatmap_weight:sum --cluster-densest-as-needed -r1 -Z10 -z15 collisions.json
+tippecanoe --force -o /data/tiles/collisions.mbtiles --accumulate-attribute=heatmap_weight:sum --cluster-densest-as-needed -r1 -Z10 -z15 /data/tiles/collisions.json
