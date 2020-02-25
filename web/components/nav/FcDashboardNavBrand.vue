5e19c6e0b8e461108c8ab8bfb81799177708ad89
Evan Savage <Evan.Savage@toronto.ca>
2020-02-25 19:46:56 +0000

    Remove tooltip from MOVE
    since it underflows `FcSearchBarLocation` on View Map.

--- web/components/nav/FcDashboardNavBrand.vue
+++ web/components/nav/FcDashboardNavBrand.vue
@@ -1,17 +1,12 @@
 <template>
-  <v-tooltip right>
-    <template v-slot:activator="{ on }">
-      <div
-        class="pa-2 pt-3"
-        v-on="on">
-        <h1 class="sr-only">MOVE</h1>
-        <v-img
-          alt="MOVE Logo"
-          src="/logo_square.png"></v-img>
-      </div>
-    </template>
-    <span>MOVE</span>
-  </v-tooltip>
+  <div
+    class="pa-2 pt-3"
+    v-on="on">
+    <h1 class="sr-only">MOVE</h1>
+    <v-img
+      alt="MOVE Logo"
+      src="/logo_square.png"></v-img>
+  </div>
 </template>
 
 <script>
