2a91598e67ad2235d3fb7231788c5a65ca9888bd
EC2 Default User <ec2-user@ip-10-162-43-198.ca-central-1.compute.internal>
2020-03-04 21:15:14 +0000

    Update intersections configuration, reduce progress output from tippecanoe
    in preparation for user testing.

--- scripts/airflow/tasks/build_collisions_tiles.sh
+++ scripts/airflow/tasks/build_collisions_tiles.sh
@@ -8,5 +8,5 @@ mkdir -p /data/tiles
 env $(xargs < "/home/ec2-user/cot-env.config") psql -v ON_ERROR_STOP=1 -f "${TASKS_ROOT}/build_collisions_tiles/download_collisionsLevel3.sql" > /data/tiles/collisionsLevel3.json
 env $(xargs < "/home/ec2-user/cot-env.config") psql -v ON_ERROR_STOP=1 -f "${TASKS_ROOT}/build_collisions_tiles/download_collisionsLevel2.sql" > /data/tiles/collisionsLevel2.json
 
-tippecanoe --force -o /data/tiles/collisionsLevel3.mbtiles -l collisionsLevel3 -Z10 -z16 --accumulate-attribute=heatmap_weight:sum --cluster-densest-as-needed -r1 /data/tiles/collisionsLevel3.json
-tippecanoe --force -o /data/tiles/collisionsLevel2.mbtiles -l collisionsLevel2 -Z14 -z16 /data/tiles/collisionsLevel2.json
+tippecanoe --progress-interval=10 --force -o /data/tiles/collisionsLevel3.mbtiles -l collisionsLevel3 -Z10 -z16 --accumulate-attribute=heatmap_weight:sum --cluster-densest-as-needed -r1 /data/tiles/collisionsLevel3.json
+tippecanoe --progress-interval=10 --force -o /data/tiles/collisionsLevel2.mbtiles -l collisionsLevel2 -Z14 -z16 /data/tiles/collisionsLevel2.json
--- scripts/airflow/tasks/build_vector_tiles.sh
+++ scripts/airflow/tasks/build_vector_tiles.sh
@@ -8,5 +8,5 @@ mkdir -p /data/tiles
 env $(xargs < "/home/ec2-user/cot-env.config") psql -v ON_ERROR_STOP=1 -f "${TASKS_ROOT}/build_vector_tiles/download_midblocks.sql" > /data/tiles/midblocks.json
 env $(xargs < "/home/ec2-user/cot-env.config") psql -v ON_ERROR_STOP=1 -f "${TASKS_ROOT}/build_vector_tiles/download_intersections.sql" > /data/tiles/intersections.json
 
-tippecanoe --force -o /data/tiles/midblocks.mbtiles -l midblocks -Z10 -z19 -J "${TASKS_ROOT}/build_vector_tiles/filter_midblocks.json" /data/tiles/midblocks.json
-tippecanoe --force -o /data/tiles/intersections.mbtiles -l intersections -Z14 -z19 -J "${TASKS_ROOT}/build_vector_tiles/filter_intersections.json" /data/tiles/intersections.json
+tippecanoe --progress-interval=10 --force -o /data/tiles/midblocks.mbtiles -l midblocks -Z10 -z19 -J "${TASKS_ROOT}/build_vector_tiles/filter_midblocks.json" /data/tiles/midblocks.json
+tippecanoe --progress-interval=10 --force -o /data/tiles/intersections.mbtiles -l intersections -Z10 -B12 -z19 -J "${TASKS_ROOT}/build_vector_tiles/filter_intersections.json" /data/tiles/intersections.json
