e8ca1b881af5e69ef4f9ca3eebfe7eda75010d2a
EC2 Default User <ec2-user@ip-10-162-43-198.ca-central-1.compute.internal>
2020-03-23 15:59:05 +0000

    Add hospitals to GIS layers copy and vector tiles ETL jobs
    This only generates level 2 tiles - for the level 1 tiles, we need to update `dump_dev_data` to include hospitals.
    
    Looking further at our latest dumps, though, we're actually exporting a number of materialized views - which means we get the view definition, not the underlying data!  We'll need to update the dev data dump ETL job to ensure we're getting proper datasets here.

---
+++ scripts/airflow/tasks/build_gis_layers_tiles/download_hospitalsLevel2.sql
@@ -0,0 +1,24 @@
+COPY (
+WITH features AS (
+  SELECT
+    objectid AS "id",
+    geom,
+    stn_name AS "name",
+    general_use_code AS "hospitalType"
+  FROM gis.hospital
+),
+geojson_features AS (
+  SELECT jsonb_build_object(
+    'type', 'Feature',
+    'id', id,
+    'geometry', ST_AsGeoJSON(geom)::jsonb,
+    'properties', to_jsonb(features.*) - 'id' - 'geom'
+  ) AS feature
+  FROM features
+)
+SELECT jsonb_build_object(
+  'type', 'FeatureCollection',
+  'features', jsonb_agg(feature)
+) AS feature_collection
+FROM geojson_features
+) TO STDOUT WITH (HEADER FALSE);
--- scripts/airflow/tasks/extract_gis_layers_tiles.sh
+++ scripts/airflow/tasks/extract_gis_layers_tiles.sh
@@ -2,5 +2,8 @@
 
 set -euo pipefail
 
+rm -rf /data/tiles/hospitalsLevel2
+mb-util --image_format=pbf --silent /data/tiles/hospitalsLevel2.mbtiles /data/tiles/hospitalsLevel2
+
 rm -rf /data/tiles/schoolsLevel2
 mb-util --image_format=pbf --silent /data/tiles/schoolsLevel2.mbtiles /data/tiles/schoolsLevel2
