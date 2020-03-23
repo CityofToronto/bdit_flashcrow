55d9870923af721cfc8fcbd0a470d545a2d7d846
EC2 Default User <ec2-user@ip-10-162-43-198.ca-central-1.compute.internal>
2020-03-23 15:38:07 +0000

    Stub DAG for multiday counts
    I'll come back to this after a bunch of other work.

---
+++ scripts/airflow/tasks/transform_multiday_counts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -euo pipefail
+GIT_ROOT=/home/ec2-user/flashcrow
+TASKS_ROOT="${GIT_ROOT}/scripts/airflow/tasks"
+
+env $(xargs < "/home/ec2-user/cot-env.config") psql -v ON_ERROR_STOP=1 < "${TASKS_ROOT}/transform_multiday_counts/transform_multiday_counts.sql"
