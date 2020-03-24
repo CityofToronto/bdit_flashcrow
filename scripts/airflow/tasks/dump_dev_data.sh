da8349a60ba287377284ae7c5919782b340210a4
EC2 Default User <ec2-user@ip-10-162-43-198.ca-central-1.compute.internal>
2020-03-24 14:28:34 +0000

    Update dev dataset to accommodate view-based pipelines
    For views, `pg_dump` dumps the view definition (i.e. the query that creates the view), not a table schema.  To get around this, we use `CREATE TABLE ... (LIKE ... INCLUDING INDEXES)` and some `sed` magic to generate table definitions that match the set of columns and indexes from these views.
    
    We can then use `COPY (SEECT* FROM ...)` commands as before to copy data out of these views.

--- scripts/airflow/tasks/dump_dev_data.sh
+++ scripts/airflow/tasks/dump_dev_data.sh
@@ -9,21 +9,60 @@ FLASHCROW_DEV_DATA=/data/dev_data/flashcrow-dev-data.sql
 mkdir -p /data/dev_data
 cp "${TASKS_ROOT}/dump_dev_data/create_schemas.sql" "${FLASHCROW_DEV_DATA}"
 
-# copy full tables
-env $(xargs < "/home/ec2-user/cot-env.config") pg_dump -t "gis.centreline" -t "gis.centreline_intersection" -t "gis.school" -t "prj_volume.artery_centreline" -t '"TRAFFIC"."CATEGORY"' -t '"TRAFFIC"."ARTERYDATA"' -x --no-owner --clean --if-exists >> "${FLASHCROW_DEV_DATA}"
+# copy schemas for unsampled views
+env $(xargs < "/home/ec2-user/cot-env.config") pg_dump -t flashcrow_dev_data.gis_centreline -x --no-owner --clean --if-exists --schema-only | sed "s/flashcrow_dev_data.gis_centreline/gis.centreline/g" >> "${FLASHCROW_DEV_DATA}"
+env $(xargs < "/home/ec2-user/cot-env.config") pg_dump -t flashcrow_dev_data.gis_centreline_intersection -x --no-owner --clean --if-exists --schema-only | sed "s/flashcrow_dev_data.gis_centreline_intersection/gis.centreline_intersection/g" >> "${FLASHCROW_DEV_DATA}"
+env $(xargs < "/home/ec2-user/cot-env.config") pg_dump -t flashcrow_dev_data.gis_hospital -x --no-owner --clean --if-exists --schema-only | sed "s/flashcrow_dev_data.gis_hospital/gis.hospital/g" >> "${FLASHCROW_DEV_DATA}"
+env $(xargs < "/home/ec2-user/cot-env.config") pg_dump -t flashcrow_dev_data.gis_school -x --no-owner --clean --if-exists --schema-only | sed "s/flashcrow_dev_data.gis_school/gis.school/g" >> "${FLASHCROW_DEV_DATA}"
 
-# copy schemas of sampled tables
-env $(xargs < "/home/ec2-user/cot-env.config") pg_dump -t "collisions.events" -t "collisions.events_centreline" -t "collisions.involved" -t '"TRAFFIC"."COUNTINFOMICS"' -t '"TRAFFIC"."DET"' -t '"TRAFFIC"."COUNTINFO"' -t '"TRAFFIC"."CNT_DET"' -t '"TRAFFIC"."CNT_SPD"' -x --no-owner --clean --if-exists --schema-only >> "${FLASHCROW_DEV_DATA}"
+env $(xargs < "/home/ec2-user/cot-env.config") pg_dump -t flashcrow_dev_data.prj_volume_artery_centreline -x --no-owner --clean --if-exists --schema-only | sed "s/flashcrow_dev_data.prj_volume_artery_centreline/prj_volume.artery_centreline/g" >> "${FLASHCROW_DEV_DATA}"
 
-# copy sampled table data from flashcrow_dev_data schema
+env $(xargs < "/home/ec2-user/cot-env.config") pg_dump -t flashcrow_dev_data.traffic_category -x --no-owner --clean --if-exists --schema-only | sed 's/flashcrow_dev_data.traffic_category/"TRAFFIC"."CATEGORY"/g' >> "${FLASHCROW_DEV_DATA}"
+env $(xargs < "/home/ec2-user/cot-env.config") pg_dump -t flashcrow_dev_data.traffic_arterydata -x --no-owner --clean --if-exists --schema-only | sed 's/flashcrow_dev_data.traffic_arterydata/"TRAFFIC"."ARTERYDATA"/g' >> "${FLASHCROW_DEV_DATA}"
+
+# copy data for unsampled views
+echo 'COPY gis.centreline FROM stdin;' >> "${FLASHCROW_DEV_DATA}"
+env $(xargs < "/home/ec2-user/cot-env.config") psql -v ON_ERROR_STOP=1 -c "COPY (SELECT * FROM gis.centreline) TO stdout (FORMAT text, ENCODING 'UTF-8')" >> "${FLASHCROW_DEV_DATA}"
+echo '\.' >> "${FLASHCROW_DEV_DATA}"
+echo 'COPY gis.centreline_intersection FROM stdin;' >> "${FLASHCROW_DEV_DATA}"
+env $(xargs < "/home/ec2-user/cot-env.config") psql -v ON_ERROR_STOP=1 -c "COPY (SELECT * FROM gis.centreline_intersection) TO stdout (FORMAT text, ENCODING 'UTF-8')" >> "${FLASHCROW_DEV_DATA}"
+echo '\.' >> "${FLASHCROW_DEV_DATA}"
+echo 'COPY gis.hospital FROM stdin;' >> "${FLASHCROW_DEV_DATA}"
+env $(xargs < "/home/ec2-user/cot-env.config") psql -v ON_ERROR_STOP=1 -c "COPY (SELECT * FROM gis.hospital) TO stdout (FORMAT text, ENCODING 'UTF-8')" >> "${FLASHCROW_DEV_DATA}"
+echo '\.' >> "${FLASHCROW_DEV_DATA}"
+echo 'COPY gis.school FROM stdin;' >> "${FLASHCROW_DEV_DATA}"
+env $(xargs < "/home/ec2-user/cot-env.config") psql -v ON_ERROR_STOP=1 -c "COPY (SELECT * FROM gis.school) TO stdout (FORMAT text, ENCODING 'UTF-8')" >> "${FLASHCROW_DEV_DATA}"
+echo '\.' >> "${FLASHCROW_DEV_DATA}"
+
+echo 'COPY prj_volume.artery_centreline FROM stdin;' >> "${FLASHCROW_DEV_DATA}"
+env $(xargs < "/home/ec2-user/cot-env.config") psql -v ON_ERROR_STOP=1 -c "COPY (SELECT * FROM prj_volume.artery_centreline) TO stdout (FORMAT text, ENCODING 'UTF-8')" >> "${FLASHCROW_DEV_DATA}"
+echo '\.' >> "${FLASHCROW_DEV_DATA}"
+
+echo 'COPY "TRAFFIC"."CATEGORY" FROM stdin;' >> "${FLASHCROW_DEV_DATA}"
+env $(xargs < "/home/ec2-user/cot-env.config") psql -v ON_ERROR_STOP=1 -c "COPY (SELECT * FROM \"TRAFFIC\".\"CATEGORY\") TO stdout (FORMAT text, ENCODING 'UTF-8')" >> "${FLASHCROW_DEV_DATA}"
+echo '\.' >> "${FLASHCROW_DEV_DATA}"
+echo 'COPY "TRAFFIC"."ARTERYDATA" FROM stdin;' >> "${FLASHCROW_DEV_DATA}"
+env $(xargs < "/home/ec2-user/cot-env.config") psql -v ON_ERROR_STOP=1 -c "COPY (SELECT * FROM \"TRAFFIC\".\"ARTERYDATA\") TO stdout (FORMAT text, ENCODING 'UTF-8')" >> "${FLASHCROW_DEV_DATA}"
+echo '\.' >> "${FLASHCROW_DEV_DATA}"
+
+# copy schemas for sampled tables / views
+env $(xargs < "/home/ec2-user/cot-env.config") pg_dump -t flashcrow_dev_data.collisions_events -x --no-owner --clean --if-exists --schema-only | sed "s/flashcrow_dev_data.collisions_events/collisions.events/g" >> "${FLASHCROW_DEV_DATA}"
+env $(xargs < "/home/ec2-user/cot-env.config") pg_dump -t flashcrow_dev_data.collisions_events_centreline -x --no-owner --clean --if-exists --schema-only | sed "s/flashcrow_dev_data.collisions_events_centreline/collisions.events_centreline/g" >> "${FLASHCROW_DEV_DATA}"
+env $(xargs < "/home/ec2-user/cot-env.config") pg_dump -t flashcrow_dev_data.collisions_involved -x --no-owner --clean --if-exists --schema-only | sed "s/flashcrow_dev_data.collisions_involved/collisions.involved/g" >> "${FLASHCROW_DEV_DATA}"
+
+env $(xargs < "/home/ec2-user/cot-env.config") pg_dump -t flashcrow_dev_data.traffic_countinfomics -x --no-owner --clean --if-exists --schema-only | sed 's/flashcrow_dev_data.traffic_countinfomics/"TRAFFIC"."COUNTINFOMICS"/g' >> "${FLASHCROW_DEV_DATA}"
+env $(xargs < "/home/ec2-user/cot-env.config") pg_dump -t flashcrow_dev_data.traffic_det -x --no-owner --clean --if-exists --schema-only | sed 's/flashcrow_dev_data.traffic_det/"TRAFFIC"."DET"/g' >> "${FLASHCROW_DEV_DATA}"
+env $(xargs < "/home/ec2-user/cot-env.config") pg_dump -t flashcrow_dev_data.traffic_countinfo -x --no-owner --clean --if-exists --schema-only | sed 's/flashcrow_dev_data.traffic_countinfo/"TRAFFIC"."COUNTINFO"/g' >> "${FLASHCROW_DEV_DATA}"
+env $(xargs < "/home/ec2-user/cot-env.config") pg_dump -t flashcrow_dev_data.traffic_cnt_det -x --no-owner --clean --if-exists --schema-only | sed 's/flashcrow_dev_data.traffic_cnt_det/"TRAFFIC"."CNT_DET"/g' >> "${FLASHCROW_DEV_DATA}"
+env $(xargs < "/home/ec2-user/cot-env.config") pg_dump -t flashcrow_dev_data.traffic_cnt_spd -x --no-owner --clean --if-exists --schema-only | sed 's/flashcrow_dev_data.traffic_cnt_spd/"TRAFFIC"."CNT_SPD"/g' >> "${FLASHCROW_DEV_DATA}"
+
+# copy data for sampled tables / views
 echo 'COPY collisions.events FROM stdin;' >> "${FLASHCROW_DEV_DATA}"
 env $(xargs < "/home/ec2-user/cot-env.config") psql -v ON_ERROR_STOP=1 -c "COPY (SELECT * FROM flashcrow_dev_data.collisions_events) TO stdout (FORMAT text, ENCODING 'UTF-8')" >> "${FLASHCROW_DEV_DATA}"
 echo '\.' >> "${FLASHCROW_DEV_DATA}"
-
 echo 'COPY collisions.events_centreline FROM stdin;' >> "${FLASHCROW_DEV_DATA}"
 env $(xargs < "/home/ec2-user/cot-env.config") psql -v ON_ERROR_STOP=1 -c "COPY (SELECT * FROM flashcrow_dev_data.collisions_events_centreline) TO stdout (FORMAT text, ENCODING 'UTF-8')" >> "${FLASHCROW_DEV_DATA}"
 echo '\.' >> "${FLASHCROW_DEV_DATA}"
-
 echo 'COPY collisions.involved FROM stdin;' >> "${FLASHCROW_DEV_DATA}"
 env $(xargs < "/home/ec2-user/cot-env.config") psql -v ON_ERROR_STOP=1 -c "COPY (SELECT * FROM flashcrow_dev_data.collisions_involved) TO stdout (FORMAT text, ENCODING 'UTF-8')" >> "${FLASHCROW_DEV_DATA}"
 echo '\.' >> "${FLASHCROW_DEV_DATA}"
@@ -31,19 +70,18 @@ echo '\.' >> "${FLASHCROW_DEV_DATA}"
 echo 'COPY "TRAFFIC"."COUNTINFOMICS" FROM stdin;' >> "${FLASHCROW_DEV_DATA}"
 env $(xargs < "/home/ec2-user/cot-env.config") psql -v ON_ERROR_STOP=1 -c "COPY (SELECT * FROM flashcrow_dev_data.traffic_countinfomics) TO stdout (FORMAT text, ENCODING 'UTF-8')" >> "${FLASHCROW_DEV_DATA}"
 echo '\.' >> "${FLASHCROW_DEV_DATA}"
-
 echo 'COPY "TRAFFIC"."DET" FROM stdin;' >> "${FLASHCROW_DEV_DATA}"
 env $(xargs < "/home/ec2-user/cot-env.config") psql -v ON_ERROR_STOP=1 -c "COPY (SELECT * FROM flashcrow_dev_data.traffic_det) TO stdout (FORMAT text, ENCODING 'UTF-8')" >> "${FLASHCROW_DEV_DATA}"
 echo '\.' >> "${FLASHCROW_DEV_DATA}"
-
 echo 'COPY "TRAFFIC"."COUNTINFO" FROM stdin;' >> "${FLASHCROW_DEV_DATA}"
 env $(xargs < "/home/ec2-user/cot-env.config") psql -v ON_ERROR_STOP=1 -c "COPY (SELECT * FROM flashcrow_dev_data.traffic_countinfo) TO stdout (FORMAT text, ENCODING 'UTF-8')" >> "${FLASHCROW_DEV_DATA}"
 echo '\.' >> "${FLASHCROW_DEV_DATA}"
-
 echo 'COPY "TRAFFIC"."CNT_DET" FROM stdin;' >> "${FLASHCROW_DEV_DATA}"
 env $(xargs < "/home/ec2-user/cot-env.config") psql -v ON_ERROR_STOP=1 -c "COPY (SELECT * FROM flashcrow_dev_data.traffic_cnt_det) TO stdout (FORMAT text, ENCODING 'UTF-8')" >> "${FLASHCROW_DEV_DATA}"
 echo '\.' >> "${FLASHCROW_DEV_DATA}"
-
 echo 'COPY "TRAFFIC"."CNT_SPD" FROM stdin;' >> "${FLASHCROW_DEV_DATA}"
 env $(xargs < "/home/ec2-user/cot-env.config") psql -v ON_ERROR_STOP=1 -c "COPY (SELECT * FROM flashcrow_dev_data.traffic_cnt_spd) TO stdout (FORMAT text, ENCODING 'UTF-8')" >> "${FLASHCROW_DEV_DATA}"
 echo '\.' >> "${FLASHCROW_DEV_DATA}"
+
+# compress to reduce copying bandwidth
+tar czvf "${FLASHCROW_DEV_DATA}.tar.gz" "${FLASHCROW_DEV_DATA}"
