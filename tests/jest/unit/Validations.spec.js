1d8a7e49361b13e72a8f081a767b27a07aa162f1
Evan Savage <Evan.Savage@toronto.ca>
2020-03-30 13:01:40 +0000

    100% coverage for ValidationsStudyRequest

--- tests/jest/unit/Validations.spec.js
+++ tests/jest/unit/Validations.spec.js
@@ -1,17 +1,20 @@
-import ValidationsStudyRequest from '@/lib/validation/ValidationsStudyRequest';
+import { StudyHours } from '@/lib/Constants';
+import ValidationsStudyRequest, {
+  numConsecutiveDaysOfWeek,
+} from '@/lib/validation/ValidationsStudyRequest';
 
-test('ValidationsStudyRequest.numConsecutiveDaysOfWeek()', () => {
+test('numConsecutiveDaysOfWeek()', () => {
   // Simple use cases.
-  expect(ValidationsStudyRequest.numConsecutiveDaysOfWeek([])).toBe(0);
-  expect(ValidationsStudyRequest.numConsecutiveDaysOfWeek([2, 3, 4])).toBe(3);
-  expect(ValidationsStudyRequest.numConsecutiveDaysOfWeek([0, 1, 2, 3, 4, 5, 6])).toBe(7);
+  expect(numConsecutiveDaysOfWeek([])).toBe(0);
+  expect(numConsecutiveDaysOfWeek([2, 3, 4])).toBe(3);
+  expect(numConsecutiveDaysOfWeek([0, 1, 2, 3, 4, 5, 6])).toBe(7);
   // It should take "wrap-arounds" at end-of-week into account.
   let i;
   for (i = 0; i < 7; i += 1) {
-    expect(ValidationsStudyRequest.numConsecutiveDaysOfWeek([i])).toBe(1);
-    expect(ValidationsStudyRequest.numConsecutiveDaysOfWeek([i, (i + 3) % 7])).toBe(1);
-    expect(ValidationsStudyRequest.numConsecutiveDaysOfWeek([i, (i + 1) % 7])).toBe(2);
-    expect(ValidationsStudyRequest.numConsecutiveDaysOfWeek([i, (i + 1) % 7, (i + 3) % 7])).toBe(2);
+    expect(numConsecutiveDaysOfWeek([i])).toBe(1);
+    expect(numConsecutiveDaysOfWeek([i, (i + 3) % 7])).toBe(1);
+    expect(numConsecutiveDaysOfWeek([i, (i + 1) % 7])).toBe(2);
+    expect(numConsecutiveDaysOfWeek([i, (i + 1) % 7, (i + 3) % 7])).toBe(2);
   }
   // Fuzz testing for invariants.
   const n = 100;
@@ -23,14 +26,14 @@ test('ValidationsStudyRequest.numConsecutiveDaysOfWeek()', () => {
         randomDaysOfWeek.push(j);
       }
     }
-    const k = ValidationsStudyRequest.numConsecutiveDaysOfWeek(randomDaysOfWeek);
+    const k = numConsecutiveDaysOfWeek(randomDaysOfWeek);
     expect(k).toBeGreaterThanOrEqual(0);
     expect(k).toBeLessThanOrEqual(randomDaysOfWeek.length);
   }
 });
 
 test('ValidationsStudyRequest.torontoInternal()', () => {
-  const { torontoInternal } = ValidationsStudyRequest.validations.studyRequest.ccEmails.$each;
+  const { torontoInternal } = ValidationsStudyRequest.studyRequest.ccEmails.$each;
 
   expect(torontoInternal('')).toBeFalsy();
   expect(torontoInternal(',,,,,,')).toBeFalsy();
@@ -41,8 +44,7 @@ test('ValidationsStudyRequest.torontoInternal()', () => {
 });
 
 test('ValidationsStudyRequest.needsValidDaysOfWeek()', () => {
-  const { studyRequest } = ValidationsStudyRequest.validations;
-  const { needsValidDaysOfWeek } = studyRequest.studies.$each.duration;
+  const { needsValidDaysOfWeek } = ValidationsStudyRequest.studyRequest.studies.$each.duration;
 
   expect(needsValidDaysOfWeek(24, { daysOfWeek: [] })).toBeFalsy();
   expect(needsValidDaysOfWeek(24, { daysOfWeek: [2, 3, 4] })).toBeTruthy();
@@ -59,4 +61,28 @@ test('ValidationsStudyRequest.needsValidDaysOfWeek()', () => {
     expect(needsValidDaysOfWeek(48, { daysOfWeek: [i, (i + 1) % 7] })).toBeTruthy();
     expect(needsValidDaysOfWeek(72, { daysOfWeek: [i, (i + 1) % 7] })).toBeFalsy();
   }
+});
+
+test('ValidationsStudyRequest.requiredIfOtherHours()', () => {
+  const { requiredIfOtherHours } = ValidationsStudyRequest.studyRequest.studies.$each.notes;
+
+  expect(requiredIfOtherHours(null, { hours: StudyHours.ROUTINE })).toBeTruthy();
+  expect(requiredIfOtherHours('', { hours: StudyHours.SCHOOL })).toBeTruthy();
+  expect(requiredIfOtherHours('shopping mall hours', { hours: StudyHours.ROUTINE })).toBeTruthy();
+
+  expect(requiredIfOtherHours(null, { hours: StudyHours.OTHER })).toBeFalsy();
+  expect(requiredIfOtherHours('', { hours: StudyHours.OTHER })).toBeFalsy();
+  expect(requiredIfOtherHours('shopping mall hours', { hours: StudyHours.OTHER })).toBeTruthy();
+});
+
+test('ValidationsStudyRequest.requiredIfUrgent()', () => {
+  const { requiredIfUrgent } = ValidationsStudyRequest.studyRequest.urgentReason;
+
+  expect(requiredIfUrgent(null, { urgent: false })).toBeTruthy();
+  expect(requiredIfUrgent('', { urgent: false })).toBeTruthy();
+  expect(requiredIfUrgent('this is urgent!', { urgent: false })).toBeFalsy();
+
+  expect(requiredIfUrgent(null, { urgent: true })).toBeFalsy();
+  expect(requiredIfUrgent('', { urgent: true })).toBeFalsy();
+  expect(requiredIfUrgent('this is urgent!', { urgent: true })).toBeTruthy();
 });
