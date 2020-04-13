562568db5272ea8760ed7c476894da2368192e89
EC2 Default User <ec2-user@ip-10-162-43-198.ca-central-1.compute.internal>
2020-04-13 18:31:54 +0000

    Normalization of nodes and arterycodes
    as part of porting `artery_tcl` pipelines to Airflow and revamping the process there.

---
+++ scripts/airflow/tasks/build_artery_centreline/normalize_arteries.sql
@@ -0,0 +1,45 @@
+CREATE SCHEMA IF NOT EXISTS prj_volume_tmp;
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS prj_volume_tmp.arteries_intersection AS (
+  WITH artery_links AS (
+    SELECT
+      "ARTERYCODE" AS arterycode,
+      "LINKID"::bigint AS link_id
+    FROM "TRAFFIC"."ARTERYDATA"
+    WHERE "LINKID" NOT LIKE '%@%'
+  )
+  SELECT
+    al.arterycode,
+    al.link_id,
+    ni.int_id,
+    ni.geom
+  FROM artery_links al
+  JOIN prj_volume_tmp.nodes_intersection ni USING (link_id)
+);
+CREATE UNIQUE INDEX IF NOT EXISTS arteries_intersection_arterycode ON prj_volume_tmp.arteries_intersection (arterycode);
+
+REFRESH MATERIALIZED VIEW CONCURRENTLY prj_volume_tmp.arteries_intersection;
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS prj_volume_tmp.arteries_midblock AS (
+  WITH artery_links AS (
+    SELECT
+      "ARTERYCODE" AS arterycode,
+      SUBSTRING("LINKID", '([0-9]{1,20})@?')::bigint AS from_link_id,
+      SUBSTRING("LINKID", '@([0-9]{1,20})')::bigint AS to_link_id
+    FROM "TRAFFIC"."ARTERYDATA"
+    WHERE "LINKID" LIKE '%@%'
+  )
+  SELECT
+    al.arterycode,
+    al.from_link_id,
+    ni_from.int_id AS from_int_id,
+    al.to_link_id,
+    ni_to.int_id AS to_int_id,
+    ST_MakeLine(ni_from.geom, ni_to.geom) AS geom
+  FROM artery_links al
+  JOIN prj_volume_tmp.nodes_intersection ni_from ON al.from_link_id = ni_from.link_id
+  JOIN prj_volume_tmp.nodes_intersection ni_to ON al.to_link_id = ni_to.link_id
+);
+CREATE UNIQUE INDEX IF NOT EXISTS arteries_midblock_arterycode ON prj_volume_tmp.arteries_midblock (arterycode);
+
+REFRESH MATERIALIZED VIEW CONCURRENTLY prj_volume_tmp.arteries_midblock;
--- scripts/deployment/rds/createDB.sql
+++ scripts/deployment/rds/createDB.sql
@@ -14,4 +14,23 @@ create extension fuzzystrmatch;
 create extension postgis_tiger_geocoder;
 create extension postgis_topology;
 set search_path=public,tiger;
+
+-- give flashcrow_dba relevant admin permissions
+alter schema tiger owner to flashcrow_dba;
+alter schema tiger_data owner to flashcrow_dba;
+alter schema topology owner to flashcrow_dba;
+
 CREATE FUNCTION exec(text) returns text language plpgsql volatile AS $f$ BEGIN EXECUTE $1; RETURN $1; END; $f$;
+SELECT exec('ALTER TABLE ' || quote_ident(s.nspname) || '.' || quote_ident(s.relname) || ' OWNER TO flashcrow_dba;')
+  FROM (
+    SELECT nspname, relname
+    FROM pg_class c JOIN pg_namespace n ON (c.relnamespace = n.oid)
+    WHERE nspname in ('tiger','topology') AND
+    relkind IN ('r','S','v') ORDER BY relkind = 'S')
+s;
+
+ALTER TABLE public.spatial_ref_sys OWNER TO flashcrow_dba;
+GRANT SELECT, INSERT ON TABLE public.spatial_ref_sys TO flashcrow_dba;
+
+-- SRID definitions
+INSERT into spatial_ref_sys (srid, auth_name, auth_srid, proj4text, srtext) values ( 92019, 'epsg', 2019, '+proj=tmerc +lat_0=0 +lon_0=-79.5 +k=0.9999 +x_0=304800 +y_0=0 +ellps=clrk66 +units=m +no_defs ', 'PROJCS["NAD27(76) / MTM zone 10",GEOGCS["NAD27(76)",DATUM["North_American_Datum_1927_1976",SPHEROID["Clarke 1866",6378206.4,294.9786982138982,AUTHORITY["EPSG","7008"]],AUTHORITY["EPSG","6608"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.01745329251994328,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4608"]],UNIT["metre",1,AUTHORITY["EPSG","9001"]],PROJECTION["Transverse_Mercator"],PARAMETER["latitude_of_origin",0],PARAMETER["central_meridian",-79.5],PARAMETER["scale_factor",0.9999],PARAMETER["false_easting",304800],PARAMETER["false_northing",0],AUTHORITY["EPSG","2019"],AXIS["X",EAST],AXIS["Y",NORTH]]') ON CONFLICT DO NOTHING;
