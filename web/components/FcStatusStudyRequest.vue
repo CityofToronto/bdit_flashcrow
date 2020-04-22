34e4273ed7bcacd5c932a289f4132262f83445e5
Evan Savage <Evan.Savage@toronto.ca>
2020-04-22 18:44:07 +0000

    Circles and details layout for FcStatusStudyRequest
    as per designs.  This requires a bit of careful padding in the details `<v-row>` to line these text blurbs up with their corresponding circles, while still preserving the even spacing of said circles.

--- web/components/FcStatusStudyRequest.vue
+++ web/components/FcStatusStudyRequest.vue
@@ -10,30 +10,49 @@
       :key="'circle_' + circle"
       class="status-circle"
       :class="'status-circle-' + circle"></div>
-    <v-row class="pt-2">
+    <v-row class="mx-0 pt-2">
       <v-col
-        v-for="({ date, status }, i) in details"
+        v-for="(detail, i) in details"
         :key="'details_' + i"
         :class="{
+          'pl-0': i !== 1,
+          'pl-2': i === 1,
+          'pr-0': i !== 3,
+          'pr-2': i === 3,
           'text-left': i === 0,
-          'text-center': i === 1,
-          'text-right': i === 2,
+          'text-center': 0 < i && i < 4,
+          'text-right': i === 4,
         }"
-        cols="4">
-        <div class="headline font-weight-regular">
-          {{status}}
-        </div>
-        <v-messages
-          class="mt-1"
-          :value="[date]"></v-messages>
+        :cols="i === 2 ? 4 : 2">
+        <template v-if="detail !== null">
+          <div class="headline font-weight-regular">
+            {{detail.status.text}}
+          </div>
+          <div class="mt-1 subtitle-2">
+            {{detail.createdAt | date}}
+          </div>
+        </template>
       </v-col>
     </v-row>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
+import ArrayUtils from '@/lib/ArrayUtils';
 import { StudyRequestStatus } from '@/lib/Constants';
-import TimeFormatters from '@/lib/time/TimeFormatters';
+
+/**
+ * @param {DateTime} dt1 - date to compare against
+ * @param {DateTime} dt2 - date being compared
+ * @returns {boolean} whether the date `dt2` takes place on is after the date
+ * `dt1` takes place on
+ */
+function afterDateOf(dt1, dt2) {
+  const endOfDay1 = dt1.endOf('day');
+  return dt2.valueOf() > endOfDay1.valueOf();
+}
 
 export default {
   name: 'FcStatusStudyRequest',
@@ -43,27 +62,89 @@ export default {
   },
   computed: {
     circles() {
-      return [
-        'REQUESTED',
-        'center',
-        'right',
-      ];
+      const circles = this.milestones.map(({ status }) => status.name);
+      if (this.progress < 50) {
+        circles.push('center');
+      }
+      if (this.progress < 100) {
+        circles.push('right');
+      }
+      return circles;
     },
     details() {
-      const detailsRequested = {
-        date: TimeFormatters.formatDefault(this.studyRequest.createdAt),
-        status: StudyRequestStatus.REQUESTED.text,
-      };
+      const details = new Array(5).fill(null);
+      this.milestones.forEach((milestone) => {
+        const { detailsIndex } = milestone.status;
+        details[detailsIndex] = milestone;
+      });
+      return details;
+    },
+    milestones() {
+      const { createdAt, status } = this.studyRequest;
+      const milestones = [{
+        createdAt,
+        status: StudyRequestStatus.REQUESTED,
+      }];
 
-      // TODO: add to this
+      if (status === StudyRequestStatus.REQUESTED) {
+        return milestones;
+      }
+      if (status === StudyRequestStatus.CHANGES_NEEDED) {
+        const changesNeeded = this.statusChanges.get(StudyRequestStatus.CHANGES_NEEDED);
+        milestones.push(changesNeeded);
+        return milestones;
+      }
+      if (status === StudyRequestStatus.CANCELLED) {
+        const cancelled = this.statusChanges.get(StudyRequestStatus.CANCELLED);
+        milestones.push(cancelled);
+        return milestones;
+      }
 
-      return [detailsRequested];
+      const assigned = this.statusChanges.get(StudyRequestStatus.ASSIGNED);
+      milestones.push(assigned);
+
+      if (status === StudyRequestStatus.ASSIGNED) {
+        return milestones;
+      }
+      if (status === StudyRequestStatus.REJECTED) {
+        const rejected = this.statusChanges.get(StudyRequestStatus.REJECTED);
+        milestones.push(rejected);
+        return milestones;
+      }
+      // COMPLETED
+      const completed = this.statusChanges.get(StudyRequestStatus.COMPLETED);
+      milestones.push(completed);
+      return milestones;
     },
     progress() {
-      // TODO: implement this
-
-      return 25;
+      const { status } = this.studyRequest;
+      if (status === StudyRequestStatus.REQUESTED) {
+        const { createdAt } = this.studyRequest;
+        return afterDateOf(createdAt, this.now) ? 25 : 0;
+      }
+      if (status === StudyRequestStatus.CHANGES_NEEDED
+        || status === StudyRequestStatus.CANCELLED) {
+        return 25;
+      }
+      if (status === StudyRequestStatus.ASSIGNED) {
+        const { createdAt: assignedAt } = this.statusChanges.get(StudyRequestStatus.ASSIGNED);
+        return afterDateOf(assignedAt, this.now) ? 75 : 50;
+      }
+      if (status === StudyRequestStatus.REJECTED) {
+        return 75;
+      }
+      // COMPLETED
+      return 100;
     },
+    statusChanges() {
+      const changesByStatus = ArrayUtils.groupBy(
+        this.studyRequestChanges,
+        change => change.status.ordinal,
+      );
+      const mostRecentChangesByStatus = changesByStatus.map(g => [g[0].status, g[0]]);
+      return new Map(mostRecentChangesByStatus);
+    },
+    ...mapState(['now']),
   },
 };
 </script>
@@ -96,12 +177,42 @@ export default {
       left: 0;
     }
 
+    &.status-circle-CHANGES_NEEDED {
+      background-color: var(--v-statusChangesNeeded-base);
+      border: 2px solid #fff;
+      left: calc(25% - 4px);
+    }
+
+    &.status-circle-CANCELLED {
+      background-color: var(--v-statusCancelled-base);
+      border: 2px solid #fff;
+      left: calc(25% - 4px);
+    }
+
+    &.status-circle-ASSIGNED {
+      background-color: var(--v-statusAssigned-base);
+      border: 2px solid #fff;
+      left: calc(50% - 8px);
+    }
+
+    &.status-circle-REJECTED {
+      background-color: var(--v-statusRejected-base);
+      border: 2px solid #fff;
+      left: calc(75% - 12px);
+    }
+
+    &.status-circle-COMPLETED {
+      background-color: var(--v-statusCompleted-base);
+      border: 2px solid #fff;
+      left: calc(100% - 16px);
+    }
+
     &.status-circle-center {
       left: calc(50% - 8px);
     }
 
     &.status-circle-right {
-      right: 0;
+      left: calc(100% - 16px);
     }
   }
 }
