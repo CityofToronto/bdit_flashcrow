965234675befa61562b5b98df81da72f25e9b906
Evan Savage <Evan.Savage@toronto.ca>
2020-04-23 20:56:25 +0000

    Start adding count summary tests
    to mirror the ones in `DAO.spec.js`, plus a couple of other cases where possible.
    
    From exploring this so far, the best approach here may be to create a subclass of `BackendClient` for testing purposes.  Instead of calling `window.fetch()`, this subclass would use `server.inject()` and build the appropriate mock request options.  (In this sense, it would be very similar to how `Mailer` wraps SendGrid email options into those expected by AWS SES.)
    
    This would reduce boilerplate to build requests, and would also make it easier to extend REST API tests to cover `WebApi` later.
    
    Nonetheless, the `server.inject()` approach works quite well so far!

--- tests/jest/api/RestApi.spec.js
+++ tests/jest/api/RestApi.spec.js
@@ -1,6 +1,13 @@
+import {
+  CentrelineType,
+  HttpStatus,
+  StudyType,
+} from '@/lib/Constants';
+import BackendClient from '@/lib/api/BackendClient';
 import UserDAO from '@/lib/db/UserDAO';
 import DAOTestUtils from '@/lib/test/DAOTestUtils';
 import { generateUser } from '@/lib/test/random/UserGenerator';
+import DateTime from '@/lib/time/DateTime';
 import { initialize } from '@/web/MoveServer';
 
 let server;
@@ -40,4 +47,99 @@ test('AuthController.getAuth', async () => {
     loggedIn: true,
     user: persistedUser,
   });
+});
+
+function expectNumPerCategory(actual, expected) {
+  expect(actual).toHaveLength(expected.length);
+  expected.forEach(([n0, value0], i) => {
+    const { category: { studyType: { name: value } }, numPerCategory } = actual[i];
+    expect(numPerCategory).toBe(n0);
+    expect(value).toBe(value0);
+  });
+}
+
+test('CountController.getCountsByCentrelineSummary', async () => {
+  // invalid feature
+  let params = {
+    centrelineId: -1,
+    centrelineType: -1,
+  };
+  let query = BackendClient.getQueryString(params);
+  let response = await server.inject({
+    method: 'GET',
+    url: `/counts/byCentreline/summary?${query}`,
+  });
+  expect(response.statusCode).toBe(HttpStatus.BAD_REQUEST.statusCode);
+
+  // invalid date range (start > end)
+  let start = DateTime.fromObject({ year: 2018, month: 1, day: 1 });
+  let end = DateTime.fromObject({ year: 2017, month: 12, day: 31 });
+  params = {
+    centrelineId: 30000549,
+    centrelineType: CentrelineType.INTERSECTION,
+    end,
+    start,
+  };
+  query = BackendClient.getQueryString(params);
+  response = await server.inject({
+    method: 'GET',
+    url: `/counts/byCentreline/summary?${query}`,
+  });
+  expect(response.statusCode).toBe(HttpStatus.BAD_REQUEST.statusCode);
+
+  // centreline feature with no counts
+  params = {
+    centrelineId: 30062737,
+    centrelineType: CentrelineType.SEGMENT,
+  };
+  query = BackendClient.getQueryString(params);
+  response = await server.inject({
+    method: 'GET',
+    url: `/counts/byCentreline/summary?${query}`,
+  });
+  expect(response.result).toEqual([]);
+
+  // centreline feature with some counts
+  params = {
+    centrelineId: 14659630,
+    centrelineType: CentrelineType.SEGMENT,
+  };
+  query = BackendClient.getQueryString(params);
+  response = await server.inject({
+    method: 'GET',
+    url: `/counts/byCentreline/summary?${query}`,
+  });
+  expectNumPerCategory(
+    response.result,
+    [[10, 'ATR_VOLUME'], [6, 'ATR_SPEED_VOLUME']],
+  );
+
+  // valid feature with some counts, date range filters to empty
+  start = DateTime.fromObject({ year: 2018, month: 1, day: 1 });
+  end = DateTime.fromObject({ year: 2019, month: 1, day: 1 });
+  params = {
+    centrelineId: 14659630,
+    centrelineType: CentrelineType.SEGMENT,
+    end,
+    start,
+  };
+  query = BackendClient.getQueryString(params);
+  response = await server.inject({
+    method: 'GET',
+    url: `/counts/byCentreline/summary?${query}`,
+  });
+  expect(response.result).toEqual([]);
+
+  // valid feature with some counts, filter by type
+  params = {
+    centrelineId: 14659630,
+    centrelineType: CentrelineType.SEGMENT,
+    studyType: [StudyType.ATR_SPEED_VOLUME],
+  };
+  query = BackendClient.getQueryString(params);
+  response = await server.inject({
+    method: 'GET',
+    url: `/counts/byCentreline/summary?${query}`,
+  });
+  expectNumPerCategory(response.result, [[6, 'ATR_SPEED_VOLUME']]);
 });
