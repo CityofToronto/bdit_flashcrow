c9b6bac90affc22b154473907ece0811fac1fdb0
Evan Savage <Evan.Savage@toronto.ca>
2020-04-23 18:19:06 +0000

    Move EmailFoo test helper class to a separate file
    so that we can reliably use this across our email-based tests.

--- tests/jest/unit/email/EmailBase.spec.js
+++ tests/jest/unit/email/EmailBase.spec.js
@@ -1,4 +1,5 @@
 import EmailBase from '@/lib/email/EmailBase';
+import EmailFoo from '@/lib/test/email/EmailFoo';
 
 test('EmailBase.getUrl', () => {
   let url = EmailBase.getUrl('');
@@ -19,37 +20,6 @@ test('EmailBase.getUrl', () => {
   url = EmailBase.getUrl('/requests/study/42/');
   expect(url).toEqual('https://localhost:8080/requests/study/42/');
 });
-
-function incrementASAP(x) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(x + 1);
-    }, 0);
-  });
-}
-
-class EmailFoo extends EmailBase {
-  constructor(x) {
-    super();
-    this.x = x;
-  }
-
-  async init() {
-    this.y = await incrementASAP(this.x);
-  }
-
-  getRecipients() {
-    return [`move-ops+${this.y}@toronto.ca`];
-  }
-
-  getSubject() {
-    return `Foo: ${this.x} -> ${this.y}`;
-  }
-
-  render() {
-    return `<h1>Foo</h1><p>incremented to ${this.y}</p>`;
-  }
-}
 
 test('EmailBase.getOptions', async () => {
   const email = new EmailFoo(1729);
--- tests/jest/unit/email/Mailer.spec.js
+++ tests/jest/unit/email/Mailer.spec.js
@@ -1,36 +1,5 @@
-import EmailBase from '@/lib/email/EmailBase';
 import Mailer from '@/lib/email/Mailer';
-
-function incrementASAP(x) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(x + 1);
-    }, 0);
-  });
-}
-
-class EmailFoo extends EmailBase {
-  constructor(x) {
-    super();
-    this.x = x;
-  }
-
-  async init() {
-    this.y = await incrementASAP(this.x);
-  }
-
-  getRecipients() {
-    return [`move-ops+${this.y}@toronto.ca`];
-  }
-
-  getSubject() {
-    return `Foo: ${this.x} -> ${this.y}`;
-  }
-
-  render() {
-    return `<h1>Foo</h1><p>incremented to ${this.y}</p>`;
-  }
-}
+import EmailFoo from '@/lib/test/email/EmailFoo';
 
 test('Mailer.convertToSes', async () => {
   const email = new EmailFoo(17);
