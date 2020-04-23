4345fb4a50b4cc735070880f7f43dbc206bf1366
Evan Savage <Evan.Savage@toronto.ca>
2020-04-23 20:13:52 +0000

    Use server.inject to test AuthController.getAuth
    As it turns out, you can also bypass authentication in tests here by passing in user credentials via the `auth` key to the `server.inject()` options object.

--- tests/jest/api/RestApi.spec.js
+++ tests/jest/api/RestApi.spec.js
@@ -1,19 +1,43 @@
-import RestApiTestUtils from '@/lib/test/RestApiTestUtils';
+import UserDAO from '@/lib/db/UserDAO';
+import DAOTestUtils from '@/lib/test/DAOTestUtils';
+import { generateUser } from '@/lib/test/random/UserGenerator';
+import { initialize } from '@/web/MoveServer';
 
-beforeAll(RestApiTestUtils.startup, RestApiTestUtils.TIMEOUT);
-afterAll(RestApiTestUtils.shutdown, RestApiTestUtils.TIMEOUT);
+let server;
 
-test('authentication works', async () => {
-  let response;
+beforeAll(async () => {
+  await DAOTestUtils.startupWithDevData();
+  server = await initialize();
+}, DAOTestUtils.TIMEOUT);
+afterAll(async () => {
+  await server.stop();
+  await DAOTestUtils.shutdown();
+}, DAOTestUtils.TIMEOUT);
 
-  response = await RestApiTestUtils.callApi('/auth');
-  expect(response.loggedIn).toBe(false);
+test('AuthController.getAuth', async () => {
+  let response = await server.inject({
+    method: 'GET',
+    url: '/auth',
+  });
+  expect(response.result).toEqual({
+    csrf: response.result.csrf,
+    loggedIn: false,
+    user: null,
+  });
 
-  await RestApiTestUtils.login();
-  response = await RestApiTestUtils.callApi('/auth');
-  expect(response.loggedIn).toBe(true);
-
-  await RestApiTestUtils.logout();
-  response = await RestApiTestUtils.callApi('/auth');
-  expect(response.loggedIn).toBe(false);
+  const transientUser = generateUser();
+  const persistedUser = await UserDAO.create(transientUser);
+  response = await server.inject({
+    auth: {
+      strategy: 'session',
+      credentials: persistedUser,
+    },
+    method: 'GET',
+    url: '/auth',
+  });
+  expect(response.result).toEqual({
+    csrf: response.result.csrf,
+    loggedIn: true,
+    user: persistedUser,
+  });
 });
