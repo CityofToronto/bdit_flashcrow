0fd82efa6701bf44d62e9028c692f45bf9dd1407
Evan Savage <Evan.Savage@toronto.ca>
2020-04-24 15:41:03 +0000

    More test cases for CountController
    to fill out our testing coverage there - these endpoints are unusual in that they have a lot of parameters!

--- tests/jest/api/RestApi.spec.js
+++ tests/jest/api/RestApi.spec.js
@@ -1,6 +1,7 @@
 import {
   CentrelineType,
   HttpStatus,
+  StudyHours,
   StudyType,
 } from '@/lib/Constants';
 import BackendClient from '@/lib/api/BackendClient';
@@ -142,4 +143,241 @@ test('CountController.getCountsByCentrelineSummary', async () => {
     url: `/counts/byCentreline/summary?${query}`,
   });
   expectNumPerCategory(response.result, [[6, 'ATR_SPEED_VOLUME']]);
+
+  // valid feature with some counts, filter by day of week
+  params = {
+    centrelineId: 14659630,
+    centrelineType: CentrelineType.SEGMENT,
+    dayOfWeek: [2, 3, 4],
+  };
+  query = BackendClient.getQueryString(params);
+  response = await server.inject({
+    method: 'GET',
+    url: `/counts/byCentreline/summary?${query}`,
+  });
+  expectNumPerCategory(response.result, [[9, 'ATR_VOLUME'], [6, 'ATR_SPEED_VOLUME']]);
+
+  // valid feature with some counts, filter by day of week
+  params = {
+    centrelineId: 14659630,
+    centrelineType: CentrelineType.SEGMENT,
+    dayOfWeek: [2, 3, 4],
+  };
+  query = BackendClient.getQueryString(params);
+  response = await server.inject({
+    method: 'GET',
+    url: `/counts/byCentreline/summary?${query}`,
+  });
+  expectNumPerCategory(response.result, [[9, 'ATR_VOLUME'], [6, 'ATR_SPEED_VOLUME']]);
+
+  // intersection with some counts
+  params = {
+    centrelineId: 13446886,
+    centrelineType: CentrelineType.INTERSECTION,
+  };
+  query = BackendClient.getQueryString(params);
+  response = await server.inject({
+    method: 'GET',
+    url: `/counts/byCentreline/summary?${query}`,
+  });
+  expectNumPerCategory(response.result, [[3, 'TMC']]);
+
+  // intersection with some counts, filter by date
+  start = DateTime.fromObject({ year: 2011, month: 1, day: 1 });
+  end = DateTime.fromObject({ year: 2019, month: 1, day: 1 });
+  params = {
+    centrelineId: 13446886,
+    centrelineType: CentrelineType.INTERSECTION,
+    end,
+    start,
+  };
+  query = BackendClient.getQueryString(params);
+  response = await server.inject({
+    method: 'GET',
+    url: `/counts/byCentreline/summary?${query}`,
+  });
+  expectNumPerCategory(response.result, [[2, 'TMC']]);
+
+  // intersection with some counts, filter by study hours
+  params = {
+    centrelineId: 13446886,
+    centrelineType: CentrelineType.INTERSECTION,
+    hours: [StudyHours.SCHOOL],
+  };
+  query = BackendClient.getQueryString(params);
+  response = await server.inject({
+    method: 'GET',
+    url: `/counts/byCentreline/summary?${query}`,
+  });
+  expectNumPerCategory(response.result, []);
+
+  // intersection with some counts, filter by days of week
+  params = {
+    centrelineId: 13446886,
+    centrelineType: CentrelineType.INTERSECTION,
+    dayOfWeek: [0, 1, 5, 6],
+  };
+  query = BackendClient.getQueryString(params);
+  response = await server.inject({
+    method: 'GET',
+    url: `/counts/byCentreline/summary?${query}`,
+  });
+  expectNumPerCategory(response.result, [[1, 'TMC']]);
+
+  // intersection with some counts, filter by type of study
+  params = {
+    centrelineId: 13446886,
+    centrelineType: CentrelineType.INTERSECTION,
+    studyType: [StudyType.TMC],
+  };
+  query = BackendClient.getQueryString(params);
+  response = await server.inject({
+    method: 'GET',
+    url: `/counts/byCentreline/summary?${query}`,
+  });
+  expectNumPerCategory(response.result, [[3, 'TMC']]);
+
+  // intersection with some counts, filter by type of study (non-TMC)
+  params = {
+    centrelineId: 13446886,
+    centrelineType: CentrelineType.INTERSECTION,
+    studyType: [StudyType.ATR_SPEED_VOLUME],
+  };
+  query = BackendClient.getQueryString(params);
+  response = await server.inject({
+    method: 'GET',
+    url: `/counts/byCentreline/summary?${query}`,
+  });
+  expectNumPerCategory(response.result, []);
+});
+
+test('CountController.getCountsByCentreline', async () => {
+  // invalid feature
+  let params = {
+    centrelineId: -1,
+    centrelineType: -1,
+    limit: 10,
+    offset: 0,
+  };
+  let query = BackendClient.getQueryString(params);
+  let response = await server.inject({
+    method: 'GET',
+    url: `/counts/byCentreline/TMC?${query}`,
+  });
+  expect(response.statusCode).toBe(HttpStatus.BAD_REQUEST.statusCode);
+
+  // invalid date range (start > end)
+  let start = DateTime.fromObject({ year: 2018, month: 1, day: 1 });
+  let end = DateTime.fromObject({ year: 2017, month: 12, day: 31 });
+  params = {
+    centrelineId: 30000549,
+    centrelineType: CentrelineType.INTERSECTION,
+    end,
+    limit: 10,
+    offset: 0,
+    start,
+  };
+  query = BackendClient.getQueryString(params);
+  response = await server.inject({
+    method: 'GET',
+    url: `/counts/byCentreline/TMC?${query}`,
+  });
+  expect(response.statusCode).toBe(HttpStatus.BAD_REQUEST.statusCode);
+
+  // centreline feature with no counts
+  params = {
+    centrelineId: 30062737,
+    centrelineType: CentrelineType.SEGMENT,
+    limit: 10,
+    offset: 0,
+  };
+  query = BackendClient.getQueryString(params);
+  response = await server.inject({
+    method: 'GET',
+    url: `/counts/byCentreline/ATR_VOLUME?${query}`,
+  });
+  expect(response.result.length).toBe(0);
+
+  // valid feature with less than maxPerCategory counts
+  params = {
+    centrelineId: 14659630,
+    centrelineType: CentrelineType.SEGMENT,
+    limit: 10,
+    offset: 0,
+  };
+  query = BackendClient.getQueryString(params);
+  response = await server.inject({
+    method: 'GET',
+    url: `/counts/byCentreline/ATR_SPEED_VOLUME?${query}`,
+  });
+  expect(response.result.length).toBe(6);
+
+  // valid feature with less than maxPerCategory counts, date range filters to empty
+  start = DateTime.fromObject({ year: 2018, month: 1, day: 1 });
+  end = DateTime.fromObject({ year: 2019, month: 1, day: 1 });
+  params = {
+    centrelineId: 14659630,
+    centrelineType: CentrelineType.SEGMENT,
+    end,
+    limit: 10,
+    offset: 0,
+    start,
+  };
+  query = BackendClient.getQueryString(params);
+  response = await server.inject({
+    method: 'GET',
+    url: `/counts/byCentreline/ATR_SPEED_VOLUME?${query}`,
+  });
+  expect(response.result.length).toBe(0);
+
+  // valid feature with more than maxPerCategory counts
+  params = {
+    centrelineId: 1145768,
+    centrelineType: CentrelineType.SEGMENT,
+    limit: 10,
+    offset: 0,
+  };
+  query = BackendClient.getQueryString(params);
+  response = await server.inject({
+    method: 'GET',
+    url: `/counts/byCentreline/RESCU?${query}`,
+  });
+  expect(response.result.length).toBe(10);
+});
+
+test('CountController.getCountsByCentreline [pagination]', async () => {
+  const start = DateTime.fromObject({ year: 2015, month: 1, day: 1 });
+  const end = DateTime.fromObject({ year: 2018, month: 1, day: 1 });
+  let params = {
+    centrelineId: 1145768,
+    centrelineType: CentrelineType.SEGMENT,
+    end,
+    start,
+    studyType: [StudyType.RESCU],
+  };
+  let query = BackendClient.getQueryString(params);
+  let response = await server.inject({
+    method: 'GET',
+    url: `/counts/byCentreline/summary?${query}`,
+  });
+
+  const { numPerCategory } = response.result[0];
+  for (let offset = 0; offset < numPerCategory; offset += 100) {
+    params = {
+      centrelineId: 1145768,
+      centrelineType: CentrelineType.SEGMENT,
+      end,
+      limit: 100,
+      offset,
+      start,
+    };
+    query = BackendClient.getQueryString(params);
+    /* eslint-disable-next-line no-await-in-loop */
+    response = await server.inject({
+      method: 'GET',
+      url: `/counts/byCentreline/RESCU?${query}`,
+    });
+    const expectedLength = Math.min(100, numPerCategory - offset);
+    expect(response.result).toHaveLength(expectedLength);
+  }
 });
