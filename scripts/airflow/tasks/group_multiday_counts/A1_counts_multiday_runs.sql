bfc0520b6e80be1c52ec32de76ae011b399a623f
EC2 Default User <ec2-user@ip-10-162-43-198.ca-central-1.compute.internal>
2020-05-11 18:29:43 +0000

    SQL tasks to build multi-direction, multi-day studies
    based on previous work on arterycode grouping.

--- scripts/airflow/tasks/group_multiday_counts/A1_counts_multiday_runs.sql
+++ scripts/airflow/tasks/group_multiday_counts/A1_counts_multiday_runs.sql
@@ -2,6 +2,7 @@ CREATE SCHEMA IF NOT EXISTS counts;
 
 CREATE MATERIALIZED VIEW IF NOT EXISTS counts.counts_multiday_runs AS (
   SELECT * FROM (
+    -- ATRs not using permanent counting stations: multiday grouping
     WITH diffs AS (
       SELECT
         ci."COUNT_INFO_ID",
@@ -24,11 +25,10 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS counts.counts_multiday_runs AS (
       d."ARTERYCODE", d."CATEGORY_ID",
       g.group_id, d."COUNT_INFO_ID", d."COUNT_DATE",
       FALSE AS perm_autom
-    FROM diffs d JOIN groups g
-      ON d."ARTERYCODE" = g."ARTERYCODE"
-      AND d."CATEGORY_ID" = g."CATEGORY_ID"
-      AND d.diff = g.diff
+    FROM diffs d
+    JOIN groups g USING ("ARTERYCODE", "CATEGORY_ID", diff)
   ) t1 UNION ALL (
+    -- ATRs using permanent counting stations: no multi-day grouping
     SELECT
       ci."ARTERYCODE", ci."CATEGORY_ID",
       ci."COUNT_INFO_ID" AS group_id, ci."COUNT_INFO_ID", ci."COUNT_DATE",
@@ -37,29 +37,12 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS counts.counts_multiday_runs AS (
     JOIN "TRAFFIC"."ARTERYDATA" ad USING ("ARTERYCODE")
     WHERE ad."COUNT_TYPE" = 'PermAutom'
   ) UNION ALL (
-    WITH diffs AS (
-      SELECT
-        "COUNT_INFO_ID",
-        "ARTERYCODE",
-        "CATEGORY_ID",
-        "COUNT_DATE",
-        dense_rank() OVER (w1) - date_part('DAYS', "COUNT_DATE" - make_timestamp(1970, 1, 1, 0, 0, 0)) AS diff
-      FROM "TRAFFIC"."COUNTINFOMICS" cim
-      WINDOW
-        w1 AS (PARTITION BY "ARTERYCODE" ORDER BY "COUNT_DATE")
-    ),
-    groups AS (
-      SELECT "ARTERYCODE", diff, MIN("COUNT_INFO_ID") AS group_id
-      FROM diffs
-      GROUP BY "ARTERYCODE", diff
-    )
+    -- TMCs: no multi-day grouping
     SELECT
-      d."ARTERYCODE", d."CATEGORY_ID",
-      g.group_id, d."COUNT_INFO_ID", d."COUNT_DATE",
+      cim."ARTERYCODE", cim."CATEGORY_ID",
+      cim."COUNT_INFO_ID" AS group_id, cim."COUNT_INFO_ID", cim."COUNT_DATE",
       FALSE AS perm_autom
-    FROM diffs d JOIN groups g
-      ON d."ARTERYCODE" = g."ARTERYCODE"
-      AND d.diff = g.diff
+    FROM "TRAFFIC"."COUNTINFOMICS" cim
   )
 );
 CREATE UNIQUE INDEX IF NOT EXISTS counts_multiday_runs_count ON counts.counts_multiday_runs ("CATEGORY_ID", "COUNT_INFO_ID");
