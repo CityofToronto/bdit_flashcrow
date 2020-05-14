9f58833534b3db1820bf34813826a812108e0108
EC2 Default User <ec2-user@ip-10-162-43-198.ca-central-1.compute.internal>
2020-05-14 12:48:51 +0000

    Remove old crash_geocoding scripts
    to clean up the tasks directory a bit.  (We're also using this overhaul as an excuse to refactor some of these DAGs to use `create_bash_task_nested`.)

--- scripts/airflow/tasks/build_events_intersections.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-GIT_ROOT=/home/ec2-user/flashcrow
-TASKS_ROOT="${GIT_ROOT}/scripts/airflow/tasks"
-
-env $(xargs < "/home/ec2-user/cot-env.config") psql -v ON_ERROR_STOP=1 < "${TASKS_ROOT}/build_events_intersections/build_events_intersections.sql"
