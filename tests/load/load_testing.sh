2332d5a8cf74d971c9431cc17a723f3ce140935b
EC2 Default User <ec2-user@ip-10-162-43-198.ca-central-1.compute.internal>
2020-06-01 19:57:47 +0000

    Handle reporting endpoints in load testing harness
    These have significantly lower request-per-second capacity, so we need a different approach than we use for higher RPS endpoints.

--- tests/load/load_testing.sh
+++ tests/load/load_testing.sh
@@ -5,24 +5,39 @@ set -euo pipefail
 mkdir -p results
 for path_list in paths/*.txt; do
   PATH_LIST_BASE="$(basename "${path_list%%.txt}")"
+  MAX_REQ=2000
+  if echo "$PATH_LIST_BASE" | grep getReport; then
+    # much lower value, to allow recovery between load tests
+    MAX_REQ=20
+  fi
 
   echo "[${PATH_LIST_BASE}] warm-up..."
   env WRK2_PATH_LIST="${path_list}" wrk -t2 -c10 -d5s -R100 -s path_list.lua --latency https://move.intra.dev-toronto.ca
 
   echo "[${PATH_LIST_BASE}] computing maximum response rate..."
-  env WRK2_PATH_LIST="${path_list}" wrk -t2 -c100 -d30s -R10000 -s path_list.lua --latency https://move.intra.dev-toronto.ca > "results/${PATH_LIST_BASE}_max.txt"
+  env WRK2_PATH_LIST="${path_list}" wrk -t2 -c100 -d20s -R"${MAX_REQ}" -s path_list.lua --latency https://move.intra.dev-toronto.ca > "results/${PATH_LIST_BASE}_max.txt"
   tail -24 "results/${PATH_LIST_BASE}_max.txt" > "results/${PATH_LIST_BASE}_max.json"
   MAX_REQ=$(jq '.requests_per_sec' "results/${PATH_LIST_BASE}_max.json" | cut -d. -f1)
   echo "[${PATH_LIST_BASE}] max = ${MAX_REQ}"
 
   echo "[${PATH_LIST_BASE}] computing practical response rate..."
-  for pct in $(seq 20 5 80); do
-    PCT_REQ=$(echo "${MAX_REQ} * ${pct} / 100" | bc)
-    env WRK2_PATH_LIST="${path_list}" wrk -t2 -c100 -d30s -R"${PCT_REQ}" -s path_list.lua --latency https://move.intra.dev-toronto.ca > "results/${PATH_LIST_BASE}_R${PCT_REQ}.txt"
-    tail -24 "results/${PATH_LIST_BASE}_R${PCT_REQ}.txt" > "results/${PATH_LIST_BASE}_R${PCT_REQ}.json"
-    PCT_LATENCY=$(jq ".latency[3][1]" "results/${PATH_LIST_BASE}_R${PCT_REQ}.json")
-    PCT_LATENCY_MS=$(echo "${PCT_LATENCY} / 1000" | bc)
-    echo "[${PATH_LIST_BASE}]   ${pct}%: ${PCT_REQ} req/s (p95 ${PCT_LATENCY_MS} ms)"
-  done
+  if (("${MAX_REQ}" <= 20)); then
+    for req in $(seq 1 "${MAX_REQ}"); do
+      env WRK2_PATH_LIST="${path_list}" wrk -t2 -c100 -d20s -R"${req}" -s path_list.lua --latency https://move.intra.dev-toronto.ca > "results/${PATH_LIST_BASE}_R${req}.txt"
+      tail -24 "results/${PATH_LIST_BASE}_R${req}.txt" > "results/${PATH_LIST_BASE}_R${req}.json"
+      REQ_LATENCY=$(jq ".latency[3][1]" "results/${PATH_LIST_BASE}_R${req}.json")
+      REQ_LATENCY_MS=$(echo "${REQ_LATENCY} / 1000" | bc)
+      echo "[${PATH_LIST_BASE}]   ${req}: ${req} req/s (p95 = ${REQ_LATENCY_MS} ms)"
+    done
+  else
+    for pct in $(seq 20 5 80); do
+      PCT_REQ=$(echo "${MAX_REQ} * ${pct} / 100" | bc -l)
+      env WRK2_PATH_LIST="${path_list}" wrk -t2 -c100 -d20s -R"${PCT_REQ}" -s path_list.lua --latency https://move.intra.dev-toronto.ca > "results/${PATH_LIST_BASE}_pct${pct}.txt"
+      tail -24 "results/${PATH_LIST_BASE}_pct${pct}.txt" > "results/${PATH_LIST_BASE}_pct${pct}.json"
+      PCT_LATENCY=$(jq ".latency[3][1]" "results/${PATH_LIST_BASE}_pct${pct}.json")
+      PCT_LATENCY_MS=$(echo "${PCT_LATENCY} / 1000" | bc)
+      echo "[${PATH_LIST_BASE}]   ${pct}%: ${PCT_REQ} req/s (p95 = ${PCT_LATENCY_MS} ms)"
+    done
+  fi
   echo "[${PATH_LIST_BASE}] finished."
 done
