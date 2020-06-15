89e7ca7e57d261bf0d3af5706a8528d77d23d170
Evan Savage <Evan.Savage@toronto.ca>
2020-06-15 19:28:18 +0000

    Top-level details for intersection summary, TSC warrant reports
    Now on to the ATR-related reports.

--- tests/jest/unit/reports/ReportIntersectionSummary.spec.js
+++ tests/jest/unit/reports/ReportIntersectionSummary.spec.js
@@ -1,15 +1,13 @@
 /* eslint-disable camelcase */
 import path from 'path';
 
-import { CardinalDirection, StudyType } from '@/lib/Constants';
-import ReportBaseFlowDirectional from '@/lib/reports/ReportBaseFlowDirectional';
+import {
+  CentrelineType,
+  StudyHours,
+  StudyType,
+} from '@/lib/Constants';
 import ReportIntersectionSummary from '@/lib/reports/ReportIntersectionSummary';
 import { loadJsonSync } from '@/lib/test/TestDataLoader';
-import {
-  generateHourlyMajorAndMinorDirections,
-  generateTmc,
-} from '@/lib/test/random/CountDataGenerator';
-import DateTime from '@/lib/time/DateTime';
 
 const countData_5_38661 = loadJsonSync(
   path.resolve(__dirname, './data/countData_5_38661.json'),
@@ -18,76 +16,147 @@ const transformedData_INTERSECTION_SUMMARY_5_38661 = loadJsonSync(
   path.resolve(__dirname, './data/transformedData_INTERSECTION_SUMMARY_5_38661.json'),
 );
 
-test('ReportIntersectionSummary#transformData', () => {
-  const reportInstance = new ReportIntersectionSummary();
+function setup_5_38661() {
+  const study = {
+    centrelineId: 13456854,
+    centrelineType: CentrelineType.INTERSECTION,
+    countGroupId: 38661,
+    type: { id: 5, studyType: StudyType.TMC },
+  };
 
-  // fuzz test
-  for (let i = 0; i < 25; i++) {
-    const countData = generateTmc();
-    const hourlyData = ReportBaseFlowDirectional.sumHourly(countData);
-    const {
-      hourlyMajorDirections,
-      hourlyMinorDirections,
-    } = generateHourlyMajorAndMinorDirections(hourlyData);
-    expect(() => {
-      reportInstance.transformData(null, {
-        countData,
-        hourlyData,
-        hourlyMajorDirections,
-        hourlyMinorDirections,
-      });
-    }).not.toThrow();
-  }
-});
+  const count = {
+    hours: StudyHours.ROUTINE,
+    id: 38661,
+    locationDesc: 'OVERLEA BLVD AT THORNCLIFFE PARK DR & E TCS (PX 679)',
+  };
+  const intersection = {
+    geom: {
+      type: 'Point',
+      coordinates: [-79.343625497, 43.70747321],
+    },
+  };
+  const segments = [
+    {
+      centrelineId: 649,
+      geom: {
+        type: 'LineString',
+        coordinates: [
+          [-79.349877201, 43.70500665],
+          [-79.349308494, 43.705553926],
+          [-79.349210149, 43.705648311],
+          [-79.349133972, 43.70572022],
+          [-79.349046938, 43.705796617],
+          [-79.348959121, 43.705868512],
+          [-79.348795579, 43.705989814],
+          [-79.34868914, 43.706070684],
+          [-79.348574187, 43.706142543],
+          [-79.348422744, 43.706227855],
+          [-79.348288444, 43.706286182],
+          [-79.348150225, 43.706353505],
+          [-79.347957711, 43.706434255],
+          [-79.347774907, 43.706497017],
+          [-79.347603766, 43.706555292],
+          [-79.347433416, 43.706604568],
+          [-79.34728871, 43.706635875],
+          [-79.34720954, 43.706662772],
+          [-79.343625497, 43.70747321],
+        ],
+      },
+      roadId: 278,
+    },
+    {
+      centrelineId: 3829449,
+      geom: {
+        type: 'LineString',
+        coordinates: [
+          [-79.342780375, 43.705549757],
+          [-79.342832909, 43.705620743],
+          [-79.342877636, 43.705707982],
+          [-79.342922628, 43.705807202],
+          [-79.342964689, 43.705903473],
+          [-79.342983796, 43.705947236],
+          [-79.343026878, 43.706042077],
+          [-79.343056291, 43.706120807],
+          [-79.343088579, 43.706206083],
+          [-79.343625497, 43.70747321],
+        ],
+      },
+      roadId: 354,
+    },
+    {
+      centrelineId: 6853474,
+      geom: {
+        type: 'LineString',
+        coordinates: [
+          [-79.343625497, 43.70747321],
+          [-79.341387089, 43.708017748],
+        ],
+      },
+      roadId: 278,
+    },
+    {
+      centrelineId: 8895679,
+      geom: {
+        type: 'LineString',
+        coordinates: [
+          [-79.343625497, 43.70747321],
+          [-79.343716829, 43.7076292],
+          [-79.3439986, 43.708259097],
+        ],
+      },
+      roadId: 354,
+    },
+  ];
+
+  return {
+    count,
+    countData: countData_5_38661,
+    intersection,
+    segments,
+    study,
+  };
+}
 
 test('ReportIntersectionSummary#transformData [Overlea and Thorncliffe: 5/38661]', () => {
   const reportInstance = new ReportIntersectionSummary();
 
-  const count = {
-    date: DateTime.fromSQL('2019-04-13 00:00:00'),
-    locationDesc: 'OVERLEA BLVD AT THORNCLIFFE PARK DR & E TCS (PX 679)',
-    type: { studyType: StudyType.TMC },
-  };
+  const {
+    count,
+    countData,
+    intersection,
+    segments,
+    study,
+  } = setup_5_38661();
 
-  const hourlyData = ReportBaseFlowDirectional.sumHourly(countData_5_38661);
-  const hourlyMajorDirections = hourlyData.map(
-    () => [CardinalDirection.EAST, CardinalDirection.WEST],
-  );
-  const hourlyMinorDirections = hourlyData.map(
-    () => [CardinalDirection.NORTH, CardinalDirection.SOUTH],
-  );
-
-  const transformedData = reportInstance.transformData(count, {
-    countData: countData_5_38661,
-    hourlyData,
-    hourlyMajorDirections,
-    hourlyMinorDirections,
+  let transformedData = reportInstance.transformData(study, {
+    count,
+    countData,
+    intersection,
+    segments,
   });
+  const { hours, px, ...transformedDataRest } = transformedData;
+  expect(hours).toBe(StudyHours.ROUTINE);
+  expect(px).toBe(679);
+  transformedData = transformedDataRest;
   expect(transformedData).toEqual(transformedData_INTERSECTION_SUMMARY_5_38661);
 });
 
 test('ReportIntersectionSummary#generateCsv [Overlea and Thorncliffe: 5/38661]', () => {
   const reportInstance = new ReportIntersectionSummary();
 
-  const count = {
-    date: DateTime.fromSQL('2019-04-13 00:00:00'),
-    locationDesc: 'OVERLEA BLVD AT THORNCLIFFE PARK DR & E TCS (PX 679)',
-    type: { studyType: StudyType.TMC },
-  };
+  const {
+    count,
+    countData,
+    intersection,
+    segments,
+    study,
+  } = setup_5_38661();
 
-  const hourlyData = ReportBaseFlowDirectional.sumHourly(countData_5_38661);
-  const hourlyMajorDirections = hourlyData.map(
-    () => [CardinalDirection.EAST, CardinalDirection.WEST],
-  );
-  const hourlyMinorDirections = hourlyData.map(
-    () => [CardinalDirection.NORTH, CardinalDirection.SOUTH],
-  );
-
-  const transformedData = reportInstance.transformData(count, {
-    countData: countData_5_38661,
-    hourlyData,
-    hourlyMajorDirections,
-    hourlyMinorDirections,
+  const transformedData = reportInstance.transformData(study, {
+    count,
+    countData,
+    intersection,
+    segments,
   });
   expect(() => {
     reportInstance.generateCsv(count, transformedData);
--- tests/jest/unit/reports/ReportWarrantTrafficSignalControl.spec.js
+++ tests/jest/unit/reports/ReportWarrantTrafficSignalControl.spec.js
@@ -1,16 +1,10 @@
 /* eslint-disable camelcase */
 import path from 'path';
 
-import { CardinalDirection, FeatureCode, StudyType } from '@/lib/Constants';
+import { CentrelineType, StudyHours, StudyType } from '@/lib/Constants';
 import { NotImplementedError } from '@/lib/error/MoveErrors';
-import ReportBaseFlowDirectional from '@/lib/reports/ReportBaseFlowDirectional';
 import ReportWarrantTrafficSignalControl from '@/lib/reports/ReportWarrantTrafficSignalControl';
 import { loadJsonSync } from '@/lib/test/TestDataLoader';
-import {
-  generateHourlyMajorAndMinorDirections,
-  generateTmc,
-} from '@/lib/test/random/CountDataGenerator';
-import DateTime from '@/lib/time/DateTime';
 
 const countData_5_38661 = loadJsonSync(
   path.resolve(__dirname, './data/countData_5_38661.json'),
@@ -19,107 +13,163 @@ const transformedData_WARRANT_TRAFFIC_SIGNAL_CONTROL_5_38661 = loadJsonSync(
   path.resolve(__dirname, './data/transformedData_WARRANT_TRAFFIC_SIGNAL_CONTROL_5_38661.json'),
 );
 
-test('ReportWarrantTrafficSignalControl#transformData', () => {
-  const reportInstance = new ReportWarrantTrafficSignalControl();
+function setup_5_38661() {
+  const study = {
+    centrelineId: 13456854,
+    centrelineType: CentrelineType.INTERSECTION,
+    countGroupId: 38661,
+    type: { id: 5, studyType: StudyType.TMC },
+  };
 
-  // fuzz test
-  for (let i = 0; i < 25; i++) {
-    const countData = generateTmc();
-    const hourlyData = ReportBaseFlowDirectional.sumHourly(countData);
-    // TODO: generate minFeatureCode
-    const minFeatureCode = FeatureCode.MINOR_ARTERIAL;
-    const {
-      hourlyMajorDirections,
-      hourlyMinorDirections,
-    } = generateHourlyMajorAndMinorDirections(hourlyData);
-    // TODO: generate segments
-    const segments = new Array(4).fill(0);
-    const options = {
-      adequateTrial: true,
-      preparedBy: 'Foo Bar',
-      preventablesByYear: [3, 5, 10],
-      startYear: 2016,
-    };
-    expect(() => {
-      reportInstance.transformData(null, {
-        countData,
-        hourlyData,
-        hourlyMajorDirections,
-        hourlyMinorDirections,
-        minFeatureCode,
-        segments,
-      }, options);
-    }).not.toThrow();
-  }
-});
+  const count = {
+    hours: StudyHours.ROUTINE,
+    id: 38661,
+    locationDesc: 'OVERLEA BLVD AT THORNCLIFFE PARK DR & E TCS (PX 679)',
+  };
+  const intersection = {
+    geom: {
+      type: 'Point',
+      coordinates: [-79.343625497, 43.70747321],
+    },
+  };
+  const segments = [
+    {
+      centrelineId: 649,
+      geom: {
+        type: 'LineString',
+        coordinates: [
+          [-79.349877201, 43.70500665],
+          [-79.349308494, 43.705553926],
+          [-79.349210149, 43.705648311],
+          [-79.349133972, 43.70572022],
+          [-79.349046938, 43.705796617],
+          [-79.348959121, 43.705868512],
+          [-79.348795579, 43.705989814],
+          [-79.34868914, 43.706070684],
+          [-79.348574187, 43.706142543],
+          [-79.348422744, 43.706227855],
+          [-79.348288444, 43.706286182],
+          [-79.348150225, 43.706353505],
+          [-79.347957711, 43.706434255],
+          [-79.347774907, 43.706497017],
+          [-79.347603766, 43.706555292],
+          [-79.347433416, 43.706604568],
+          [-79.34728871, 43.706635875],
+          [-79.34720954, 43.706662772],
+          [-79.343625497, 43.70747321],
+        ],
+      },
+      roadId: 278,
+    },
+    {
+      centrelineId: 3829449,
+      geom: {
+        type: 'LineString',
+        coordinates: [
+          [-79.342780375, 43.705549757],
+          [-79.342832909, 43.705620743],
+          [-79.342877636, 43.705707982],
+          [-79.342922628, 43.705807202],
+          [-79.342964689, 43.705903473],
+          [-79.342983796, 43.705947236],
+          [-79.343026878, 43.706042077],
+          [-79.343056291, 43.706120807],
+          [-79.343088579, 43.706206083],
+          [-79.343625497, 43.70747321],
+        ],
+      },
+      roadId: 354,
+    },
+    {
+      centrelineId: 6853474,
+      geom: {
+        type: 'LineString',
+        coordinates: [
+          [-79.343625497, 43.70747321],
+          [-79.341387089, 43.708017748],
+        ],
+      },
+      roadId: 278,
+    },
+    {
+      centrelineId: 8895679,
+      geom: {
+        type: 'LineString',
+        coordinates: [
+          [-79.343625497, 43.70747321],
+          [-79.343716829, 43.7076292],
+          [-79.3439986, 43.708259097],
+        ],
+      },
+      roadId: 354,
+    },
+  ];
+
+  return {
+    count,
+    countData: countData_5_38661,
+    intersection,
+    segments,
+    study,
+  };
+}
 
 test('ReportWarrantTrafficSignalControl#transformData [Overlea and Thorncliffe: 5/38661]', () => {
   const reportInstance = new ReportWarrantTrafficSignalControl();
-
-  const count = {
-    date: DateTime.fromSQL('2019-04-13 00:00:00'),
-    locationDesc: 'OVERLEA BLVD AT THORNCLIFFE PARK DR & E TCS (PX 679)',
-    type: { studyType: StudyType.TMC },
-  };
-
-  const hourlyData = ReportBaseFlowDirectional.sumHourly(countData_5_38661);
-  const hourlyMajorDirections = hourlyData.map(
-    () => [CardinalDirection.EAST, CardinalDirection.WEST],
-  );
-  const hourlyMinorDirections = hourlyData.map(
-    () => [CardinalDirection.NORTH, CardinalDirection.SOUTH],
-  );
-  const minFeatureCode = FeatureCode.MAJOR_ARTERIAL;
-  const segments = new Array(4).fill(0);
-
+  const {
+    count,
+    countData,
+    intersection,
+    segments,
+    study,
+  } = setup_5_38661();
   const options = {
     adequateTrial: true,
     preparedBy: 'Foo Bar',
     preventablesByYear: [3, 5, 10],
     startYear: 2016,
   };
-  const transformedData = reportInstance.transformData(count, {
-    countData: countData_5_38661,
-    hourlyData,
-    hourlyMajorDirections,
-    hourlyMinorDirections,
-    minFeatureCode,
+
+  let transformedData = reportInstance.transformData(study, {
+    count,
+    countData,
+    intersection,
     segments,
   }, options);
+  const {
+    hours,
+    isTwoLane,
+    isXIntersection,
+    px,
+    ...transformedDataRest
+  } = transformedData;
+  expect(hours).toBe(StudyHours.ROUTINE);
+  expect(isTwoLane).toBe(false);
+  expect(isXIntersection).toBe(true);
+  expect(px).toBe(679);
+  transformedData = transformedDataRest;
   expect(transformedData).toEqual(transformedData_WARRANT_TRAFFIC_SIGNAL_CONTROL_5_38661);
 });
 
 test('ReportWarrantTrafficSignalControl#generateCsv [Overlea and Thorncliffe: 5/38661]', () => {
   const reportInstance = new ReportWarrantTrafficSignalControl();
-
-  const count = {
-    date: DateTime.fromSQL('2019-04-13 00:00:00'),
-    locationDesc: 'OVERLEA BLVD AT THORNCLIFFE PARK DR & E TCS (PX 679)',
-    type: { studyType: StudyType.TMC },
-  };
-
-  const hourlyData = ReportBaseFlowDirectional.sumHourly(countData_5_38661);
-  const hourlyMajorDirections = hourlyData.map(
-    () => [CardinalDirection.EAST, CardinalDirection.WEST],
-  );
-  const hourlyMinorDirections = hourlyData.map(
-    () => [CardinalDirection.NORTH, CardinalDirection.SOUTH],
-  );
-  const minFeatureCode = FeatureCode.MAJOR_ARTERIAL;
-  const segments = new Array(4).fill(0);
-
+  const {
+    count,
+    countData,
+    intersection,
+    segments,
+    study,
+  } = setup_5_38661();
   const options = {
     adequateTrial: true,
     preparedBy: 'Foo Bar',
     preventablesByYear: [3, 5, 10],
     startYear: 2016,
   };
-  const transformedData = reportInstance.transformData(count, {
-    countData: countData_5_38661,
-    hourlyData,
-    hourlyMajorDirections,
-    hourlyMinorDirections,
-    minFeatureCode,
+  const transformedData = reportInstance.transformData(study, {
+    count,
+    countData,
+    intersection,
     segments,
   }, options);
   expect(() => {
