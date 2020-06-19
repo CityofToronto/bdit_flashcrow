6cfd0f665dd0370e8ed60df808e1dd121c387523
EC2 Default User <ec2-user@ip-10-162-43-198.ca-central-1.compute.internal>
2020-06-19 19:38:04 +0000

    Add traffic signals to location search index
    in preparation for supporting signals and arterycodes within the location search bar.

--- scripts/airflow/dags/location_search_index.py
+++ scripts/airflow/dags/location_search_index.py
@@ -14,3 +14,4 @@ SCHEDULE_INTERVAL = '30 6 * * 6'
 DAG = create_dag(__file__, __doc__, START_DATE, SCHEDULE_INTERVAL)
 
 TRANSFORM_INTERSECTIONS_INDEX = create_bash_task_nested(DAG, 'transform_intersections_index')
+TRANSFORM_TRAFFIC_SIGNAL = create_bash_task_nested(DAG, 'transform_traffic_signal')
--- scripts/airflow/tasks/location_search_index/transform_intersections_index.sql
+++ scripts/airflow/tasks/location_search_index/transform_intersections_index.sql
@@ -1,6 +1,6 @@
 CREATE SCHEMA IF NOT EXISTS location_search;
 
-CREATE MATERIALIZED VIEW IF NOT EXISTS location_search.centreline_intersection AS
+CREATE MATERIALIZED VIEW IF NOT EXISTS location_search.centreline_intersection AS (
   SELECT
     int_id,
     MODE() WITHIN GROUP (ORDER BY intersec5) AS intersec5,
@@ -9,7 +9,8 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS location_search.centreline_intersection A
   WHERE
     intersec5 IS NOT NULL AND intersec5 LIKE '%/%' AND intersec5 NOT LIKE '% Pl%'
     AND elevatio9 != 0 AND elevatio9 < 501700
-  GROUP BY int_id;
+  GROUP BY int_id
+);
 
 CREATE UNIQUE INDEX IF NOT EXISTS ls_centreline_intersection_int_id ON location_search.centreline_intersection (int_id);
 CREATE INDEX IF NOT EXISTS ls_centreline_intersection_tsvector_intersec5 ON location_search.centreline_intersection USING GIN (to_tsvector('english', intersec5));
