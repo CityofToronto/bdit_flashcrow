e48a0f46717afd0fbdb5a130a825f69fe3d4c1d4
EC2 Default User <ec2-user@ip-10-162-43-198.ca-central-1.compute.internal>
2020-06-19 18:56:15 +0000

    Migrate location search index to nested format
    in preparation for further work here.

--- scripts/airflow/dags/location_search_index.py
+++ scripts/airflow/dags/location_search_index.py
@@ -7,10 +7,10 @@ supplements geocoding and suggestion APIs provided by GCC.
 # pylint: disable=pointless-statement
 from datetime import datetime
 
-from airflow_utils import create_dag, create_bash_task
+from airflow_utils import create_dag, create_bash_task_nested
 
 START_DATE = datetime(2020, 4, 5)
 SCHEDULE_INTERVAL = '30 6 * * 6'
 DAG = create_dag(__file__, __doc__, START_DATE, SCHEDULE_INTERVAL)
 
-TRANSFORM_INTERSECTIONS_INDEX = create_bash_task(DAG, 'transform_intersections_index')
+TRANSFORM_INTERSECTIONS_INDEX = create_bash_task_nested(DAG, 'transform_intersections_index')
