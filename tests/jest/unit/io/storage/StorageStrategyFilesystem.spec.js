66f4ee4008ecc8ac4e3b766d23f3396cea673db9
Evan Savage <Evan.Savage@toronto.ca>
2020-08-12 20:29:47 +0000

    Finish up stream API tests for filesystem strategy

--- tests/jest/unit/io/storage/StorageStrategyFilesystem.spec.js
+++ tests/jest/unit/io/storage/StorageStrategyFilesystem.spec.js
@@ -6,9 +6,9 @@ import StorageStrategyFilesystem from '@/lib/io/storage/StorageStrategyFilesyste
 
 jest.mock('fs');
 
-function randomBuffer(n) {
-  const arr = new Uint8Array(n);
-  for (let i = 0; i < n; i++) {
+function randomBuffer() {
+  const arr = new Uint8Array(8);
+  for (let i = 0; i < 8; i++) {
     arr[i] = Random.range(0, 256);
   }
   return Buffer.from(arr);
@@ -46,22 +46,22 @@ test('StorageStrategyFilesystem [buffer API]', async () => {
 test('StorageStrategyFilesystem [buffer API, fuzz test]', async () => {
   const storage = new StorageStrategyFilesystem('/data/move-storage/buffer');
 
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 30; i++) {
     const namespace = Random.choice(['a', 'b', 'c']);
-    const key = Random.range(100, 200).toString();
-    const n = Random.range(32, 256);
-    const value = randomBuffer(n);
+    const key = Random.range(10, 40).toString();
+    const value = randomBuffer();
 
     await expect(storage.put(namespace, key, value)).resolves.toEqual(true);
     await expect(storage.has(namespace, key)).resolves.toEqual(true);
     await expect(storage.get(namespace, key)).resolves.toEqual(value);
   }
 
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 30; i++) {
     const namespace = Random.choice(['a', 'b', 'c']);
-    const key = Random.range(100, 200).toString();
+    const key = Random.range(10, 40).toString();
     const hasKey = await storage.has(namespace, key);
     await expect(storage.delete(namespace, key)).resolves.toEqual(hasKey);
+    await expect(storage.has(namespace, key)).resolves.toEqual(false);
   }
 });
 
@@ -98,16 +98,51 @@ test('StorageStrategyFilesystem [stream API]', async () => {
   const storage = new StorageStrategyFilesystem('/data/move-storage/stream');
   const namespace = 'foo';
 
-  const key = 'bar';
+  let key = 'bar';
   await expect(storage.has(namespace, key)).resolves.toEqual(false);
   let valueStream = storage.getStream(namespace, key);
   await expect(readableStreamToBuffer(valueStream)).rejects.toBeInstanceOf(Error);
 
-  const value = Buffer.from('baz');
+  let value = Buffer.from('baz');
   valueStream = bufferToDuplexStream(value);
-  const writableStream = await storage.putStream(namespace, key, valueStream);
+  let writableStream = await storage.putStream(namespace, key, valueStream);
   await writableStreamFinish(writableStream);
   await expect(storage.has(namespace, key)).resolves.toEqual(true);
   valueStream = storage.getStream(namespace, key);
   await expect(readableStreamToBuffer(valueStream)).resolves.toEqual(value);
+
+  key = 'zing';
+  writableStream = await storage.putStream(namespace, key, valueStream);
+  await writableStreamFinish(writableStream);
+
+  key = 'bar';
+  value = Buffer.from('frob');
+  valueStream = bufferToDuplexStream(value);
+  writableStream = await storage.putStream(namespace, key, valueStream);
+  await writableStreamFinish(writableStream);
+  await expect(storage.has(namespace, key)).resolves.toEqual(true);
+  valueStream = storage.getStream(namespace, key);
+  await expect(readableStreamToBuffer(valueStream)).resolves.toEqual(value);
+
+  await expect(storage.delete(namespace, key)).resolves.toEqual(true);
+  await expect(storage.has(namespace, key)).resolves.toEqual(false);
+  valueStream = storage.getStream(namespace, key);
+  await expect(readableStreamToBuffer(valueStream)).rejects.toBeInstanceOf(Error);
+
+  await expect(storage.delete(namespace, key)).resolves.toEqual(false);
+});
+
+test('StorageStrategyFilesystem [stream API, fuzz test]', async () => {
+  const storage = new StorageStrategyFilesystem('/data/move-storage/stream');
+
+  for (let i = 0; i < 30; i++) {
+    const namespace = Random.choice(['a', 'b', 'c']);
+    const key = Random.range(10, 40).toString();
+    const value = randomBuffer();
+    const valueStream = bufferToDuplexStream(value);
+    const writableStream = await storage.putStream(namespace, key, valueStream);
+    await writableStreamFinish(writableStream);
+    await expect(storage.has(namespace, key)).resolves.toEqual(true);
+    await expect(storage.get(namespace, key)).resolves.toEqual(value);
+  }
 });
