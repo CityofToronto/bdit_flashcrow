2cd9dcc796cb64f475253f04680badb9389f803e
Evan Savage <Evan.Savage@toronto.ca>
2020-08-12 20:20:51 +0000

    Streaming API for storage strategies
    This turns out to be an important prerequisite of working with ZIP libraries in node.

--- tests/jest/unit/io/storage/StorageStrategyFilesystem.spec.js
+++ tests/jest/unit/io/storage/StorageStrategyFilesystem.spec.js
@@ -1,11 +1,21 @@
 /* eslint-disable no-await-in-loop */
+import stream from 'stream';
+
 import Random from '@/lib/Random';
 import StorageStrategyFilesystem from '@/lib/io/storage/StorageStrategyFilesystem';
 
 jest.mock('fs');
 
-test('StorageStrategyFilesystem', async () => {
-  const storage = new StorageStrategyFilesystem('/data/move-storage');
+function randomBuffer(n) {
+  const arr = new Uint8Array(n);
+  for (let i = 0; i < n; i++) {
+    arr[i] = Random.range(0, 256);
+  }
+  return Buffer.from(arr);
+}
+
+test('StorageStrategyFilesystem [buffer API]', async () => {
+  const storage = new StorageStrategyFilesystem('/data/move-storage/buffer');
   const namespace = 'foo';
 
   let key = 'bar';
@@ -33,16 +43,8 @@ test('StorageStrategyFilesystem', async () => {
   await expect(storage.delete(namespace, key)).resolves.toEqual(false);
 });
 
-function randomBuffer(n) {
-  const arr = new Uint8Array(n);
-  for (let i = 0; i < n; i++) {
-    arr[i] = Random.range(0, 256);
-  }
-  return Buffer.from(arr);
-}
-
-test('StorageStrategyFilesystem [fuzz test]', async () => {
-  const storage = new StorageStrategyFilesystem('/data/move-storage');
+test('StorageStrategyFilesystem [buffer API, fuzz test]', async () => {
+  const storage = new StorageStrategyFilesystem('/data/move-storage/buffer');
 
   for (let i = 0; i < 100; i++) {
     const namespace = Random.choice(['a', 'b', 'c']);
@@ -61,4 +63,51 @@ test('StorageStrategyFilesystem [fuzz test]', async () => {
     const hasKey = await storage.has(namespace, key);
     await expect(storage.delete(namespace, key)).resolves.toEqual(hasKey);
   }
+});
+
+async function readableStreamToBuffer(readableStream) {
+  return new Promise((resolve, reject) => {
+    const bufs = [];
+    readableStream
+      .on('error', reject)
+      .on('data', buf => bufs.push(buf))
+      .on('end', () => resolve(Buffer.concat(bufs)));
+  });
+}
+
+function bufferToDuplexStream(buffer) {
+  const duplexStream = new stream.PassThrough();
+  duplexStream.write(buffer, (err) => {
+    if (err) {
+      throw err;
+    }
+    duplexStream.end();
+  });
+  return duplexStream;
+}
+
+async function writableStreamFinish(writableStream) {
+  return new Promise((resolve, reject) => {
+    writableStream
+      .on('error', reject)
+      .on('finish', () => resolve());
+  });
+}
+
+test('StorageStrategyFilesystem [stream API]', async () => {
+  const storage = new StorageStrategyFilesystem('/data/move-storage/stream');
+  const namespace = 'foo';
+
+  const key = 'bar';
+  await expect(storage.has(namespace, key)).resolves.toEqual(false);
+  let valueStream = storage.getStream(namespace, key);
+  await expect(readableStreamToBuffer(valueStream)).rejects.toBeInstanceOf(Error);
+
+  const value = Buffer.from('baz');
+  valueStream = bufferToDuplexStream(value);
+  const writableStream = await storage.putStream(namespace, key, valueStream);
+  await writableStreamFinish(writableStream);
+  await expect(storage.has(namespace, key)).resolves.toEqual(true);
+  valueStream = storage.getStream(namespace, key);
+  await expect(readableStreamToBuffer(valueStream)).resolves.toEqual(value);
 });
