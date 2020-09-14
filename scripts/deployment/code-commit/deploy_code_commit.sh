a070412e96d3b89f2cf58f683c18a11cda2875fb
Evan Savage <Evan.Savage@toronto.ca>
2020-09-14 13:42:21 +0000

    Update CI configuration to use current branch for deployment
    and test to make sure we're on a deployable branch in `deploy_code_commit.sh`.  I'm going to merge this into `master`, then perform the merge upwards to `deploy-qa` separately as the real test.

--- scripts/deployment/code-commit/deploy_code_commit.sh
+++ scripts/deployment/code-commit/deploy_code_commit.sh
@@ -2,7 +2,17 @@
 
 set -euo pipefail
 
-# notify prj_move on Slack
-curl -H "Content-Type: application/json; charset=utf-8" -d "{\"text\": \"<!here> MOVE down for deployment: ${CI_COMMIT_REF_NAME} @ ${CI_COMMIT_SHORT_SHA} -> flashcrow-dev0\"}" "$SLACK_WEBHOOK_URL"
+TARGET_ENV=
+if [ "${CI_COMMIT_REF_NAME}" = "master" ]; then
+  TARGET_ENV="flashcrow-dev0"
+elif [ "${CI_COMMIT_REF_NAME}" = "deploy-qa" ]; then
+  TARGET_ENV="flashcrow-qa0"
+else
+  echo "Invalid branch for deployment: ${CI_COMMIT_REF_NAME}"
+  exit 1
+fi
 
-git push https://git-codecommit.ca-central-1.amazonaws.com/v1/repos/bdit_flashcrow HEAD:master
+# notify prj_move on Slack
+curl -H "Content-Type: application/json; charset=utf-8" -d "{\"text\": \"<!here> MOVE down for deployment: ${CI_COMMIT_REF_NAME} @ ${CI_COMMIT_SHORT_SHA} -> ${TARGET_ENV}\"}" "$SLACK_WEBHOOK_URL"
+
+git push https://git-codecommit.ca-central-1.amazonaws.com/v1/repos/bdit_flashcrow "HEAD:${CI_COMMIT_REF_NAME}"
