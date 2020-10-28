8f24aa2b831401b5e4a69d9672975518f7ea647e
Evan Savage <Evan.Savage@toronto.ca>
2020-10-28 18:30:21 +0000

    Cover commenting endpoints in StudyRequestController
    to complete this part of REST API testing.  (Fixed a couple of minor bugs in the process!)

--- tests/jest/api/StudyRequestController.spec.js
+++ tests/jest/api/StudyRequestController.spec.js
@@ -450,3 +450,323 @@ test('StudyRequestController.putStudyRequest [status changes]', async () => {
   });
   expect(response.statusCode).toBe(HttpStatus.BAD_REQUEST.statusCode);
 });
+
+function expectStudyRequestChanges(actual, expected) {
+  expect(actual).toHaveLength(expected.length);
+  expected.forEach((status, i) => {
+    expect(actual[i].status).toBe(status);
+  });
+}
+
+test('StudyRequestController.getStudyRequestChanges', async () => {
+  const transientStudyRequest = generateStudyRequest();
+  mockDAOsForStudyRequest(transientStudyRequest);
+
+  client.setUser(requester);
+  let response = await client.fetch('/requests/study', {
+    method: 'POST',
+    data: transientStudyRequest,
+  });
+  let persistedStudyRequest = response.result;
+
+  // newly created study request has no changes
+  response = await client.fetch(`/requests/study/${persistedStudyRequest.id}/changes`);
+  expect(response.statusCode).toBe(HttpStatus.OK.statusCode);
+  expectStudyRequestChanges(response.result, []);
+
+  persistedStudyRequest.notes = 'testing study request changes';
+  response = await client.fetch(`/requests/study/${persistedStudyRequest.id}`, {
+    method: 'PUT',
+    data: persistedStudyRequest,
+  });
+  persistedStudyRequest = response.result;
+
+  // non-status-related changes are not logged in changes table
+  response = await client.fetch(`/requests/study/${persistedStudyRequest.id}/changes`);
+  expect(response.statusCode).toBe(HttpStatus.OK.statusCode);
+  expectStudyRequestChanges(response.result, []);
+
+  persistedStudyRequest.status = StudyRequestStatus.CANCELLED;
+  persistedStudyRequest.closed = true;
+  response = await client.fetch(`/requests/study/${persistedStudyRequest.id}`, {
+    method: 'PUT',
+    data: persistedStudyRequest,
+  });
+  persistedStudyRequest = response.result;
+
+  // status-related changes are logged in change table
+  response = await client.fetch(`/requests/study/${persistedStudyRequest.id}/changes`);
+  expect(response.statusCode).toBe(HttpStatus.OK.statusCode);
+  expectStudyRequestChanges(response.result, [StudyRequestStatus.CANCELLED]);
+
+  // other users can fetch changes as well
+  client.setUser(ett1);
+  response = await client.fetch(`/requests/study/${persistedStudyRequest.id}/changes`);
+  expect(response.statusCode).toBe(HttpStatus.OK.statusCode);
+  expectStudyRequestChanges(response.result, [StudyRequestStatus.CANCELLED]);
+
+  client.setUser(requester);
+  persistedStudyRequest.status = StudyRequestStatus.REQUESTED;
+  persistedStudyRequest.closed = false;
+  response = await client.fetch(`/requests/study/${persistedStudyRequest.id}`, {
+    method: 'PUT',
+    data: persistedStudyRequest,
+  });
+  persistedStudyRequest = response.result;
+
+  // changes are returned most recent first
+  response = await client.fetch(`/requests/study/${persistedStudyRequest.id}/changes`);
+  expect(response.statusCode).toBe(HttpStatus.OK.statusCode);
+  expectStudyRequestChanges(response.result, [
+    StudyRequestStatus.REQUESTED,
+    StudyRequestStatus.CANCELLED,
+  ]);
+
+  client.setUser(supervisor);
+  persistedStudyRequest.status = StudyRequestStatus.CHANGES_NEEDED;
+  response = await client.fetch(`/requests/study/${persistedStudyRequest.id}`, {
+    method: 'PUT',
+    data: persistedStudyRequest,
+  });
+  persistedStudyRequest = response.result;
+
+  persistedStudyRequest.status = StudyRequestStatus.ASSIGNED;
+  persistedStudyRequest.assignedTo = StudyRequestAssignee.FIELD_STAFF;
+  response = await client.fetch(`/requests/study/${persistedStudyRequest.id}`, {
+    method: 'PUT',
+    data: persistedStudyRequest,
+  });
+  persistedStudyRequest = response.result;
+
+  persistedStudyRequest.assignedTo = StudyRequestAssignee.OTI;
+  response = await client.fetch(`/requests/study/${persistedStudyRequest.id}`, {
+    method: 'PUT',
+    data: persistedStudyRequest,
+  });
+  persistedStudyRequest = response.result;
+
+  response = await client.fetch(`/requests/study/${persistedStudyRequest.id}/changes`);
+  expect(response.statusCode).toBe(HttpStatus.OK.statusCode);
+  expectStudyRequestChanges(response.result, [
+    StudyRequestStatus.ASSIGNED,
+    StudyRequestStatus.CHANGES_NEEDED,
+    StudyRequestStatus.REQUESTED,
+    StudyRequestStatus.CANCELLED,
+  ]);
+});
+
+test('StudyRequestController [comments: post / get]', async () => {
+  const transientStudyRequest = generateStudyRequest();
+  mockDAOsForStudyRequest(transientStudyRequest);
+
+  client.setUser(requester);
+  let response = await client.fetch('/requests/study', {
+    method: 'POST',
+    data: transientStudyRequest,
+  });
+  let persistedStudyRequest = response.result;
+
+  // users can comment on their own requests: counts as edit operation
+  response = await client.fetch(`/requests/study/${persistedStudyRequest.id}/comments`, {
+    method: 'POST',
+    data: { comment: 'comment from requester' },
+  });
+  expect(response.statusCode).toBe(HttpStatus.OK.statusCode);
+  persistedStudyRequest = response.result.studyRequest;
+  const persistedComment1 = response.result.studyRequestComment;
+  expect(persistedStudyRequest.lastEditorId).toBe(requester.id);
+  expect(persistedComment1.userId).toBe(requester.id);
+  expect(persistedComment1.studyRequestId).toBe(persistedStudyRequest.id);
+
+  // getting comments
+  response = await client.fetch(`/requests/study/${persistedStudyRequest.id}/comments`);
+  expect(response.statusCode).toBe(HttpStatus.OK.statusCode);
+  expect(response.result).toEqual([persistedComment1]);
+
+  // other staff can comment on requests: also counts as edit operation
+  client.setUser(ett1);
+  response = await client.fetch(`/requests/study/${persistedStudyRequest.id}/comments`, {
+    method: 'POST',
+    data: { comment: 'comment from ETT1' },
+  });
+  expect(response.statusCode).toBe(HttpStatus.OK.statusCode);
+  persistedStudyRequest = response.result.studyRequest;
+  const persistedComment2 = response.result.studyRequestComment;
+  expect(persistedStudyRequest.lastEditorId).toBe(ett1.id);
+  expect(persistedComment2.userId).toBe(ett1.id);
+  expect(persistedComment2.studyRequestId).toBe(persistedStudyRequest.id);
+
+  // getting comments: other staff can see all comments, newest first
+  response = await client.fetch(`/requests/study/${persistedStudyRequest.id}/comments`);
+  expect(response.statusCode).toBe(HttpStatus.OK.statusCode);
+  expect(response.result).toEqual([persistedComment2, persistedComment1]);
+
+  // supervisors can comment on requests: also counts as edit operation
+  client.setUser(supervisor);
+  response = await client.fetch(`/requests/study/${persistedStudyRequest.id}/comments`, {
+    method: 'POST',
+    data: { comment: 'comment from supervisor' },
+  });
+  expect(response.statusCode).toBe(HttpStatus.OK.statusCode);
+  persistedStudyRequest = response.result.studyRequest;
+  const persistedComment3 = response.result.studyRequestComment;
+  expect(persistedStudyRequest.lastEditorId).toBe(supervisor.id);
+  expect(persistedComment3.userId).toBe(supervisor.id);
+  expect(persistedComment3.studyRequestId).toBe(persistedStudyRequest.id);
+
+  // getting comments: supervisors can see all comments, newest first
+  response = await client.fetch(`/requests/study/${persistedStudyRequest.id}/comments`);
+  expect(response.statusCode).toBe(HttpStatus.OK.statusCode);
+  expect(response.result).toEqual([persistedComment3, persistedComment2, persistedComment1]);
+});
+
+test('StudyRequestController [comments: put]', async () => {
+  const transientStudyRequest = generateStudyRequest();
+  mockDAOsForStudyRequest(transientStudyRequest);
+
+  client.setUser(requester);
+  let response = await client.fetch('/requests/study', {
+    method: 'POST',
+    data: transientStudyRequest,
+  });
+  let persistedStudyRequest = response.result;
+
+  client.setUser(ett1);
+  response = await client.fetch(`/requests/study/${persistedStudyRequest.id}/comments`, {
+    method: 'POST',
+    data: { comment: 'comment from ETT1' },
+  });
+  persistedStudyRequest = response.result.studyRequest;
+  const persistedComment = response.result.studyRequestComment;
+
+  // users can update their own comments
+  const urlPutComment = `/requests/study/${persistedStudyRequest.id}/comments/${persistedComment.id}`;
+  persistedComment.comment = 'comment from ETT1: now updated!';
+  response = await client.fetch(urlPutComment, {
+    method: 'PUT',
+    data: persistedComment,
+  });
+  expect(response.statusCode).toBe(HttpStatus.OK.statusCode);
+  persistedStudyRequest = response.result.studyRequest;
+  expect(response.result.studyRequestComment).toEqual(persistedComment);
+
+  // users cannot update others' comments
+  client.setUser(requester);
+  response = await client.fetch(urlPutComment, {
+    method: 'PUT',
+    data: {
+      ...persistedComment,
+      comment: 'yeah, but this is *really* from the requester',
+    },
+  });
+  expect(response.statusCode).toBe(HttpStatus.FORBIDDEN.statusCode);
+});
+
+test('StudyRequestController [comments: read-only fields]', async () => {
+  const transientStudyRequest = generateStudyRequest();
+  mockDAOsForStudyRequest(transientStudyRequest);
+
+  client.setUser(requester);
+  let response = await client.fetch('/requests/study', {
+    method: 'POST',
+    data: transientStudyRequest,
+  });
+  let persistedStudyRequest = response.result;
+
+  client.setUser(ett1);
+  response = await client.fetch(`/requests/study/${persistedStudyRequest.id}/comments`, {
+    method: 'POST',
+    data: { comment: 'comment from ETT1' },
+  });
+  persistedStudyRequest = response.result.studyRequest;
+  const persistedComment = response.result.studyRequestComment;
+
+  // cannot change ID
+  const urlPutComment = `/requests/study/${persistedStudyRequest.id}/comments/${persistedComment.id}`;
+  response = await client.fetch(urlPutComment, {
+    method: 'PUT',
+    data: {
+      ...persistedComment,
+      id: persistedComment.id + 1000,
+    },
+  });
+  expect(response.statusCode).toBe(HttpStatus.BAD_REQUEST.statusCode);
+
+  // cannot change createdAt timestamp
+  response = await client.fetch(urlPutComment, {
+    method: 'PUT',
+    data: {
+      ...persistedComment,
+      createdAt: DateTime.local().minus({ weeks: 3 }),
+    },
+  });
+  expect(response.statusCode).toBe(HttpStatus.BAD_REQUEST.statusCode);
+
+  // cannot change commenter ID
+  response = await client.fetch(urlPutComment, {
+    method: 'PUT',
+    data: {
+      ...persistedComment,
+      userId: requester.id,
+    },
+  });
+  expect(response.statusCode).toBe(HttpStatus.BAD_REQUEST.statusCode);
+
+  // cannot change study request ID
+  response = await client.fetch(urlPutComment, {
+    method: 'PUT',
+    data: {
+      ...persistedComment,
+      studyRequestId: persistedStudyRequest.id + 1000,
+    },
+  });
+  expect(response.statusCode).toBe(HttpStatus.BAD_REQUEST.statusCode);
+});
+
+test('StudyRequestController [comments: delete]', async () => {
+  const transientStudyRequest = generateStudyRequest();
+  mockDAOsForStudyRequest(transientStudyRequest);
+
+  client.setUser(requester);
+  let response = await client.fetch('/requests/study', {
+    method: 'POST',
+    data: transientStudyRequest,
+  });
+  let persistedStudyRequest = response.result;
+
+  client.setUser(ett1);
+  response = await client.fetch(`/requests/study/${persistedStudyRequest.id}/comments`, {
+    method: 'POST',
+    data: { comment: 'comment from ETT1' },
+  });
+  persistedStudyRequest = response.result.studyRequest;
+  const persistedComment = response.result.studyRequestComment;
+
+  // other users cannot delete comment
+  client.setUser(supervisor);
+  const urlDeleteComment = `/requests/study/${persistedStudyRequest.id}/comments/${persistedComment.id}`;
+  response = await client.fetch(urlDeleteComment, {
+    method: 'DELETE',
+  });
+  expect(response.statusCode).toBe(HttpStatus.FORBIDDEN.statusCode);
+
+  // users can delete their own comments
+  client.setUser(ett1);
+  response = await client.fetch(urlDeleteComment, {
+    method: 'DELETE',
+  });
+  expect(response.statusCode).toBe(HttpStatus.OK.statusCode);
+  persistedStudyRequest = response.result.studyRequest;
+  expect(response.result.studyRequestComment.success).toBe(true);
+
+  // deleting comment means there are no more comments
+  response = await client.fetch(`/requests/study/${persistedStudyRequest.id}/comments`);
+  expect(response.statusCode).toBe(HttpStatus.OK.statusCode);
+  expect(response.result).toEqual([]);
+
+  // users cannot delete non-existent comments
+  response = await client.fetch(urlDeleteComment, {
+    method: 'DELETE',
+  });
+  expect(response.statusCode).toBe(HttpStatus.NOT_FOUND.statusCode);
+});
