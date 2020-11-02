6c535c0d7b84a01f53869389586c3a0a7f8a5dea
Evan Savage <Evan.Savage@toronto.ca>
2020-11-02 22:14:37 +0000

    Add refs/heads prefix to CD script
    so that this will work for newly-formed environments as well.

--- scripts/deployment/code-commit/deploy_code_commit.sh
+++ scripts/deployment/code-commit/deploy_code_commit.sh
@@ -15,4 +15,4 @@ fi
 # notify prj_move on Slack
 curl -H "Content-Type: application/json; charset=utf-8" -d "{\"text\": \"<!here> MOVE down for deployment: ${CI_COMMIT_REF_NAME} @ ${CI_COMMIT_SHORT_SHA} -> ${TARGET_ENV}\"}" "$SLACK_WEBHOOK_URL"
 
-git push https://git-codecommit.ca-central-1.amazonaws.com/v1/repos/bdit_flashcrow "HEAD:${CI_COMMIT_REF_NAME}"
+git push https://git-codecommit.ca-central-1.amazonaws.com/v1/repos/bdit_flashcrow "HEAD:refs/heads/${CI_COMMIT_REF_NAME}"
