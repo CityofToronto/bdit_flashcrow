24b08ac1f7f3f2c02c13dfbe0288c66d0fe80238
Evan Savage <Evan.Savage@toronto.ca>
2020-11-10 22:22:16 +0000

    Refactor request export into separate module
    to make it easier to change this in isolation.

--- web/views/FcRequestsTrack.vue
+++ web/views/FcRequestsTrack.vue
@@ -94,13 +94,11 @@
 </template>
 
 <script>
-import { csvFormat } from 'd3-dsv';
 import { saveAs } from 'file-saver';
 import { Ripple } from 'vuetify/lib/directives';
 import { mapActions, mapState } from 'vuex';
 
 import { AuthScope } from '@/lib/Constants';
-import { formatDuration } from '@/lib/StringFormatters';
 import { getStudyRequests } from '@/lib/api/WebApi';
 import { filterItem, getFilterChips } from '@/lib/requests/RequestFilters';
 import {
@@ -108,8 +106,8 @@ import {
   getStudyRequestBulkItem,
 } from '@/lib/requests/RequestItems';
 import RequestDataTableColumns from '@/lib/requests/RequestDataTableColumns';
+import RequestItemExport from '@/lib/requests/RequestItemExport';
 import { ItemType } from '@/lib/requests/RequestStudyBulkUtils';
-import TimeFormatters from '@/lib/time/TimeFormatters';
 import FcDataTableRequests from '@/web/components/FcDataTableRequests.vue';
 import FcButton from '@/web/components/inputs/FcButton.vue';
 import FcSearchBarRequests from '@/web/components/inputs/FcSearchBarRequests.vue';
@@ -122,73 +120,6 @@ import FcMenuStudyRequestsStatus
   from '@/web/components/requests/status/FcMenuStudyRequestsStatus.vue';
 import FcMixinAuthScope from '@/web/mixins/FcMixinAuthScope';
 import FcMixinRouteAsync from '@/web/mixins/FcMixinRouteAsync';
-
-function getItemRow(item) {
-  let { location, requestedBy } = item;
-  if (location !== null) {
-    location = location.description;
-  }
-  if (requestedBy !== null) {
-    requestedBy = requestedBy.uniqueName;
-  }
-
-  const { studyRequest } = item;
-  const {
-    assignedTo,
-    geom: {
-      coordinates: [lng, lat],
-    },
-    hours,
-    id,
-    notes,
-    status,
-    studyType,
-    urgent,
-    urgentReason,
-  } = studyRequest;
-
-  let {
-    createdAt,
-    daysOfWeek,
-    dueDate,
-    duration,
-    estimatedDeliveryDate,
-    lastEditedAt,
-  } = studyRequest;
-  createdAt = TimeFormatters.formatDefault(createdAt);
-  daysOfWeek = TimeFormatters.formatDaysOfWeek(daysOfWeek);
-  dueDate = TimeFormatters.formatDefault(studyRequest.dueDate);
-  if (studyType.automatic) {
-    duration = formatDuration(duration);
-  } else {
-    duration = null;
-  }
-  estimatedDeliveryDate = TimeFormatters.formatDefault(studyRequest.estimatedDeliveryDate);
-  if (lastEditedAt !== null) {
-    lastEditedAt = TimeFormatters.formatDefault(lastEditedAt);
-  }
-
-  return {
-    assignedTo,
-    createdAt,
-    daysOfWeek,
-    dueDate,
-    duration,
-    estimatedDeliveryDate,
-    hours,
-    id,
-    lastEditedAt,
-    lat,
-    lng,
-    location,
-    notes,
-    requestedBy,
-    status,
-    studyType,
-    urgent,
-    urgentReason,
-  };
-}
 
 export default {
   name: 'FcRequestsTrack',
@@ -306,28 +237,7 @@ export default {
   },
   methods: {
     actionDownload(items) {
-      const rows = items.map(getItemRow);
-      const columns = [
-        'id',
-        'location',
-        'studyType',
-        'requestedBy',
-        'createdAt',
-        'assignedTo',
-        'dueDate',
-        'status',
-        'lastEditedAt',
-        'urgent',
-        'urgentReason',
-        'estimatedDeliveryDate',
-        'lng',
-        'lat',
-        'daysOfWeek',
-        'duration',
-        'hours',
-        'notes',
-      ];
-      const csvStr = csvFormat(rows, columns);
+      const csvStr = RequestItemExport.get(items);
       const csvData = new Blob([csvStr], { type: 'text/csv' });
       saveAs(csvData, 'requests.csv');
     },
