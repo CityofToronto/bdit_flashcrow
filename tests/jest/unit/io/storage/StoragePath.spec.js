1b4a7d35d5eefcab5cc297d0e839bf13375f791b
Evan Savage <Evan.Savage@toronto.ca>
2020-11-16 19:49:42 +0000

    Make StoragePath methods async
    so that we can use DAOs to fetch the information needed to build filenames.

--- tests/jest/unit/io/storage/StoragePath.spec.js
+++ tests/jest/unit/io/storage/StoragePath.spec.js
@@ -1,13 +1,13 @@
 import { ReportFormat, ReportType } from '@/lib/Constants';
 import StoragePath from '@/lib/io/storage/StoragePath';
 
-test('StoragePath.forReport', () => {
+test('StoragePath.forReport', async () => {
   let report = {
     type: ReportType.SPEED_PERCENTILE,
     id: '1/2345',
     format: ReportFormat.CSV,
   };
-  expect(StoragePath.forReport(report)).toEqual({
+  await expect(StoragePath.forReport(report)).resolves.toEqual({
     namespace: StoragePath.NAMESPACE_REPORTS,
     key: 'SPEED_PERCENTILE_1_2345.csv',
   });
@@ -20,19 +20,19 @@ test('StoragePath.forReport', () => {
     foo: 1,
     bar: 'baz',
   };
-  expect(StoragePath.forReport(report)).toEqual({
+  await expect(StoragePath.forReport(report)).resolves.toEqual({
     namespace: StoragePath.NAMESPACE_REPORTS,
     key: 'COLLISION_DIRECTORY_s1:AkttmBoXtmB_204c1293209bcf240d71f87d91f3f0eb.pdf',
   });
 });
 
-test('StoragePath.forReportZip', () => {
+test('StoragePath.forReportZip', async () => {
   const report = {
     type: ReportType.SPEED_PERCENTILE,
     id: '1/2345',
     format: ReportFormat.CSV,
   };
-  const { namespace, key } = StoragePath.forReportZip([report]);
+  const { namespace, key } = await StoragePath.forReportZip([report]);
   expect(namespace).toEqual(StoragePath.NAMESPACE_REPORTS);
   expect(key).toMatch(/[a-f0-9]+\.zip/);
 });
