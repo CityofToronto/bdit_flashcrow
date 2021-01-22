e1b525fdc50a11bb8a74afb874d2046d6cd44459
Evan Savage <Evan.Savage@toronto.ca>
2021-01-22 18:20:15 +0000

    New comment email, null-safe recipients for all emails
    Now the last piece is to integrate this with REST API endpoints, and ensure that those are tested properly.

---
+++ tests/jest/unit/email/EmailStudyRequestNewComment.spec.js
@@ -0,0 +1,47 @@
+import CentrelineDAO from '@/lib/db/CentrelineDAO';
+import UserDAO from '@/lib/db/UserDAO';
+import EmailBase from '@/lib/email/EmailBase';
+import EmailStudyRequestNewComment from '@/lib/email/EmailStudyRequestNewComment';
+import CompositeId from '@/lib/io/CompositeId';
+import { generateStudyRequest } from '@/lib/test/random/StudyRequestGenerator';
+import { generateUser } from '@/lib/test/random/UserGenerator';
+
+jest.mock('@/lib/db/CentrelineDAO');
+jest.mock('@/lib/db/UserDAO');
+
+test('EmailStudyRequestNewComment', async () => {
+  const requester = generateUser();
+  const studyRequest = generateStudyRequest();
+  studyRequest.id = 42;
+  const studyRequestComment = {
+    comment: 'Test comment',
+  };
+  const email = new EmailStudyRequestNewComment(studyRequest, studyRequestComment);
+
+  const { centrelineId, centrelineType } = studyRequest;
+  CentrelineDAO.byFeature.mockResolvedValue({
+    centrelineId,
+    centrelineType,
+    description: 'Test location',
+  });
+  UserDAO.byId.mockResolvedValue(requester);
+
+  await email.init();
+
+  const recipients = email.getRecipients();
+  expect(recipients).toEqual([EmailBase.getRecipientStudyRequestAdmin(), ...studyRequest.ccEmails]);
+
+  const subject = email.getSubject();
+  expect(subject).toEqual('[MOVE] New comment on request #42 for Test location');
+
+  const params = email.getBodyParams();
+  const s1 = CompositeId.encode([studyRequest]);
+  expect(params.comment).toEqual('Test comment');
+  expect(params.hrefLocation).toEqual(`https://localhost:8080/view/location/${s1}/POINTS`);
+  expect(params.hrefStudyRequest).toEqual('https://localhost:8080/requests/study/42');
+  expect(params.location).toEqual('Test location');
+
+  expect(() => {
+    email.render();
+  }).not.toThrow();
+});
