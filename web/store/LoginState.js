dee6f67669451a3c9656b69c203e3b6f3781f340
Evan Savage <Evan.Savage@toronto.ca>
2021-02-18 14:30:07 +0000

    More documentation for login process, Constants
    as per PR comments.

--- web/store/LoginState.js
+++ web/store/LoginState.js
@@ -1,5 +1,17 @@
 const STORAGE_KEY_LOGIN_STATE = 'ca.toronto.move.loginState';
 
+/**
+ * Restores the navigation target saved by {@link saveLoginState}.  This is intended to
+ * be used after login.  If the user successfully logged in _and_ a valid navigation target
+ * is available in `sessionStorage`, this results in one of two actions:
+ *
+ * 1. when the user used the bottom-right menu to log in, to return them to the page they were on;
+ * 2. when the user clicked a button or link that takes them to a page they must log in to
+ *    access, to complete navigation to that page.
+ *
+ * @param {Function} next - `next` method from `vue-router` navigation guards
+ * @returns {boolean} whether conditions to restore a navigation target were met
+ */
 function restoreLoginState(next) {
   const urlParams = new URLSearchParams(window.location.search);
   const paramLogin = urlParams.get('login');
@@ -35,6 +47,19 @@ function restoreLoginState(next) {
   }
 }
 
+/**
+ * Saves the given navigation target to `sessionStorage`.  This is intended to be called
+ * before login, and is used in one of two ways:
+ *
+ * 1. when the user uses the bottom-right menu to log in, to save their current page;
+ * 2. when the user clicks a button or link that takes them to a page they must log in to
+ *    access, to save that page.
+ *
+ * This navigation target is saved to `sessionStorage`, to be restored by
+ * {@link restoreLoginState} after login.
+ *
+ * @param {Object} to - navigation target
+ */
 function saveLoginState(to) {
   const { name, params = {} } = to;
   const loginState = JSON.stringify({ name, params });
