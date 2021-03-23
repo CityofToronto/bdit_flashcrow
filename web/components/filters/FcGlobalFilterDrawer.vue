490e49094d10d3d9faab475a70e25655512ecf98
Evan Savage <Evan.Savage@toronto.ca>
2021-03-23 14:36:33 +0000

    Stub layout for global filters drawer
    similar to our filter dialog boxes, with heading / Clear All at top and Cancel / Save at bottom.

--- web/components/filters/FcGlobalFilterDrawer.vue
+++ web/components/filters/FcGlobalFilterDrawer.vue
@@ -6,15 +6,61 @@
     right
     temporary
     :width="280">
+    <div class="d-flex fill-height flex-column">
+      <div class="align-center d-flex flex-grow-0 flex-shrink-0 px-4 py-3 shading">
+        <h2 class="display-1">Filter</h2>
+        <v-spacer></v-spacer>
+        <FcButton
+          type="secondary"
+          @click="actionClearAll">
+          Clear All
+        </FcButton>
+      </div>
+
+      <v-divider></v-divider>
+
+      <div class="flex-grow-1 flex-shrink-1">
+        TODO: filters body
+      </div>
+
+      <v-divider></v-divider>
+
+      <div class="d-flex flex-grow-0 flex-shrink-0 px-4 py-2 shading">
+        <v-spacer></v-spacer>
+        <FcButton
+          type="tertiary"
+          @click="internalValue = false">
+          Cancel
+        </FcButton>
+        <FcButton
+          type="tertiary"
+          @click="actionSave">
+          Save
+        </FcButton>
+      </div>
+    </div>
   </v-navigation-drawer>
 </template>
 
 <script>
+import FcButton from '@/web/components/inputs/FcButton.vue';
 import FcMixinVModelProxy from '@/web/mixins/FcMixinVModelProxy';
 
 export default {
   name: 'FcGlobalFilterDrawer',
   mixins: [FcMixinVModelProxy(Boolean)],
+  components: {
+    FcButton,
+  },
+  methods: {
+    actionClearAll() {
+
+    },
+    actionSave() {
+      // TODO: implement this
+      this.internalValue = false;
+    },
+  },
 };
 </script>
 
