4abb69c87750775ba6b8752b2566cf5c33deb83b
Evan Savage <Evan.Savage@toronto.ca>
2021-06-08 18:28:48 +0000

    Use v-menu for project mode menu
    instead of `FcMenu`, which is really only useful for single-line menus.

--- web/components/requests/status/FcMenuStudyRequestsProjectMode.vue
+++ web/components/requests/status/FcMenuStudyRequestsProjectMode.vue
@@ -1,27 +1,52 @@
 <template>
-  <FcMenu
-    :items="[ProjectMode.CREATE_NEW, ProjectMode.ADD_TO_EXISTING]"
-    v-bind="$attrs"
-    @action-menu="actionMenu">
-    <v-icon color="primary" left>mdi-folder-plus</v-icon>
-    <span>{{label}}</span>
-    <template v-slot:item="{ item }">
-      <span>{{item.title}}</span>
-      <v-messages :value="[item.subtitle.replace('{}', textInject)]" />
+  <v-menu :disabled="disabled">
+    <template v-slot:activator="{ on, attrs }">
+      <FcButton
+        v-bind="attrs"
+        v-on="on"
+        :class="buttonClass"
+        :disabled="disabled"
+        :loading="loading"
+        type="secondary">
+        <v-icon color="primary" left>mdi-folder-plus</v-icon>
+        <span>{{label}}</span>
+        <v-icon right>mdi-menu-down</v-icon>
+      </FcButton>
     </template>
-  </FcMenu>
+    <v-list two-line>
+      <v-list-item
+        v-for="item in [ProjectMode.CREATE_NEW, ProjectMode.ADD_TO_EXISTING]"
+        :key="item.name"
+        @click="$emit('action-project-mode', item)">
+        <v-list-item-content>
+          <v-list-item-title>{{item.title}}</v-list-item-title>
+          <v-list-item-subtitle>
+            {{item.subtitle.replace('{}', textInject)}}
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-menu>
 </template>
 
 <script>
 import { ProjectMode } from '@/lib/Constants';
-import FcMenu from '@/web/components/inputs/FcMenu.vue';
+import FcButton from '@/web/components/inputs/FcButton.vue';
 
 export default {
   name: 'FcMenuStudyRequestsProjectMode',
   components: {
-    FcMenu,
+    FcButton,
   },
   props: {
+    buttonClass: {
+      type: [Array, Object, String],
+      default: null,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
     label: String,
     textInject: String,
   },
