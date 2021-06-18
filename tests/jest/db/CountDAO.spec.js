9bdff8e28df8e4659a6090759a748c5fdab5e379
Evan Savage <Evan.Savage@toronto.ca>
2021-06-18 19:18:30 +0000

    Fix CountDAO issues
    We were using the old `arteryGroupId` field here still - changed that to `countLocationId`, and updated tests accordingly.

--- tests/jest/db/CountDAO.spec.js
+++ tests/jest/db/CountDAO.spec.js
@@ -7,58 +7,68 @@ afterAll(() => {
   db.$pool.end();
 });
 
-test('CountDAO.byStudy', async () => {
-  // TMC: single-day, single-arterycode
-  let result = await CountDAO.byStudy({
-    arteryGroupId: 23945,
+test('CountDAO.byStudy [TMC]', async () => {
+  const result = await CountDAO.byStudy({
+    legacy: true,
+    countLocationId: 23945,
     endDate: DateTime.fromObject({ year: 2013, month: 5, day: 13 }),
     startDate: DateTime.fromObject({ year: 2013, month: 5, day: 13 }),
-    type: { id: 5, studyType: StudyType.TMC },
+    studyType: StudyType.TMC,
   });
   expect(result).toHaveLength(1);
+});
 
-  // RESCU: not aggregated across days
-  result = await CountDAO.byStudy({
-    arteryGroupId: 3184,
+test('CountDAO.byStudy [RESCU]', async () => {
+  const result = await CountDAO.byStudy({
+    legacy: true,
+    countLocationId: 3184,
     endDate: DateTime.fromObject({ year: 2009, month: 11, day: 20 }),
     startDate: DateTime.fromObject({ year: 2009, month: 11, day: 20 }),
-    type: { id: 2, studyType: StudyType.RESCU },
+    studyType: StudyType.RESCU,
   });
   expect(result).toHaveLength(1);
+});
 
-  // volume ATR: single-day, single-direction
-  result = await CountDAO.byStudy({
-    arteryGroupId: 32532,
+test('CountDAO.byStudy [volume ATR, single-day, single-direction]', async () => {
+  const result = await CountDAO.byStudy({
+    legacy: true,
+    countLocationId: 32532,
     endDate: DateTime.fromObject({ year: 2012, month: 12, day: 20 }),
     startDate: DateTime.fromObject({ year: 2012, month: 12, day: 20 }),
-    type: { id: 4, studyType: StudyType.ATR_SPEED_VOLUME },
+    studyType: StudyType.ATR_SPEED_VOLUME,
   });
   expect(result).toHaveLength(1);
+});
 
-  // volume ATR: multi-day, single-direction
-  result = await CountDAO.byStudy({
-    arteryGroupId: 38438,
+test('CountDAO.byStudy [volume ATR, multi-day, single-direction]', async () => {
+  const result = await CountDAO.byStudy({
+    legacy: true,
+    countLocationId: 38438,
     endDate: DateTime.fromObject({ year: 2017, month: 6, day: 15 }),
     startDate: DateTime.fromObject({ year: 2017, month: 6, day: 13 }),
-    type: { id: 4, studyType: StudyType.ATR_SPEED_VOLUME },
+    studyType: StudyType.ATR_SPEED_VOLUME,
   });
   expect(result).toHaveLength(3);
+});
 
-  // volume ATR: multi-day, multi-direction
-  result = await CountDAO.byStudy({
-    arteryGroupId: 32532,
+test('CountDAO.byStudy [volume ATR, multi-day, multi-direction]', async () => {
+  const result = await CountDAO.byStudy({
+    legacy: true,
+    countLocationId: 32532,
     endDate: DateTime.fromObject({ year: 2009, month: 9, day: 17 }),
     startDate: DateTime.fromObject({ year: 2009, month: 9, day: 15 }),
-    type: { id: 1, studyType: StudyType.ATR_VOLUME },
+    studyType: StudyType.ATR_VOLUME,
   });
   expect(result).toHaveLength(6);
+});
 
-  // study with notes
-  result = await CountDAO.byStudy({
-    arteryGroupId: 3184,
+test('CountDAO.byStudy [study with notes]', async () => {
+  const result = await CountDAO.byStudy({
+    legacy: true,
+    countLocationId: 3184,
     endDate: DateTime.fromObject({ year: 2009, month: 5, day: 10 }),
     startDate: DateTime.fromObject({ year: 2009, month: 5, day: 10 }),
-    type: { id: 2, studyType: StudyType.RESCU },
+    studyType: StudyType.RESCU,
   });
   expect(result).toHaveLength(1);
 });
