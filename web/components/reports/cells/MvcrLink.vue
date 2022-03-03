dbeb292a1d2c635ad97f8510c37aa90479fefc2d
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2022-03-03 19:24:44 +0000

    MVCR view triggers alert if no permission

--- web/components/reports/cells/MvcrLink.vue
+++ web/components/reports/cells/MvcrLink.vue
@@ -1,15 +1,23 @@
 <template>
   <div>
-    <a :href="'/api/mvcr/' + sampleMvcrFileName" target="_blank">view</a>
+    <a v-if="hasMvcrReadPermission"
+      :href="'/api/mvcr/' + sampleMvcrFileName" target="_blank">view</a>
+    <a v-else v-on:click="accessDenied">view</a>
     &nbsp;
     <button v-on:click="download">download</button>
   </div>
 </template>
 
 <script>
+import { AuthScope } from '@/lib/Constants';
+import FcMixinAuthScope from '@/web/mixins/FcMixinAuthScope';
+import { mapMutations } from 'vuex';
 
 export default {
   name: 'MvcrLink',
+  mixins: [
+    FcMixinAuthScope,
+  ],
   props: {
     value: {
       type: Boolean,
@@ -17,11 +25,19 @@ export default {
     },
   },
   methods: {
+    ...mapMutations(['setDialog']),
     download() {
       return true;
     },
+    accessDenied() {
+      window.alert('You must have MVCR_READ permission to view or download MVCRs'); // eslint-disable-line no-alert
+      return false;
+    },
   },
   computed: {
+    hasMvcrReadPermission() {
+      return this.hasAuthScope(AuthScope.MVCR_READ);
+    },
     sampleMvcrFileName() {
       return 'sample_mvcr_redacted.pdf';
     },
