9ed0a48374df3d944647ebe8012dd5ef64ae0023
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2022-04-05 16:03:00 +0000

    remove explicit foreign key reference to db view

--- scripts/db/schema-22.up.sql
+++ scripts/db/schema-22.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE mvcr_access_events (
   user_id INT NOT NULL REFERENCES users (id),
   filename TEXT NOT NULL,
   created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  collision_id TEXT NOT NULL REFERENCES collisions.events (collision_id)
+  collision_id TEXT NOT NULL -- fk to collisiond_id on view collisions.events (psql doesn't allow explicit fk referecnes to views)
 );
 
 UPDATE "APP_META"."DB_UPDATE" SET "currentVersion" = 22;
