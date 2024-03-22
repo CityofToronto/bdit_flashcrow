9ed103c0b36b3f4559504b2213b19cc1a32c737e
Spencer Kelly <spencer.kelly@toronto.ca>
2024-03-22 15:41:45 +0000

    disable spellcheck on search input el

--- web/components/inputs/FcInputLocationSearch.vue
+++ web/components/inputs/FcInputLocationSearch.vue
@@ -14,6 +14,7 @@
             v-model="query"
             :aria-label="query"
             autocomplete="off"
+            spellcheck="false"
             dense
             :flat="hasLocationIndex"
             hide-details
