9674d532641927187938102d4b9cc54e763bf4d9
Spencer Kelly <spencer.kelly@toronto.ca>
2024-03-26 16:31:26 +0000

    remove Totals over section heading

--- web/components/data/FcViewDataMultiEdit.vue
+++ web/components/data/FcViewDataMultiEdit.vue
@@ -11,13 +11,7 @@
     </p>
     <template v-else>
       <section
-        aria-labelledby="heading_multi_edit_totals"
         class="fc-multi-edit-inset mt-5">
-        <h3
-          class="display-2 pb-1"
-          id="heading_multi_edit_totals">
-          Totals Over Selection
-        </h3>
         <v-row
           class="my-6"
           no-gutters>
