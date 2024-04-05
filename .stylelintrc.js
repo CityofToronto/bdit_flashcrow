656f71676a43e6364fc328cb6dd196d29474b94b
Spencer Kelly <spencer.kelly@toronto.ca>
2024-04-05 16:15:03 +0000

    ignore disobeyed rules

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,5 +3,13 @@ module.exports = {
   rules: {
     'custom-property-empty-line-before': null,
     'rule-empty-line-before': null,
+    'selector-class-pattern': null,
+    'custom-property-pattern': null,
+    'shorthand-property-no-redundant-values': null,
+    'selector-id-pattern': null,
+    'alpha-value-notation': null,
+    'color-function-notation': null,
+    'media-feature-range-notation': null,
+    'selector-not-notation': null,
   },
 };
