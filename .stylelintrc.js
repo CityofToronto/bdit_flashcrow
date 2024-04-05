2922798ea11758f67846d64e6e4dcfd7203a5ee6
Spencer Kelly <spencer.kelly@toronto.ca>
2024-04-05 13:48:32 +0000

    update stylelint to new api

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: 'stylelint-config-standard',
+  extends: ['stylelint-config-standard', 'stylelint-config-standard-vue'],
   rules: {
     'custom-property-empty-line-before': null,
     'rule-empty-line-before': null,
