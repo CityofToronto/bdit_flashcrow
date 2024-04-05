03e113db04be6034a7badd72423ed22f9c82db4f
Spencer Kelly <spencer.kelly@toronto.ca>
2024-04-05 16:02:50 +0000

    find working scss & postcss version

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-standard-vue'],
+  extends: ['stylelint-config-standard', 'stylelint-config-recommended-vue/scss'],
   rules: {
     'custom-property-empty-line-before': null,
     'rule-empty-line-before': null,
