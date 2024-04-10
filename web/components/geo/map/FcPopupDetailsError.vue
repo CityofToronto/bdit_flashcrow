cc926a8d4b002149e11704e7d32a0081cdc6676b
Omar Khan <Omar.Khan5@toronto.ca>
2024-04-10 19:35:34 +0000

    failing popup event message + toast added

---
+++ web/components/geo/map/FcPopupDetailsError.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>
+      There was a problem loading this information.
+      The MOVE Team has been notified. Email
+      <a href="mailto:move-team@toronto.ca">move-team@toronto.ca</a> if you have questions.
+    </p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'FcPopupDetailsError',
+};
+</script>
