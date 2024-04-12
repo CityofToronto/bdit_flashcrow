66b5be826edf5a6a550f47013c275074cc89ce4a
Spencer Kelly <spencer.kelly@toronto.ca>
2024-04-12 16:43:43 +0000

    dont generate sidebar map popup

--- web/components/geo/map/FcMap.vue
+++ web/components/geo/map/FcMap.vue
@@ -111,14 +111,6 @@ function getFeatureKey(feature) {
   return `${layerId}:${id}`;
 }
 
-function getFeatureKeyLocation(location) {
-  if (location === null) {
-    return null;
-  }
-  const { centrelineType, centrelineId } = location;
-  return `c:${centrelineType}:${centrelineId}`;
-}
-
 export default {
   name: 'FcMap',
   components: {
@@ -379,27 +371,8 @@ export default {
     hoveredFeature: debounce(function watchHoveredFeature() {
       this.featureKeyHoveredPopup = this.featureKeyHovered;
     }, 200),
-    locationActive(locationActive, locationActivePrev) {
-      if (locationActive === null) {
-        const featureKeyLocationActivePrev = getFeatureKeyLocation(locationActivePrev);
-        if (this.featureKeySelected === featureKeyLocationActivePrev) {
-          this.selectedFeature = null;
-        }
-        return;
-      }
-      const { description, geom, ...locationActiveRest } = locationActive;
-      const properties = {
-        ...locationActiveRest,
-        name: description,
-      };
-      const layerId = properties.centrelineType === CentrelineType.INTERSECTION
-        ? 'intersections'
-        : 'midblocks';
-      this.selectedFeature = {
-        geometry: geom,
-        layer: { id: layerId },
-        properties,
-      };
+    locationActive() {
+      this.selectedFeature = null;
     },
     locationsGeoJson() {
       this.updateLocationsSource();
