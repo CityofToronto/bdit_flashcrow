4a60d205176f2d8744d08d033992e5046ca5a6c7
Imad Syed <creeperone23@gmail.com>
2024-04-15 17:22:13 +0000

    Updated popup info and styles

--- web/components/geo/map/FcPopupDetailsStudyRequest.vue
+++ web/components/geo/map/FcPopupDetailsStudyRequest.vue
@@ -6,12 +6,23 @@
       class="body-1 mb-1">
       {{line}}
     </p>
+    <FcButton
+    type="tertiary"
+    class="px-0"
+    @click="actionShowRequest">
+    View Request
+  </FcButton>
   </div>
 </template>
 
 <script>
+import FcButton from '../../inputs/FcButton.vue';
+
 export default {
   name: 'FcPopupDetailsStudyRequest',
+  components: {
+    FcButton,
+  },
   props: {
     featureDetails: Object,
   },
@@ -20,7 +31,17 @@ export default {
       const {
         studyType, studyId, studyHours,
       } = this.featureDetails;
-      return [`#${studyId} - ${studyType}`, `${studyHours === null ? 'No hours info' : studyHours}`];
+      return [`#${studyId} - ${studyType}`, `${studyHours === null ? 'No hours info' : studyHours} hours`];
+    },
+  },
+  methods: {
+    actionShowRequest() {
+      const { studyId } = this.featureDetails;
+      const route = {
+        name: 'requestStudyView',
+        params: { id: studyId },
+      };
+      this.$router.push(route);
     },
   },
 };
