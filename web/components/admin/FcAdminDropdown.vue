eba8c745821f1b7197b8a23f2ee3cdfad1bf96d5
Ryan Daley <ryan.daley@toronto.ca>
2024-04-26 18:52:47 +0000

    Added expiry copy beside dropdown

--- web/components/admin/FcAdminDropdown.vue
+++ web/components/admin/FcAdminDropdown.vue
@@ -1,15 +1,19 @@
 <template>
-  <select class="dropdown" v-model="selectedPermission" @change="handleSelection">
-    <option value='' :selected="true">{{ currentSelection.label }}</option>
-    <option v-for="{ permissionState, permissionSlot } in
-      getOptions(currentSelection, permissions)"
-      :key="permissionSlot"
-      :value="permissionState">
-      <span v-if="currentSelection != permissionState.label">{{ permissionState.label }}</span>
-    </option>
-  </select>
+  <div>
+    <select class="dropdown" v-model="selectedPermission" @change="handleSelection">
+      <option value='' :selected="true">{{ currentSelection.label }}</option>
+      <option v-for="{ permissionState, permissionSlot } in
+        getOptions(currentSelection, permissions)" :key="permissionSlot" :value="permissionState">
+        <span>{{ permissionState.label }}</span>
+      </option>
+    </select>
+    <span v-if="currentSelection == '1'">
+      Access valid until: {{ parseExpiryDateTime(currentUser.mvcrExpiryDate) }}
+    </span>
+  </div>
 </template>
 <script>
+import TimeFormatters from '@/lib/time/TimeFormatters';
 
 export default {
   props: ['permissions', 'currentSelection', 'currentUser'],
@@ -19,6 +23,9 @@ export default {
     };
   },
   methods: {
+    parseExpiryDateTime(expiryDateTime) {
+      return TimeFormatters.formatDateTime(expiryDateTime);
+    },
     getOptions(selectedItem, allItems) {
       return allItems.filter(({ permissionState }) => permissionState !== selectedItem);
     },
