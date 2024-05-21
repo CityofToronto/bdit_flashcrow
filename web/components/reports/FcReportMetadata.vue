ac909347eb4a5c266f85d81a298ef9d279f099f3
Ryan Daley <ryan.daley@toronto.ca>
2024-05-21 10:52:42 -0400

    MOVE-1191 - Spell out study hours in TMC report header (#1240)
    * make TSN wider to force Total Volume on new line.
    
    * Convert 'subtext' to 'tooltip' and make it work.
    
    * give some breating room between the value and ?

--- web/components/reports/FcReportMetadata.vue
+++ web/components/reports/FcReportMetadata.vue
@@ -1,55 +1,59 @@
 <template>
   <div>
-  <v-row class="mb-6" tag="dl">
-    <v-col
-      v-for="({ cols, name, value, subtext }, i) in entries"
-      :key="i"
-      :cols="cols">
-      <dt class="subtitle-1 font-weight-medium">{{name}}</dt>
-      <dd class="mt-1 display-1 font-weight-medium">
+    <v-row class="mb-6" tag="dl">
+      <v-col v-for="({ cols, name, value, tooltip }, i) in entries" :key="i" :cols="cols">
+        <dt class="subtitle-1 font-weight-medium">{{ name }}</dt>
+        <dd v-if="tooltip != null" class="mt-1 display-1 font-weight-medium">
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <FcTextReportValue text-null="None" :value="value" style="padding-right: 0.5rem;"/>
+              <v-icon v-on="on">mdi-help-circle-outline</v-icon>
+            </template>
+            <span>{{ tooltip }}</span>
+          </v-tooltip>
+        </dd>
+        <dd v-else class="mt-1 display-1 font-weight-medium">
         <FcTextReportValue
           text-null="None"
           :value="value" />
       </dd>
-      <dd v-if="subtext != null" class="v-messages theme--light subtext">
-        <FcTextReportValue
-          :value="subtext" />
-      </dd>
-    </v-col>
-  </v-row>
-
-  <div v-if="numFilters > 0 && this.type.label.startsWith('Collision')" class="px-0">
-    <v-row class="align-center mx-0 px-1">
-      <h3 class="flex-1">{{ numFilters }} Filter{{ this.numFilters > 1 ? 's' : '' }}</h3>
-      <FcButton @click="isExpanded = !isExpanded" type="icon" class="flex-1">
-        <v-icon v-if="isExpanded">mdi-menu-up</v-icon>
-        <v-icon v-else>mdi-menu-down</v-icon>
-      </FcButton>
+      </v-col>
     </v-row>
-    <v-expand-transition>
-      <ul v-show="isExpanded" class="pt-1">
-        <li v-for="(item, i) in collisionFilters" :key="i">
-          <b>{{ item.filter }}: </b> {{ item.label }}
-        </li>
-      </ul>
-    </v-expand-transition>
-  </div>
 
-  <div class="callout-container" v-if="this.showCallOut">
-    <div class="callout ma-3">
-      <div class="ma-3">
-        <v-icon color="blue">mdi-information</v-icon>
-      </div>
-      <div class="ml-1 mr-2 pr-2 py-2">
-        For an in-depth explanation of how to interpret this data,
-        <a class="link" href="https://bditto.notion.site/How-to-interpret-a-TMC-Summary-Report-310c8b7e9ca74b18b99aadc50dc27196" target="_blank" rel="noopener noreferrer">
-          see here
-        </a>
+    <div v-if="numFilters > 0 && this.type.label.startsWith('Collision')" class="px-0">
+      <v-row class="align-center mx-0 px-1">
+        <h3 class="flex-1">{{ numFilters }} Filter{{ this.numFilters > 1 ? 's' : '' }}</h3>
+        <FcButton @click="isExpanded = !isExpanded" type="icon" class="flex-1">
+          <v-icon v-if="isExpanded">mdi-menu-up</v-icon>
+          <v-icon v-else>mdi-menu-down</v-icon>
+        </FcButton>
+      </v-row>
+      <v-expand-transition>
+        <ul v-show="isExpanded" class="pt-1">
+          <li v-for="(item, i) in collisionFilters" :key="i">
+            <b>{{ item.filter }}: </b> {{ item.label }}
+          </li>
+        </ul>
+      </v-expand-transition>
+    </div>
+
+    <div class="callout-container" v-if="this.showCallOut">
+      <div class="callout ma-3">
+        <div class="ma-3">
+          <v-icon color="blue">mdi-information</v-icon>
+        </div>
+        <div class="ml-1 mr-2 pr-2 py-2">
+          For an in-depth explanation of how to interpret this data,
+          <a class="link"
+            href="https://bditto.notion.site/How-to-interpret-a-TMC-Summary-Report-310c8b7e9ca74b18b99aadc50dc27196"
+            target="_blank" rel="noopener noreferrer">
+            see here
+          </a>
+        </div>
       </div>
     </div>
-  </div>
 
-</div>
+  </div>
 </template>
 
 <script>
@@ -133,11 +137,13 @@ export default {
   font-size: 14px;
   max-width: 300px;
 }
+
 .callout-container {
   display: flex;
   justify-content: flex-end;
 }
-.subtext {
+
+.tooltip {
   width: 60%;
 }
 
@@ -147,5 +153,4 @@ export default {
     display: none !important;
   }
 }
-
 </style>
