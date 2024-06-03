15f9f723a7df71f1f01d3ab18c05cb433145155d
Ryan Daley <ryan.daley@toronto.ca>
2024-06-03 13:32:04 -0400

    MOVE-1189   After editing the location of a request, other study values switch to defaults (#1261)
    * Needs the error view still
    
    * Mostly working... 1 race condition to chase.
    
    * I think this is most I can do in this popup.
    
    * minor copy change that I forgot to push.
    
    * Don't allow users to set the location to incompatible locations.
    
    * Monday copy updates?

--- web/components/geo/map/EditStudyLocationPopUp.vue
+++ web/components/geo/map/EditStudyLocationPopUp.vue
@@ -1,20 +1,38 @@
 <template>
-  <FcButton
-    type="tertiary"
-    @click="actionSelected">
-    Set Location
-  </FcButton>
+  <div>
+    <FcButton type="tertiary" @click="actionSelected">
+      Set Location
+    </FcButton>
+    <FcDialogAlert
+      v-model="showIncompatableDialog"
+      textOk="OK" title="Invalid location for Study Type" okButtonType="primary">
+      <span class="body-1">
+        We cannot conduct your study at this location.<br />
+        Ensure that the Study Type selected for this location is valid before proceeding with the
+        selection.
+      </span>
+    </FcDialogAlert>
+  </div>
+
 </template>
 
 <script>
 import { mapActions, mapMutations, mapState } from 'vuex';
-import { getLocationByCentreline } from '@/lib/api/WebApi';
+import { getLocationByCentreline, getStudyRequest } from '@/lib/api/WebApi';
+import FcDialogAlert from '@/web/components/dialogs/FcDialogAlert.vue';
 import FcButton from '@/web/components/inputs/FcButton.vue';
+import { getLocationStudyTypes } from '@/lib/geo/CentrelineUtils';
 
 export default {
   name: 'EditStudyLocationPopUp',
   components: {
     FcButton,
+    FcDialogAlert,
+  },
+  data() {
+    return {
+      showIncompatableDialog: false,
+    };
   },
   props: {
     feature: Object,
@@ -23,6 +41,13 @@ export default {
     ...mapState('editRequests', ['indicesSelected', 'studyRequests']),
   },
   methods: {
+    async isCompatibleLocationChange(location) {
+      const requestId = this.$route.params.id;
+      const { studyRequest } = await getStudyRequest(requestId);
+      const { studyType } = studyRequest;
+      const validStudiesArray = await getLocationStudyTypes(location);
+      return validStudiesArray.includes(studyType);
+    },
     async actionSetStudyLocation(location) {
       const { description } = location;
       this.setToastInfo(`Set study location to ${description}.`);
@@ -32,7 +57,11 @@ export default {
       const { centrelineId, centrelineType } = this.feature.properties;
       const feature = { centrelineId, centrelineType };
       const location = await getLocationByCentreline(feature);
-      await this.actionSetStudyLocation(location);
+      if (await this.isCompatibleLocationChange(location)) {
+        await this.actionSetStudyLocation(location);
+      } else {
+        this.showIncompatableDialog = true;
+      }
     },
     ...mapMutations(['setToastInfo']),
     ...mapMutations('editRequests', ['setIndicesSelected']),
