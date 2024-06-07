efa06c9d34f2fc8f32cb2d9bf230d3c729aa5f07
Ryan Daley <ryan.daley@toronto.ca>
2024-06-07 13:19:34 -0400

    MOVE-1189 - Make the dialog hang around for a bit. (#1271)
    * 301 -> 401
    
    * This is working... now I have to clean it up.
    
    * I think this is good now.

--- web/components/geo/map/EditStudyLocationPopUp.vue
+++ web/components/geo/map/EditStudyLocationPopUp.vue
@@ -3,17 +3,6 @@
     <FcButton type="tertiary" @click="actionSelected">
       Set Location
     </FcButton>
-    <FcDialogAlertIncompatibleStudy
-      v-model="showIncompatableDialog"
-      textOk="OK" title="Cannot change location" okButtonType="primary">
-      <span class="body-1">
-        We cannot conduct a {{this.currentStudyTypeString}} at this location.
-        To learn more about studies and the limitations of where they can be
-        conducted, email Data Collection at TrafficData@toronto.ca. <br />
-        Alternatively, cancel this request and submit a new one. This will
-        not affect the turnaround time of your request.
-      </span>
-    </FcDialogAlertIncompatibleStudy>
   </div>
 
 </template>
@@ -21,7 +10,6 @@
 <script>
 import { mapActions, mapMutations, mapState } from 'vuex';
 import { getLocationByCentreline, getStudyRequest } from '@/lib/api/WebApi';
-import FcDialogAlertIncompatibleStudy from '@/web/components/dialogs/FcDialogAlertIncompatibleStudy.vue';
 import FcButton from '@/web/components/inputs/FcButton.vue';
 import { getLocationStudyTypes } from '@/lib/geo/CentrelineUtils';
 
@@ -29,7 +17,6 @@ export default {
   name: 'EditStudyLocationPopUp',
   components: {
     FcButton,
-    FcDialogAlertIncompatibleStudy,
   },
   data() {
     return {
@@ -64,7 +51,8 @@ export default {
       if (await this.isCompatibleLocationChange(location)) {
         await this.actionSetStudyLocation(location);
       } else {
-        this.showIncompatableDialog = true;
+        const msgStudyType = this.currentStudyTypeString;
+        this.$emit('showDialog', msgStudyType);
       }
     },
     ...mapMutations(['setToastInfo']),
