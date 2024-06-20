df497dcd0e7535a14474bbd09827c46be31508f6
Omar Khan <o.khandxb@gmail.com>
2024-06-20 16:10:12 -0400

    docker repo fixes

--- ssl/test/README.md
+++
@@ -1,3 +0,0 @@
-# Local Development SSL Certificates
-
-These certificates are used by `vue.config.js`, and by the REST API tests at `tests/jest/api`, to run MOVE locally under HTTPS.
--- ssl/test/generate_localhost_cert_chain.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-cd "$(dirname "$0")"
-openssl req -x509 -days 36500 -config ca.cnf -newkey rsa:4096 -sha256 -nodes -out ca.pem -outform PEM
-openssl req -config localhost.cnf -newkey rsa:2048 -sha256 -nodes -out localhost.csr -outform PEM
-openssl ca -config ca.cnf -policy signing_policy -extensions signing_req -out localhost.pem -infiles localhost.csr
-cat localhost.pem ca.pem > localhost.crt
