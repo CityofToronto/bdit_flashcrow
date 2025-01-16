4955f8443bc5476e3720a3556e9c9fb3ca5b31ae
Omar Khan <o.khandxb@gmail.com>
2025-01-16 15:26:16 -0500

    no hacky regex, using URL constructor instead

--- web/components/admin/FcAdminUtilities.vue
+++ web/components/admin/FcAdminUtilities.vue
@@ -99,13 +99,11 @@ export default {
   },
   methods: {
     isValidUrl(urlString) {
-      const urlPattern = new RegExp('^(https?:\\/\\/)?' // validate protocol
-        + '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' // validate domain name
-        + '((\\d{1,3}\\.){3}\\d{1,3}))'// validate OR ip (v4) address
-        + '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' // validate port and path
-        + '(\\?[;&a-z\\d%_.~+=-]*)?' // validate query string
-        + '(\\#[-a-z\\d_]*)?$', 'i'); // validate fragment locator
-      return !!urlPattern.test(urlString);
+      try {
+        return Boolean(new URL(urlString));
+      } catch (e) {
+        return false;
+      }
     },
     emptyButtonUrlError() {
       this.buttonErrors = [];
