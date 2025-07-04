1f5c4df816fd8de2bab187481866034292cf6051
Omar Khan <26462466+lowsound42@users.noreply.github.com>
2025-07-04 15:20:59 -0400

    Move 1816 (#1346)
    * added study types as per MOVE-1816
    
    * pulled docker branch in and changed bike_volume to bike_count

--- web/components/requests/fields/FcStudyRequestDuration.vue
+++ web/components/requests/fields/FcStudyRequestDuration.vue
@@ -44,7 +44,8 @@ export default {
       return caption;
     },
     itemsDuration() {
-      if (this.studyType === StudyType.ATR_VOLUME_BICYCLE
+      if (this.studyType === StudyType.BIKE_COUNT
+      || this.studyType === StudyType.PED_COUNT
       || this.studyType === StudyType.ATR_SVC) {
         const itemsDuration = [
           { text: '1 day', value: 24 },
